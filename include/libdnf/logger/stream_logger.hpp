30ac13587948d9ec81032d820a4a27a780240f78
Jaroslav Rohel <jrohel@redhat.com>
2020-03-13 08:54:33 +0100

    [dnf5] Add StreamLogger - logs messages into stream

---
+++ include/libdnf/logger/stream_logger.hpp
@@ -0,0 +1,47 @@
+/*
+Copyright (C) 2020 Red Hat, Inc.
+
+This file is part of libdnf: https://github.com/rpm-software-management/libdnf/
+
+Libdnf is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+Libdnf is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with libdnf.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#ifndef LIBDNF_LOGGER_STREAM_LOGGER_HPP
+#define LIBDNF_LOGGER_STREAM_LOGGER_HPP
+
+
+#include "logger.hpp"
+
+#include <memory>
+#include <mutex>
+#include <ostream>
+
+namespace libdnf {
+
+
+/// StreamLogger is an implementation of logging class that writes messages into a stream.
+class StreamLogger : public Logger {
+public:
+    explicit StreamLogger(std::unique_ptr<std::ostream> && log_stream) : log_stream(std::move(log_stream)) {}
+    void write(time_t time, pid_t pid, Level level, const std::string & message) noexcept override;
+
+private:
+    mutable std::mutex stream_mutex;
+    std::unique_ptr<std::ostream> log_stream;
+};
+
+
+}  // namespace libdnf
+
+#endif
---
+++ libdnf/logger/stream_logger.cpp
@@ -0,0 +1,44 @@
+/*
+Copyright (C) 2020 Red Hat, Inc.
+
+This file is part of libdnf: https://github.com/rpm-software-management/libdnf/
+
+Libdnf is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+Libdnf is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with libdnf.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#include "libdnf/logger/stream_logger.hpp"
+
+#include <iomanip>
+#include <sstream>
+
+namespace libdnf {
+
+void StreamLogger::write(time_t time, pid_t pid, Level level, const std::string & message) noexcept {
+    try {
+        struct tm now;
+
+        // gmtime_r() is used because it is thread-safe (std::gmtime() is not).
+        gmtime_r(&time, &now);
+
+        std::ostringstream ss;
+        ss << std::put_time(&now, "%FT%TZ [");  // "YYYY-MM-DDTHH:MM:SSZ ["
+        ss << pid << "] ";
+        ss << level_to_cstr(level) << " " << message << "\n";
+        std::lock_guard<std::mutex> guard(stream_mutex);
+        *log_stream << ss.str() << std::flush;
+    } catch (...) {
+    }
+}
+
+}  // namespace libdnf
