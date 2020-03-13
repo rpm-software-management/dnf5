1aab0a8cd74cc61cd09dd48bc79f73f03fed7077
Jaroslav Rohel <jrohel@redhat.com>
2020-03-13 08:52:16 +0100

    [dnf5] Add MemoryBufferLogger - logs into memory buffer

---
+++ include/libdnf/logger/memory_buffer_logger.hpp
@@ -0,0 +1,60 @@
+/*
+Copyright (C) 2020 Red Hat, Inc.
+
+This file is part of libdnf: https://github.com/rpm-software-management/libdnf/
+
+Libdnf is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+Libdnf is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with libdnf.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#ifndef LIBDNF_LOGGER_MEMORY_BUFFER_LOGGER_HPP
+#define LIBDNF_LOGGER_MEMORY_BUFFER_LOGGER_HPP
+
+
+#include "logger.hpp"
+
+#include <mutex>
+#include <vector>
+
+namespace libdnf {
+
+
+/// MemoryBufferLogger is an implementation of logging class that stores incoming logging messages into memory buffer.
+/// It is usually used as temporary logger until a final logger is not configured.
+class MemoryBufferLogger : public Logger {
+public:
+    struct Item {
+        time_t time;
+        pid_t pid;
+        Level level;
+        std::string message;
+    };
+
+    explicit MemoryBufferLogger(std::size_t max_items_to_keep, std::size_t reserve = 0);
+    void write(time_t time, pid_t pid, Level level, const std::string & message) noexcept override;
+    std::size_t get_items_count() const { return items.size(); }
+    const Item & get_item(std::size_t item_idx) const;
+    void clear() noexcept;
+    void write_to_logger(Logger * logger);
+
+private:
+    mutable std::mutex items_mutex;
+    std::size_t max_items;  // rotation, oldest messages are replaced
+    std::size_t first_item_idx;
+    std::vector<Item> items;
+};
+
+
+}  // namespace libdnf
+
+#endif
---
+++ libdnf/logger/memory_buffer_logger.cpp
@@ -0,0 +1,78 @@
+/*
+Copyright (C) 2020 Red Hat, Inc.
+
+This file is part of libdnf: https://github.com/rpm-software-management/libdnf/
+
+Libdnf is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+Libdnf is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with libdnf.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#include "libdnf/logger/memory_buffer_logger.hpp"
+
+namespace libdnf {
+
+MemoryBufferLogger::MemoryBufferLogger(std::size_t max_items_to_keep, std::size_t reserve)
+    : max_items(max_items_to_keep)
+    , first_item_idx(0) {
+    if (reserve > 0) {
+        items.reserve(reserve < max_items_to_keep ? reserve : max_items_to_keep);
+    }
+}
+
+void MemoryBufferLogger::write(time_t time, pid_t pid, Level level, const std::string & message) noexcept {
+    try {
+        std::lock_guard<std::mutex> guard(items_mutex);
+        if (max_items == 0 || items.size() < max_items) {
+            items.push_back({time, pid, level, message});
+        } else {
+            items[first_item_idx] = {time, pid, level, message};
+            if (++first_item_idx >= max_items) {
+                first_item_idx = 0;
+            }
+        }
+    } catch (...) {
+    }
+}
+
+const MemoryBufferLogger::Item & MemoryBufferLogger::get_item(std::size_t item_idx) const {
+    if (item_idx >= items.size() || item_idx >= max_items) {
+        throw std::out_of_range("MemoryBufferLogger");
+    }
+
+    std::lock_guard<std::mutex> guard(items_mutex);
+    auto idx = item_idx + first_item_idx;
+    if (idx >= max_items) {
+        idx -= max_items;
+    }
+    return items[idx];
+}
+
+void MemoryBufferLogger::write_to_logger(Logger * logger) {
+    std::lock_guard<std::mutex> guard(items_mutex);
+    for (size_t idx = first_item_idx; idx < items.size(); ++idx) {
+        auto & msg = items[idx];
+        logger->write(msg.time, msg.pid, msg.level, msg.message);
+    }
+    for (size_t idx = 0; idx < first_item_idx; ++idx) {
+        auto & msg = items[idx];
+        logger->write(msg.time, msg.pid, msg.level, msg.message);
+    }
+}
+
+void MemoryBufferLogger::clear() noexcept {
+    std::lock_guard<std::mutex> guard(items_mutex);
+    first_item_idx = 0;
+    items.clear();
+}
+
+}  // namespace libdnf
