2d1c91e1e4109ee03dc340cee7d7b08ed44a9434
Jaroslav Rohel <jrohel@redhat.com>
2020-03-20 09:46:56 +0100

    [dnf5] Modify Config for new libdnf - (license, snake_case)

--- include/libdnf/conf/config.hpp
+++ include/libdnf/conf/config.hpp
@@ -1,42 +1,38 @@
 /*
- * Copyright (C) 2018 Red Hat, Inc.
- *
- * Licensed under the GNU Lesser General Public License Version 2.1
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
- */
+Copyright (C) 2018-2020 Red Hat, Inc.
 
-#ifndef _LIBDNF_CONFIG_HPP
-#define _LIBDNF_CONFIG_HPP
+This file is part of libdnf: https://github.com/rpm-software-management/libdnf/
 
-#ifdef LIBDNF_UNSTABLE_API
+Libdnf is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
 
-#include "OptionBinds.hpp"
+Libdnf is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with libdnf.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#ifndef LIBDNF_CONF_CONFIG_HPP
+#define LIBDNF_CONF_CONFIG_HPP
+
+#include "option_binds.hpp"
 
 namespace libdnf {
 
+/// Base class for configurations objects
 class Config {
 public:
-    OptionBinds & optBinds() { return binds; }
+    OptionBinds & opt_binds() noexcept { return binds; }
 
 private:
     OptionBinds binds;
 };
 
-}
-
-#endif
+}  // namespace libdnf
 
 #endif
