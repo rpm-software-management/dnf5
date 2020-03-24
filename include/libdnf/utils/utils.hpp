20999ae4e32e7fd10659480e8628c1e0b3f10f5e
Jaroslav Rohel <jrohel@redhat.com>
2020-03-24 13:56:13 +0100

    [dnf5] Add utils - have_files_same_content_noexcept function

---
+++ include/libdnf/utils/utils.hpp
@@ -0,0 +1,32 @@
+/*
+Copyright (C) 2020 Red Hat, Inc.
+
+This file is part of libdnf: https://github.com/rpm-software-management/libdnf/
+
+Libdnf is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+Libdnf is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with libdnf.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#ifndef LIBDNF_UTILS_UTILS_HPP
+#define LIBDNF_UTILS_UTILS_HPP
+
+namespace libdnf {
+
+/// Compares content of two files.
+/// Returns "true" if files have the same content.
+/// If content differs or error occurred (file doesn't exist, not readable, ...) returns "false".
+bool have_files_same_content_noexcept(const char * file_path1, const char * file_path2) noexcept;
+
+}  // namespace libdnf
+
+#endif
---
+++ libdnf/utils/utils.cpp
@@ -0,0 +1,74 @@
+/*
+Copyright (C) 2020 Red Hat, Inc.
+
+This file is part of libdnf: https://github.com/rpm-software-management/libdnf/
+
+Libdnf is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+Libdnf is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with libdnf.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#include "libdnf/utils/utils.hpp"
+
+#include <fcntl.h>
+#include <unistd.h>
+
+#include <cstring>
+
+namespace libdnf {
+
+bool have_files_same_content_noexcept(const char * file_path1, const char * file_path2) noexcept {
+    static constexpr int block_size = 4096;
+    bool ret = false;
+    int fd1 = -1;
+    int fd2 = -1;
+    do {
+        if ((fd1 = open(file_path1, O_CLOEXEC)) == -1) {
+            break;
+        }
+        if ((fd2 = open(file_path2, O_CLOEXEC)) == -1) {
+            break;
+        }
+        auto len1 = lseek(fd1, 0, SEEK_END);
+        auto len2 = lseek(fd2, 0, SEEK_END);
+        if (len1 != len2) {
+            break;
+        }
+        ret = true;
+        if (len1 == 0) {
+            break;
+        }
+        lseek(fd1, 0, SEEK_SET);
+        lseek(fd2, 0, SEEK_SET);
+        char buf1[block_size];
+        char buf2[block_size];
+        ssize_t readed;
+        do {
+            readed = read(fd1, &buf1, block_size);
+            auto readed2 = read(fd2, &buf2, block_size);
+            if (readed2 != readed || std::memcmp(&buf1, &buf2, static_cast<size_t>(readed)) != 0) {
+                ret = false;
+                break;
+            }
+        } while (readed == block_size);
+    } while (false);
+
+    if (fd1 != -1) {
+        close(fd1);
+    }
+    if (fd2 != -1) {
+        close(fd2);
+    }
+    return ret;
+}
+
+}  // namespace libdnf
