c4baabb63ec6e7307368657abae7f8c6d6dea0cd
Daniel Mach <dmach@redhat.com>
2020-03-27 17:14:43 +0100

    Run clang-tidy and clang-format on PackageSet

--- include/libdnf/rpm/package_set.hpp
+++ include/libdnf/rpm/package_set.hpp
@@ -1,57 +1,61 @@
 /*
- * Copyright (C) 2018 Red Hat, Inc.
- *
- * Licensed under the GNU Lesser General Public License Version 2.1
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
- */
+Copyright (C) 2018-2020 Red Hat, Inc.
 
-#ifndef __PACKAGE_SET_HPP
-#define __PACKAGE_SET_HPP
+This file is part of libdnf: https://github.com/rpm-software-management/libdnf/
 
-#include <memory>
-#include <solv/bitmap.h>
+Libdnf is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+Libdnf is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with libdnf.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+
+#ifndef LIBDNF_RPM_PACKAGE_SET_HPP
+#define LIBDNF_RPM_PACKAGE_SET_HPP
+
 #include "../dnf-types.h"
-#include <solv/pooltypes.h>
 #include "../hy-package.h"
 
-namespace libdnf {
+#include <solv/bitmap.h>
+#include <solv/pooltypes.h>
 
-struct PackageSet {
+#include <memory>
+
+
+namespace libdnf::rpm {
+
+
+class PackageSet {
 public:
-    PackageSet(DnfSack* sack);
-    PackageSet(DnfSack* sack, Map* map);
+    explicit PackageSet(DnfSack * sack);
+    PackageSet(DnfSack * sack, Map * map);
     PackageSet(const PackageSet & pset);
-    PackageSet(PackageSet && pset);
+    PackageSet(PackageSet && pset) noexcept;
     ~PackageSet();
-    Id operator [](unsigned int index) const;
-    PackageSet & operator +=(const PackageSet & other);
-    PackageSet & operator -=(const PackageSet & other);
-    PackageSet & operator /=(const PackageSet & other);
-    PackageSet & operator +=(const Map * other);
-    PackageSet & operator -=(const Map * other);
-    PackageSet & operator /=(const Map * other);
+    Id operator[](unsigned int index) const;
+    PackageSet & operator+=(const PackageSet & other);
+    PackageSet & operator-=(const PackageSet & other);
+    PackageSet & operator/=(const PackageSet & other);
+    PackageSet & operator+=(const Map * other);
+    PackageSet & operator-=(const Map * other);
+    PackageSet & operator/=(const Map * other);
     void clear();
     bool empty();
-    void set(DnfPackage *pkg);
+    void set(DnfPackage * pkg);
     void set(Id id);
-    bool has(DnfPackage *pkg) const;
+    bool has(DnfPackage * pkg) const;
     bool has(Id id) const;
     void remove(Id id);
-    Map *getMap() const;
-    DnfSack *getSack() const;
+    Map * get_map() const;
+    DnfSack * get_sack() const;
     size_t size() const;
 
     /**
@@ -67,6 +71,8 @@ private:
     std::unique_ptr<Impl> pImpl;
 };
 
-}
 
-#endif /* __PACKAGE_SET_HPP */
+}  // namespace libdnf::rpm
+
+
+#endif // LIBDNF_RPM_PACKAGE_SET_HPP
--- libdnf/rpm/package_set.cpp
+++ libdnf/rpm/package_set.cpp
@@ -1,77 +1,84 @@
 /*
- * Copyright (C) 2018 Red Hat, Inc.
- *
- * Licensed under the GNU Lesser General Public License Version 2.1
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
- */
+Copyright (C) 2018-2020 Red Hat, Inc.
+
+This file is part of libdnf: https://github.com/rpm-software-management/libdnf/
+
+Libdnf is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+Libdnf is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with libdnf.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+
+#include "../dnf-sack.h"
+#include "../hy-util-private.hpp"
+#include "libdnf/rpm/package_set.hpp"
 
 #include <assert.h>
 
-#include "libdnf/rpm/package_set.hpp"
-#include "../dnf-sack.h"
-#include "../hy-util-private.hpp"
 
-namespace libdnf {
+namespace libdnf::rpm {
+
 
 class PackageSet::Impl {
 public:
-    Impl(DnfSack* sack);
-    Impl(DnfSack* sack, Map* map);
+    Impl(DnfSack * sack);
+    Impl(DnfSack * sack, Map * map);
     Impl(const PackageSet & pset);
     ~Impl();
 
 private:
     friend PackageSet;
-    DnfSack *sack;
+    DnfSack * sack;
     Map map;
 };
 
-PackageSet::PackageSet(DnfSack* sack) : pImpl(new Impl(sack)) {}
-PackageSet::PackageSet(DnfSack* sack, Map* map_source) : pImpl(new Impl(sack, map_source)) {}
-PackageSet::PackageSet(const PackageSet & pset): pImpl(new Impl(pset)) {}
-PackageSet::PackageSet(PackageSet && pset): pImpl(std::move(pset.pImpl)) {}
+
+PackageSet::PackageSet(DnfSack * sack) : pImpl(new Impl(sack)) {}
+PackageSet::PackageSet(DnfSack * sack, Map * map_source) : pImpl(new Impl(sack, map_source)) {}
+PackageSet::PackageSet(const PackageSet & pset) : pImpl(new Impl(pset)) {}
+PackageSet::PackageSet(PackageSet && pset) noexcept : pImpl(std::move(pset.pImpl)) {}
 PackageSet::~PackageSet() = default;
 
-PackageSet::Impl::Impl(DnfSack* sack) :
-sack(sack)
-{
+
+PackageSet::Impl::Impl(DnfSack * sack) : sack(sack) {
     map_init(&map, dnf_sack_get_pool(sack)->nsolvables);
 }
-PackageSet::Impl::Impl(DnfSack* sack, Map* map_source) : sack(sack)
-{
+
+
+PackageSet::Impl::Impl(DnfSack * sack, Map * map_source) : sack(sack) {
     map_init_clone(&map, map_source);
 }
-PackageSet::Impl::Impl(const PackageSet & pset): sack(pset.pImpl->sack)
-{
+
+
+PackageSet::Impl::Impl(const PackageSet & pset) : sack(pset.pImpl->sack) {
     map_init_clone(&map, &pset.pImpl->map);
 }
-PackageSet::Impl::~Impl() { map_free(&map); }
 
-Id
-PackageSet::operator [](unsigned int index) const
-{
-    const unsigned char *ti = pImpl->map.map;
-    const unsigned char *end = ti + pImpl->map.size;
+
+PackageSet::Impl::~Impl() {
+    map_free(&map);
+}
+
+
+Id PackageSet::operator[](unsigned int index) const {
+    const unsigned char * ti = pImpl->map.map;
+    const unsigned char * end = ti + pImpl->map.size;
     unsigned int enabled;
     Id id;
 
     while (ti < end) {
         enabled = _BitCountLookup[*ti];
 
-        if (index >= enabled ){
+        if (index >= enabled) {
             index -= enabled;
             ti++;
             continue;
@@ -90,59 +97,50 @@ PackageSet::operator [](unsigned int index) const
     return -1;
 }
 
-PackageSet &
-PackageSet::operator +=(const PackageSet & other)
-{
+
+PackageSet & PackageSet::operator+=(const PackageSet & other) {
     map_or(&pImpl->map, &other.pImpl->map);
     return *this;
 }
 
-PackageSet &
-PackageSet::operator -=(const PackageSet & other)
-{
+
+PackageSet & PackageSet::operator-=(const PackageSet & other) {
     map_subtract(&pImpl->map, &other.pImpl->map);
     return *this;
 }
 
-PackageSet &
-PackageSet::operator /=(const PackageSet & other)
-{
+
+PackageSet & PackageSet::operator/=(const PackageSet & other) {
     map_and(&pImpl->map, &other.pImpl->map);
     return *this;
 }
 
-PackageSet &
-PackageSet::operator +=(const Map * other)
-{
+
+PackageSet & PackageSet::operator+=(const Map * other) {
     map_or(&pImpl->map, const_cast<Map *>(other));
     return *this;
 }
 
-PackageSet &
-PackageSet::operator -=(const Map * other)
-{
+PackageSet & PackageSet::operator-=(const Map * other) {
     map_subtract(&pImpl->map, const_cast<Map *>(other));
     return *this;
 }
 
-PackageSet &
-PackageSet::operator /=(const Map * other)
-{
+
+PackageSet & PackageSet::operator/=(const Map * other) {
     map_and(&pImpl->map, const_cast<Map *>(other));
     return *this;
 }
 
-void
-PackageSet::clear()
-{
+
+void PackageSet::clear() {
     map_empty(&pImpl->map);
 }
 
-bool
-PackageSet::empty()
-{
-    const unsigned char *res = pImpl->map.map;
-    const unsigned char *end = res + pImpl->map.size;
+
+bool PackageSet::empty() {
+    const unsigned char * res = pImpl->map.map;
+    const unsigned char * end = res + pImpl->map.size;
 
     while (res < end) {
         if (*res++)
@@ -152,25 +150,55 @@ PackageSet::empty()
 }
 
 
-void PackageSet::set(DnfPackage *pkg) { MAPSET(&pImpl->map, dnf_package_get_id(pkg)); }
-void PackageSet::set(Id id) { MAPSET(&pImpl->map, id); }
-bool PackageSet::has(DnfPackage *pkg) const { return MAPTST(&pImpl->map, dnf_package_get_id(pkg)); }
-bool PackageSet::has(Id id) const { return MAPTST(&pImpl->map, id); }
-void PackageSet::remove(Id id) { MAPCLR(&pImpl->map, id); }
-Map *PackageSet::getMap() const { return &pImpl->map; }
-DnfSack *PackageSet::getSack() const { return pImpl->sack; }
-size_t PackageSet::size() const { return map_count(&pImpl->map); }
+void PackageSet::set(DnfPackage * pkg) {
+    MAPSET(&pImpl->map, dnf_package_get_id(pkg));
+}
 
-Id PackageSet::next(Id previous) const
-{
-    const unsigned char *ti = pImpl->map.map;
-    const unsigned char *end = ti + pImpl->map.size;
+
+void PackageSet::set(Id id) {
+    MAPSET(&pImpl->map, id);
+}
+
+
+bool PackageSet::has(DnfPackage * pkg) const {
+    return MAPTST(&pImpl->map, dnf_package_get_id(pkg));
+}
+
+
+bool PackageSet::has(Id id) const {
+    return MAPTST(&pImpl->map, id);
+}
+
+
+void PackageSet::remove(Id id) {
+    MAPCLR(&pImpl->map, id);
+}
+
+
+Map * PackageSet::getMap() const {
+    return &pImpl->map;
+}
+
+
+DnfSack * PackageSet::getSack() const {
+    return pImpl->sack;
+}
+
+
+size_t PackageSet::size() const {
+    return map_count(&pImpl->map);
+}
+
+
+Id PackageSet::next(Id previous) const {
+    const unsigned char * ti = pImpl->map.map;
+    const unsigned char * end = ti + pImpl->map.size;
     Id id;
 
     if (previous >= 0) {
         ti += previous >> 3;
-        unsigned char byte = *ti; // byte with the previous match
-        byte >>= (previous & 7) + 1; // shift away all previous 1 bits
+        unsigned char byte = *ti;     // byte with the previous match
+        byte >>= (previous & 7) + 1;  // shift away all previous 1 bits
 
         for (id = previous + 1; byte; byte >>= 1, id++)
             if (byte & 0x01)
@@ -179,8 +207,7 @@ Id PackageSet::next(Id previous) const
     }
 
     while (ti < end) {
-
-        if (!*ti){
+        if (!*ti) {
             ti++;
             continue;
         }
@@ -193,4 +220,5 @@ Id PackageSet::next(Id previous) const
     return -1;
 }
 
-}
+
+}  // namespace libdnf::rpm
