6caf6cce4b3983c1b766c442128e6e397ac91581
Jaroslav Rohel <jrohel@redhat.com>
2020-06-01 14:12:50 +0200

    [microdnf] Add Context class
    It will collect all the data needed for microdnf.
    This commit adds: ArgumentParser, libdnf::Base, container for commands
    and selected command.

---
+++ microdnf/context.hpp
@@ -0,0 +1,49 @@
+/*
+Copyright (C) 2019-2020 Red Hat, Inc.
+
+This file is part of microdnf: https://github.com/rpm-software-management/libdnf/
+
+Microdnf is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+Microdnf is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with microdnf.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#ifndef MICRODNF_CONTEXT_HPP
+#define MICRODNF_CONTEXT_HPP
+
+#include "argument_parser.hpp"
+#include "commands/command.hpp"
+
+#include <libdnf/base/base.hpp>
+
+#include <memory>
+#include <vector>
+
+namespace microdnf {
+
+constexpr const char * VERSION = "0.1.0";
+
+class Context {
+public:
+    /// Select commend to execute
+    void select_command(Command * cmd) { selected_command = cmd; }
+
+    ArgumentParser arg_parser;
+    libdnf::Base base;
+
+    std::vector<std::unique_ptr<Command>> commands;
+    Command * selected_command{nullptr};
+};
+
+}  // namespace microdnf
+
+#endif
