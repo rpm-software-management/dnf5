d78c3a0ad7b22524193f948c7699b154f4596b6c
Daniel Mach <dmach@redhat.com>
2020-09-30 12:55:27 +0200

    tests: Mark IdQueueTest::test_iterator_performance() as a performance test
    This avoids allocating huge amount of memory when sanitizers are enabled

--- test/libdnf/rpm/solv/test_id_queue.hpp
+++ test/libdnf/rpm/solv/test_id_queue.hpp
@@ -27,11 +27,18 @@ along with libdnf.  If not, see <https://www.gnu.org/licenses/>.
 
 class IdQueueTest : public CppUnit::TestCase {
     CPPUNIT_TEST_SUITE(IdQueueTest);
+
+    #ifndef WITH_PERFORMANCE_TESTS
     CPPUNIT_TEST(test_push_back);
     CPPUNIT_TEST(test_operators);
     CPPUNIT_TEST(test_iterator_empty);
     CPPUNIT_TEST(test_iterator_full);
+    #endif
+
+    #ifdef WITH_PERFORMANCE_TESTS
     CPPUNIT_TEST(test_iterator_performance);
+    #endif
+
     CPPUNIT_TEST_SUITE_END();
 
 public:
