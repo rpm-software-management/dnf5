ea617bc55aa96ce1686803c7acd47b8c381364ac
Jaroslav Rohel <jrohel@redhat.com>
2020-09-30 09:14:16 +0200

    [argparser] Add documentation strings

--- include/libdnf-cli/argument_parser.hpp
+++ include/libdnf-cli/argument_parser.hpp
@@ -77,15 +77,38 @@ public:
         Argument & operator=(const Argument &) = delete;
         Argument & operator=(Argument &&) = delete;
         virtual ~Argument() = default;
+
+        /// Sets a description of the argument.
         void set_description(std::string descr) noexcept { description = std::move(descr); }
+
+        /// Sets a brief description of the argument.
         void set_short_description(std::string descr) noexcept { short_description = std::move(descr); }
+
+        /// Passes a list of arguments that cannot be used with this argument.
+        /// Can contain this argument. Groups of conflicting argument can be used.
         void set_conflict_arguments(std::vector<Argument *> * args) noexcept { conflict_args = args; }
+
+        /// Gets argument name.
+        /// It is like id. Do not confuse with the long name of the named argument.
         const std::string & get_name() const noexcept { return name; }
+
+        /// Gets a description of the argument.
         const std::string & get_description() const { return description; }
+
+        /// Gets a brief description of the argument.
         const std::string & get_short_description() const { return short_description; }
+
+        /// Gets the number of times the argument was used during analysis.
+        /// Can be used to determine how many times the argument has been used on the command line.
         int get_parse_count() const noexcept { return parse_count; }
+
+        /// Set the number of times the argument was used during analysis count to 0.
         void reset_parse_count() noexcept { parse_count = 0; }
+
+        /// Tests input for arguments that cannot be used with this argument.
+        /// Returns the first conflicting argument.
         Argument * get_conflict_argument() const noexcept;
+
         static std::string get_conflict_arg_msg(const Argument * conflict_arg);
         virtual void help() const noexcept {}
         ArgumentParser & get_owner() const noexcept { return owner; }
@@ -128,13 +151,26 @@ public:
             libdnf::Option * init_value,
             std::vector<std::unique_ptr<libdnf::Option>> * values);
 
+        /// Gets the number of values required by this argument on the command line.
+        /// May return special values: OPTIONAL, UNLIMITED, UNLIMITED_BUT_ONE
         int get_nvals() const noexcept { return nvals; }
+
+        /// Enables/disables storing parsed values.
+        /// Values can be processed / stored in the user parse hook function.
         void set_store_value(bool enable) noexcept { store_value = enable; }
+
+        /// Returns true if storing the parsed values is enabled.
         bool get_store_value() const noexcept { return store_value; }
+
+        /// Gets list of values.
+        /// Parsed values are stored there if if get_store_value() == true.
         std::vector<std::unique_ptr<libdnf::Option>> * get_linked_values() noexcept { return values; }
+
+        /// Sets the user function for parsing the argument.
         void set_parse_hook_func(ParseHookFunc && func) { parse_hook = std::move(func); }
 
-        // returns number of consumed arguments
+        /// Parses input.
+        /// Returns number of consumed arguments from the input.
         int parse(const char * option, int argc, const char * const argv[]);
 
     private:
@@ -168,27 +204,68 @@ public:
         using ParseHookFunc = std::function<bool(NamedArg * arg, const char * option, const char * value)>;
 
         NamedArg(ArgumentParser & owner, const std::string & name) : Argument(owner, name) {}
+
+        /// Sets long name of argument. Long name is prefixed with two dashes on the command line (e.g. "--help").
         void set_long_name(std::string long_name) noexcept { this->long_name = std::move(long_name); }
+
+        /// Sets short name of argument. Short name is prefixed with one dash on the command line (e.g. "-h").
         void set_short_name(char short_name) { this->short_name = short_name; }
+
+        /// Does the argument need a value on the command line?
         void set_has_value(bool has_value) { this->has_value = has_value; }
+
+        /// Links the value to the argument.
+        /// Parsed value is stored there if if get_store_value() == true.
         void link_value(libdnf::Option * value) { this->value = value; }
+
+        /// Sets constant argument value.
+        /// It is used for argument without value on command line (get_has_value() == false).
         void set_const_value(std::string const_value) noexcept { const_val = std::move(const_value); }
+
+        /// Gets long name of argument. Long name is prefixed with two dashes on the command line (e.g. "--help").
         const std::string & get_long_name() const noexcept { return long_name; }
+
+        /// Gets short name of argument. Short name is prefixed with one dash on the command line (e.g. "-h").
         char get_short_name() const noexcept { return short_name; }
+
+        /// Returns true if the argument need a value on the command line.
         bool get_has_value() const noexcept { return has_value; }
+
+        /// Gets constant argument value.
+        /// It is used for argument without value on command line (get_has_value() == false).
         const std::string & get_const_value() const noexcept { return const_val; }
+
+        /// Gets the value.
+        /// Parsed value is stored there if if get_store_value() == true.
         libdnf::Option * get_linked_value() noexcept { return value; }
+
+        /// Gets value.
+        /// Parsed value is stored there if if get_store_value() == true.
         const libdnf::Option * get_linked_value() const noexcept { return value; }
+
+        /// Enables/disables storing parsed values.
+        /// Values can be processed / stored in the user parse hook function.
         void set_store_value(bool enable) noexcept { store_value = enable; }
+
+        /// Returns true if storing the parsed values is enabled.
         bool get_store_value() const noexcept { return store_value; }
+
+        /// Sets the user function for parsing the argument.
         void set_parse_hook_func(ParseHookFunc && func) { parse_hook = std::move(func); }
 
-        // returns number of consumed arguments
+        /// Parses long argument.
+        /// Returns number of consumed arguments from the input.
         int parse_long(const char * option, int argc, const char * const argv[]);
-        // returns number of consumed arguments
+
+        /// Parses short argument.
+        /// Returns the number of arguments consumed from the input. It may be zero.
+        /// Multiple short arguments can be packed in one item. (e.g. "-v -f" -> "-vf").
         int parse_short(const char * option, int argc, const char * const argv[]);
 
+        /// Sets help text for argument value.
         void set_arg_value_help(std::string text) { arg_value_help = std::move(text); }
+
+        /// Gets help text for argument value.
         const std::string & get_arg_value_help() const noexcept { return arg_value_help; }
 
     private:
@@ -235,25 +312,65 @@ public:
         using ParseHookFunc = std::function<bool(Command * arg, const char * cmd, int argc, const char * const argv[])>;
 
         Command(ArgumentParser & owner, const std::string & name) : Argument(owner, name) {}
+
+        /// Parses input. The input may contain named arguments, (sub)commands and positional arguments.
+        /// Returns number of consumed arguments from the input.
         void parse(const char * option, int argc, const char * const argv[]);
+
+        /// Registers (sub)command to the command.
         void add_command(Command * arg) { cmds.push_back(arg); }
+
+        /// Registers named argument to the command.
         void add_named_arg(NamedArg * arg) { named_args.push_back(arg); }
+
+        /// Registers positional argument to the command.
         void add_positional_arg(PositionalArg * arg) { pos_args.push_back(arg); }
+
+        /// Gets a list of registered commands.
         const std::vector<Command *> & get_commands() const noexcept { return cmds; }
+
+        /// Gets a list of registered named arguments.
         const std::vector<NamedArg *> & get_named_args() const noexcept { return named_args; }
+
+        /// Gets a list of registered positional arguments.
         const std::vector<PositionalArg *> & get_positional_args() const noexcept { return pos_args; }
+
+        /// Returns (sub)command with given ID.
+        /// Exception CommandNotFound is thrown if command is not found.
         Command & get_command(const std::string & name) const;
+
+        /// Returns named argument with given ID.
+        /// Exception NamedArgNotFound is thrown if argument is not found.
         NamedArg & get_named_arg(const std::string & name) const;
+
+        /// Returns positional argument with given ID.
+        /// Exception PositionalArgNotFound is thrown if argument is not found.
         PositionalArg & get_positional_arg(const std::string & name) const;
+
+        /// Sets the user function for parsing the argument.
         void set_parse_hook_func(ParseHookFunc && func) { parse_hook = std::move(func); }
+
+        /// Prints command help text.
         void help() const noexcept override;
+
+        /// Sets the header of the subcommand table. Used to generate help.
         void set_commands_help_header(std::string text) noexcept { commands_help_header = std::move(text); }
+
+        /// Sets the header of the named arguments table. Used to generate help.
         void set_named_args_help_header(std::string text) noexcept { named_args_help_header = std::move(text); }
+
+        /// Sets the header of the positional arguments table. Used to generate help.
         void set_positional_args_help_header(std::string text) noexcept {
             positional_args_help_header = std::move(text);
         }
+
+        /// Gets the header of the subcommand table. Used to generate help.
         const std::string & get_commands_help_header() const noexcept { return commands_help_header; }
+
+        /// Gets the header of the named arguments table. Used to generate help.
         const std::string & get_named_args_help_header() const noexcept { return named_args_help_header; }
+
+        /// Gets the header of the positional arguments table. Used to generate help.
         const std::string & get_positional_args_help_header() const noexcept { return positional_args_help_header; }
 
     private:
@@ -267,24 +384,57 @@ public:
         std::string positional_args_help_header;
     };
 
+    /// Constructs a new command and stores it to the argument parser.
+    /// Returns a pointer to the newly created command.
     Command * add_new_command(const std::string & name);
+
+    /// Constructs a new named argument and stores it to the argument parser.
+    /// Returns a pointer to the newly created named argument.
     NamedArg * add_new_named_arg(const std::string & name);
+
+    /// Constructs a new positional argument and stores it to the argument parser.
+    /// Returns a pointer to the newly created positional argument.
     PositionalArg * add_new_positional_arg(
         const std::string & name, std::vector<std::unique_ptr<libdnf::Option>> * values);
+
+    /// Constructs a new positional argument and stores it to the argument parser.
+    /// Returns a pointer to the newly created positional argument.
     PositionalArg * add_new_positional_arg(
         const std::string & name,
         int nargs,
         libdnf::Option * init_value,
         std::vector<std::unique_ptr<libdnf::Option>> * values);
+
+    /// Moves a list of conflicting argument to the parser.
+    /// Returns a pointer to the list.
     std::vector<Argument *> * add_conflict_args_group(std::unique_ptr<std::vector<Argument *>> && conflict_args_group);
+
+    /// Moves the option with the initial value to the argument parser.
+    /// Returns a pointer to the option.
     libdnf::Option * add_init_value(std::unique_ptr<libdnf::Option> && src);
+
+    /// Constructs a new empty list of values and stores it to the argument parser.
+    /// Returns a pointer to the newly created list.
     std::vector<std::unique_ptr<libdnf::Option>> * add_new_values();
+
+    /// Moves an existing list of values to the argument parser.
+    /// Returns a pointer to the list.
     std::vector<std::unique_ptr<libdnf::Option>> * add_values(
         std::unique_ptr<std::vector<std::unique_ptr<libdnf::Option>>> && values);
 
+    /// Sets the "root" command.
+    /// This is the top-level command in the command hierarchy. It can contain named and positional arguments and subcommands.
     void set_root_command(Command * command) noexcept { root_command = command; }
+
+    /// Gets the "root" command.
+    /// This is the top-level command in the command hierarchy. It can contain named and positional arguments and subcommands.
     Command * get_root_command() noexcept { return root_command; }
+
+    /// Parses input. The parser from the "root" command is called.
+    /// The "LogicError" exception is thrown when the "root" command is not set.
     void parse(int argc, const char * const argv[]);
+
+    /// Reset parse count in all arguments.
     void reset_parse_count();
 
 private:
