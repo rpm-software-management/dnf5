f358a37ce250429309fb98e24f462b65d5d8a5d1
Jaroslav Rohel <jrohel@redhat.com>
2020-11-20 08:21:18 +0100

    Rewrite rpm::solv::IdQueueIterator
    The old version was complicated and contained bugs.

--- libdnf/rpm/solv/queue_iterator.hpp
+++ libdnf/rpm/solv/queue_iterator.hpp
@@ -21,84 +21,58 @@ along with libdnf.  If not, see <https://www.gnu.org/licenses/>.
 #ifndef LIBDNF_RPM_SOLV_QUEUE_ITERATOR_HPP
 #define LIBDNF_RPM_SOLV_QUEUE_ITERATOR_HPP
 
-#include "libdnf/rpm/solv_sack.hpp"
-
-#include <bits/stdc++.h>
-#include <solv/pooltypes.h>
 #include <solv/queue.h>
 
+#include <cstddef>
 #include <iterator>
-
 
 namespace libdnf::rpm::solv {
 
-
-class IdQueue;
-
-
 class IdQueueIterator {
 public:
-    explicit IdQueueIterator(const Queue * queue);
-    IdQueueIterator(const IdQueueIterator & other) = default;
+    explicit IdQueueIterator(const Queue * queue) noexcept;
+    IdQueueIterator(const IdQueueIterator & other) noexcept = default;
 
     using iterator_category = std::forward_iterator_tag;
-    using difference_type = Id;
+    using difference_type = std::ptrdiff_t;
     using value_type = Id;
     using pointer = void;
-    using reference = void;
+    using reference = Id;
 
-    Id operator*() const { return current_value; }
+    Id operator*() const noexcept { return queue->elements[current_index]; }
 
-    IdQueueIterator & operator++();
-    IdQueueIterator operator++(int) { return ++(*this); }
+    IdQueueIterator & operator++() noexcept;
+    IdQueueIterator operator++(int) noexcept;
 
-    bool operator==(const IdQueueIterator & other) const { return current_index == other.current_index; }
-    bool operator!=(const IdQueueIterator & other) const { return current_index != other.current_index; }
+    bool operator==(const IdQueueIterator & other) const noexcept { return current_index == other.current_index; }
+    bool operator!=(const IdQueueIterator & other) const noexcept { return current_index != other.current_index; }
 
-    void begin();
-    void end();
+    void begin() noexcept { current_index = 0; }
+    void end() noexcept { current_index = queue->count; }
 
 protected:
     const Queue * get_queue() const noexcept { return queue; }
 
 private:
-    constexpr static int END = -2;
-
-    // pointer to a queue owned by IdQueue
     const Queue * queue;
-
     int current_index;
-    Id current_value;
 };
 
-
-inline IdQueueIterator::IdQueueIterator(const Queue * queue) : queue{queue} {
-    current_index = -1;
-    ++(*this);
+inline IdQueueIterator::IdQueueIterator(const Queue * queue) noexcept : queue{queue} {
+    begin();
 }
 
-inline void IdQueueIterator::begin() {
-    current_index = -1;
-    ++(*this);
-}
-
-inline void IdQueueIterator::end() {
-    current_value = END;
-    current_index = queue->count;
-}
-
-inline IdQueueIterator & IdQueueIterator::operator++() {
-    current_index++;
-    if (current_index >= queue->count) {
-        current_value = END;
-    } else {
-        current_value = queue->elements[current_index];
-    }
+inline IdQueueIterator & IdQueueIterator::operator++() noexcept {
+    ++current_index;
     return *this;
 }
 
+inline IdQueueIterator IdQueueIterator::operator++(int) noexcept {
+    IdQueueIterator ret(*this);
+    ++*this;
+    return ret;
+}
 
 }  // namespace libdnf::rpm::solv
-
 
 #endif  // LIBDNF_RPM_SOLV_QUEUE_ITERATOR_HPP
