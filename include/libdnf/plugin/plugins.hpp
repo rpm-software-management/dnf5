9dbb64f15af83b75b21ae3f85590fd8640475bb5
Jaroslav Rohel <jrohel@redhat.com>
2021-02-06 22:15:05 +0100

    [plugin] Plugin access classes and Plugin manager

---
+++ include/libdnf/plugin/plugins.hpp
@@ -0,0 +1,157 @@
+/*
+Copyright (C) 2021 Red Hat, Inc.
+
+This file is part of libdnf: https://github.com/rpm-software-management/libdnf/
+
+Libdnf is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 2.1 of the License, or
+(at your option) any later version.
+
+Libdnf is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with libdnf.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#ifndef LIBDNF_PLUGIN_PLUGINS_HPP
+#define LIBDNF_PLUGIN_PLUGINS_HPP
+
+#include "iplugin.hpp"
+
+#include "libdnf/utils/library.hpp"
+
+#include <memory>
+#include <string>
+#include <vector>
+
+namespace libdnf::plugin {
+
+class Plugin {
+public:
+    explicit Plugin(IPlugin & iplugin_instance);
+    virtual ~Plugin();
+
+    Plugin(const Plugin &) = delete;
+    Plugin(Plugin &&) = delete;
+    Plugin & operator=(const Plugin &) = delete;
+    Plugin & operator=(Plugin &&) = delete;
+
+    IPlugin * get_iplugin() const noexcept;
+
+    void set_enabled(bool enabled) noexcept;
+    bool get_enabled() const noexcept;
+
+    void init(Base * base);
+    bool hook(HookId hook_id);
+    void finish() noexcept;
+
+protected:
+    Plugin() = default;
+    IPlugin * iplugin_instance{nullptr};
+    bool enabled{true};
+};
+
+
+class PluginLibrary : public Plugin {
+public:
+    explicit PluginLibrary(const std::string & library_path);
+    ~PluginLibrary();
+
+private:
+    using TGetApiVersionFunc = decltype(&libdnf_plugin_get_api_version);
+    using TGetNameFunc = decltype(&libdnf_plugin_get_name);
+    using TGetVersionFunc = decltype(&libdnf_plugin_get_version);
+    using TNewInstanceFunc = decltype(&libdnf_plugin_new_instance);
+    using TDeleteInstanceFunc = decltype(&libdnf_plugin_delete_instance);
+    TGetApiVersionFunc get_api_version{nullptr};
+    TGetNameFunc get_name{nullptr};
+    TGetVersionFunc get_version{nullptr};
+    TNewInstanceFunc new_instance{nullptr};
+    TDeleteInstanceFunc delete_instance{nullptr};
+    utils::Library library;
+};
+
+
+/// Plugin manager
+class Plugins {
+public:
+    Plugins(Base & base);
+    ~Plugins();
+
+    /// Registers the plugin passed by the argument.
+    void register_plugin(std::unique_ptr<Plugin> && plugin);
+
+    /// Loads the plugin from the library defined by the file path.
+    void load_plugin(const std::string & file_path);
+
+    /// Loads plugins from libraries in the directory.
+    void load_plugins(const std::string & dir_path);
+
+    /// Returns the number of registered plugins.
+    size_t count() const noexcept;
+
+    /// Call init of all allowed plugins.
+    bool init();
+
+    /// Call hook of all allowed plugins.
+    bool hook(HookId id);
+
+    /// Call finish of all allowed plugins in reverse order.
+    void finish() noexcept;
+
+private:
+    Base * base;
+    std::vector<std::unique_ptr<Plugin>> plugins;
+};
+
+
+inline Plugin::Plugin(IPlugin & iplugin_instance) : iplugin_instance{&iplugin_instance} {}
+
+inline Plugin::~Plugin() {
+    finish();
+}
+
+inline IPlugin * Plugin::get_iplugin() const noexcept {
+    return iplugin_instance;
+}
+
+inline void Plugin::set_enabled(bool enabled) noexcept {
+    this->enabled = enabled;
+}
+
+inline bool Plugin::get_enabled() const noexcept {
+    return enabled;
+}
+
+inline void Plugin::init(Base * base) {
+    if (iplugin_instance) {
+        iplugin_instance->init(base);
+    }
+}
+
+inline bool Plugin::hook(HookId hook_id) {
+    if (iplugin_instance) {
+        return iplugin_instance->hook(hook_id);
+    }
+    return true;
+}
+
+inline void Plugin::finish() noexcept {
+    if (iplugin_instance) {
+        iplugin_instance->finish();
+    }
+}
+
+inline Plugins::Plugins(Base & base) : base(&base) {}
+
+inline size_t Plugins::count() const noexcept {
+    return plugins.size();
+}
+
+}  // namespace libdnf::plugin
+
+#endif
---
+++ libdnf/plugin/plugins.cpp
@@ -0,0 +1,140 @@
+/*
+Copyright (C) 2021 Red Hat, Inc.
+
+This file is part of libdnf: https://github.com/rpm-software-management/libdnf/
+
+Libdnf is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 2.1 of the License, or
+(at your option) any later version.
+
+Libdnf is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with libdnf.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#include "libdnf/plugin/plugins.hpp"
+
+#include "libdnf/base/base.hpp"
+#include "libdnf/utils/bgettext/bgettext-lib.h"
+
+#include <fmt/format.h>
+
+#include <filesystem>
+
+namespace libdnf::plugin {
+
+PluginLibrary::PluginLibrary(const std::string & library_path) : library(library_path) {
+    get_api_version = reinterpret_cast<TGetApiVersionFunc>(library.get_address("libdnf_plugin_get_api_version"));
+    get_name = reinterpret_cast<TGetNameFunc>(library.get_address("libdnf_plugin_get_name"));
+    get_version = reinterpret_cast<TGetVersionFunc>(library.get_address("libdnf_plugin_get_version"));
+    new_instance = reinterpret_cast<TNewInstanceFunc>(library.get_address("libdnf_plugin_new_instance"));
+    delete_instance = reinterpret_cast<TDeleteInstanceFunc>(library.get_address("libdnf_plugin_delete_instance"));
+    iplugin_instance = new_instance(PLUGIN_API_VERSION);
+}
+
+PluginLibrary::~PluginLibrary() {
+    finish();
+    delete_instance(iplugin_instance);
+    iplugin_instance = nullptr;
+}
+
+Plugins::~Plugins() {
+    finish();
+    for (auto plugin = plugins.rbegin(), stop = plugins.rend(); plugin != stop; ++plugin) {
+        (*plugin).reset();
+    }
+}
+
+void Plugins::register_plugin(std::unique_ptr<Plugin> && plugin) {
+    auto & logger = base->get_logger();
+    auto * iplugin = plugin->get_iplugin();
+    plugins.emplace_back(std::move(plugin));
+    auto name = iplugin->get_name();
+    auto version = iplugin->get_version();
+    logger.debug(fmt::format(
+        _("Added plugin name=\"{}\", version=\"{}.{}.{}\""), name, version.major, version.minor, version.micro));
+
+    logger.debug(fmt::format(_("Trying to load more plugins using the \"{}\" plugin."), name));
+    iplugin->load_plugins(base);
+    logger.debug(fmt::format(_("End of loading plugins using the \"{}\" plugin."), name));
+}
+
+void Plugins::load_plugin(const std::string & file_path) {
+    auto & logger = base->get_logger();
+    logger.debug(fmt::format(_("Loading plugin file=\"{}\""), file_path));
+    auto plugin = std::make_unique<PluginLibrary>(file_path);
+    auto * iplugin = plugin->get_iplugin();
+    plugins.emplace_back(std::move(plugin));
+    auto name = iplugin->get_name();
+    auto version = iplugin->get_version();
+    logger.debug(fmt::format(
+        _("Loaded plugin name=\"{}\", version=\"{}.{}.{}\""), name, version.major, version.minor, version.micro));
+
+    logger.debug(fmt::format(_("Trying to load more plugins using the \"{}\" plugin."), name));
+    iplugin->load_plugins(base);
+    logger.debug(fmt::format(_("End of loading plugins using the \"{}\" plugin."), name));
+}
+
+void Plugins::load_plugins(const std::string & dir_path) {
+    auto & logger = base->get_logger();
+    if (dir_path.empty())
+        throw std::runtime_error(_("Plugins::loadPlugins() dirPath cannot be empty"));
+
+    std::vector<std::filesystem::path> lib_names;
+    for (auto & p : std::filesystem::directory_iterator(dir_path)) {
+        if ((p.is_regular_file() || p.is_symlink()) && p.path().extension() == ".so") {
+            lib_names.emplace_back(p.path());
+        }
+    }
+    std::sort(lib_names.begin(), lib_names.end());
+
+    std::string error_msgs;
+    for (auto & p : lib_names) {
+        try {
+            load_plugin(p);
+        } catch (const std::exception & ex) {
+            std::string msg = fmt::format(_("Can't load plugin \"{}\": {}"), p.string(), ex.what());
+            logger.error(msg);
+            error_msgs += msg + '\n';
+        }
+    }
+
+    if (!error_msgs.empty()) {
+        throw std::runtime_error(error_msgs);
+    }
+}
+
+bool Plugins::init() {
+    for (auto & plugin : plugins) {
+        if (plugin->get_enabled()) {
+            plugin->init(base);
+        }
+    }
+    return true;
+}
+
+bool Plugins::hook(HookId id) {
+    for (auto & plugin : plugins) {
+        if (plugin->get_enabled()) {
+            if (!plugin->hook(id)) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+void Plugins::finish() noexcept {
+    for (auto plugin = plugins.rbegin(), stop = plugins.rend(); plugin != stop; ++plugin) {
+        if ((*plugin)->get_enabled()) {
+            (*plugin)->finish();
+        }
+    }
+}
+
+}  // namespace libdnf::plugin
