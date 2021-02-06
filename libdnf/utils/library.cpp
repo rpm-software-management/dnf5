673735f71eb9cc4b71e4e6bc7dbc57425a9d4ce0
Jaroslav Rohel <jrohel@redhat.com>
2021-02-06 19:51:46 +0100

    [utils] "Library" class - For working with shared libraries
    It will be used to work with shared libraries containing the plugin.

---
+++ libdnf/utils/library.cpp
@@ -0,0 +1,52 @@
+/*
+Copyright (C) 2021 Red Hat, Inc.
+
+This file is part of libdnf: https://github.com/rpm-software-management/libdnf/
+
+Libdnf is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 2.1 of the License, or
+(at your option) any later version.
+
+Libdnf is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with libdnf.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#include "library.hpp"
+
+#include <dlfcn.h>
+
+namespace libdnf::utils {
+
+Library::Library(const std::string & path)
+: path(path) {
+    handle = dlopen(path.c_str(), RTLD_LAZY);
+    if (!handle) {
+        const char * err_msg = dlerror();
+        throw CantLoad(err_msg);
+    }
+}
+
+Library::~Library() {
+    dlclose(handle);
+}
+
+void * Library::get_address(const char * symbol) const {
+    dlerror();  // Clear any existing error
+    void * address  = dlsym(handle, symbol);
+    if (!address)
+    {
+        const char * err_msg = dlerror();
+        if (err_msg) {
+            throw NotFound(err_msg);
+        }
+    }
+    return address;
+}
+
+}  // namespace libdnf::utils
---
+++ libdnf/utils/library.hpp
@@ -0,0 +1,72 @@
+/*
+Copyright (C) 2021 Red Hat, Inc.
+
+This file is part of libdnf: https://github.com/rpm-software-management/libdnf/
+
+Libdnf is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 2.1 of the License, or
+(at your option) any later version.
+
+Libdnf is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with libdnf.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#ifndef _LIBDNF_UTILS_LIBRARY_HPP
+#define _LIBDNF_UTILS_LIBRARY_HPP
+
+#include "libdnf/utils/exception.hpp"
+
+#include <string>
+
+namespace libdnf::utils {
+
+class Library {
+public:
+    /// Library exception
+    class Exception : public RuntimeError {
+    public:
+        using RuntimeError::RuntimeError;
+        const char * get_domain_name() const noexcept override { return "libdnf::Library"; }
+        const char * get_name() const noexcept override { return "Exception"; }
+        const char * get_description() const noexcept override { return "Library exception"; }
+    };
+
+    class CantLoad : public Exception {
+    public:
+        using Exception::Exception;
+        const char * get_name() const noexcept override { return "CantLoad"; }
+        const char * get_description() const noexcept override { return "Can't load shared library"; }
+    };
+
+    class NotFound : public Exception {
+    public:
+        using Exception::Exception;
+        const char * get_name() const noexcept override { return "NotFound"; }
+        const char * get_description() const noexcept override { return "Can't obtain address of symbol"; }
+    };
+
+    explicit Library(const std::string & path);
+    ~Library();
+
+    Library(const Library &) = delete;
+    Library(Library &&) = delete;
+    Library & operator =(const Library &) = delete;
+    Library & operator =(Library &&) = delete;
+
+    const std::string & get_path() const noexcept { return path; }
+    void * get_address(const char * symbol) const;
+
+private:
+    std::string path;
+    void * handle;
+};
+
+}  // namespace libdnf::utils
+
+#endif
