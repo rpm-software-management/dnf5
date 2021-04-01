50fea951d660ab4070338f5fca635a295ba48e60
Jaroslav Mracek <jmracek@redhat.com>
2021-04-01 14:39:08 +0200

    Add function to accumulate strings

--- libdnf/utils/string.hpp
+++ libdnf/utils/string.hpp
@@ -1,3 +1,19 @@
+/*
+Copyright (C) 2020-2021 Red Hat, Inc.
+This file is part of libdnf: https://github.com/rpm-software-management/libdnf/
+Libdnf is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 2.1 of the License, or
+(at your option) any later version.
+Libdnf is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+You should have received a copy of the GNU Lesser General Public License
+along with libdnf.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+
 #ifndef LIBDNF_UTILS_STRING_HPP
 #define LIBDNF_UTILS_STRING_HPP
 
@@ -26,8 +42,32 @@ inline bool ends_with(const std::string & value, const std::string & pattern) {
     return std::equal(pattern.rbegin(), pattern.rend(), value.rbegin());
 }
 
+/// Join all items in a container into a string using a joiner
+template <typename ContainerT>
+inline std::string join(const ContainerT & input, const std::string & joiner) {
+    auto it = std::begin(input);
+    auto it_end = std::end(input);
 
-}  // libdnf::utils::string
+    std::string result;
+
+    if (it == it_end) {
+        return result;
+    }
+
+    // Append first element
+    result.append(*it);
+    ++it;
+
+    for (; it != it_end; ++it) {
+        result.append(joiner);
+        result.append(*it);
+    }
+
+    return result;
+}
+
+
+}  // namespace libdnf::utils::string
 
 
 #endif  // LIBDNF_UTILS_STRING_HPP
