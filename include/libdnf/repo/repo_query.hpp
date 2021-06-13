5f0f93b0bf867d8663fe3a2ce8621976ec75476c
Daniel Mach <dmach@redhat.com>
2021-06-13 18:18:45 +0200

    doc: Improve repo::RepoQuery docstrings

--- include/libdnf/repo/repo_query.hpp
+++ include/libdnf/repo/repo_query.hpp
@@ -17,12 +17,15 @@ You should have received a copy of the GNU Lesser General Public License
 along with libdnf.  If not, see <https://www.gnu.org/licenses/>.
 */
 
-#ifndef LIBDNF_RPM_REPO_QUERY_HPP
-#define LIBDNF_RPM_REPO_QUERY_HPP
+
+#ifndef LIBDNF_REPO_REPO_QUERY_HPP
+#define LIBDNF_REPO_REPO_QUERY_HPP
+
 
 #include "libdnf/common/sack/query.hpp"
 #include "libdnf/common/weak_ptr.hpp"
 #include "libdnf/repo/repo.hpp"
+
 
 namespace libdnf {
 
@@ -30,6 +33,7 @@ class Base;
 using BaseWeakPtr = WeakPtr<Base, false>;
 
 }  // namespace libdnf
+
 
 namespace libdnf::repo {
 
@@ -41,22 +45,72 @@ class RepoSack;
 
 using RepoSackWeakPtr = WeakPtr<RepoSack, false>;
 
+
 class RepoQuery : public libdnf::sack::Query<RepoWeakPtr> {
 public:
 #ifndef SWIG
     using Query<RepoWeakPtr>::Query;
 #endif
 
+    /// Create a new RepoQuery instance.
+    ///
+    /// @param sack     A weak pointer to RepoSack
     explicit RepoQuery(const RepoSackWeakPtr & sack);
+
+    /// Create a new RepoQuery instance.
+    ///
+    /// @param base     A weak pointer to Base
     explicit RepoQuery(const BaseWeakPtr & base);
+
+    /// Create a new RepoQuery instance.
+    ///
+    /// @param base     Reference to Base
     explicit RepoQuery(Base & base);
 
+    /// Filter repos by their `enabled` state.
+    ///
+    /// @param enabled  A boolean value the filter is matched against.
+    /// @since 5.0
     RepoQuery & filter_enabled(bool enabled);
+
+    /// Filter repos by their `expired` state.
+    ///
+    /// @param expired  A boolean value the filter is matched against.
+    /// @since 5.0
     RepoQuery & filter_expired(bool expired);
+
+    /// Filter repos by their `id`.
+    ///
+    /// @param pattern  A string the filter is matched against.
+    /// @param cmp      A comparison (match) operator, defaults to `QueryCmp::EQ`.
+    /// @since 5.0
     RepoQuery & filter_id(const std::string & pattern, sack::QueryCmp cmp = libdnf::sack::QueryCmp::EQ);
+
+    /// Filter repos by their `id`.
+    ///
+    /// @param pattern  A vector of strings the filter is matched against.
+    /// @param cmp      A comparison (match) operator, defaults to `QueryCmp::EQ`.
+    /// @since 5.0
     RepoQuery & filter_id(const std::vector<std::string> & patterns, sack::QueryCmp cmp = libdnf::sack::QueryCmp::EQ);
+
+    /// Filter repos by their `name`.
+    ///
+    /// @param pattern  A string the filter is matched against.
+    /// @since 5.0
     RepoQuery & filter_local(bool local);
+
+    /// Filter repos by their `name`.
+    ///
+    /// @param pattern  A string the filter is matched against.
+    /// @param cmp      A comparison (match) operator, defaults to `QueryCmp::EQ`.
+    /// @since 5.0
     RepoQuery & filter_name(const std::string & pattern, sack::QueryCmp cmp = libdnf::sack::QueryCmp::EQ);
+
+    /// Filter repos by their `name`.
+    ///
+    /// @param pattern  A vector of strings the filter is matched against.
+    /// @param cmp      A comparison (match) operator, defaults to `QueryCmp::EQ`.
+    /// @since 5.0
     RepoQuery & filter_name(const std::vector<std::string> & patterns, sack::QueryCmp cmp = libdnf::sack::QueryCmp::EQ);
 
 private:
@@ -69,41 +123,50 @@ private:
     };
 };
 
+
 inline RepoQuery & RepoQuery::filter_enabled(bool enabled) {
     filter(F::enabled, enabled, sack::QueryCmp::EQ);
     return *this;
 }
+
 
 inline RepoQuery & RepoQuery::filter_expired(bool expired) {
     filter(F::expired, expired, sack::QueryCmp::EQ);
     return *this;
 }
 
+
 inline RepoQuery & RepoQuery::filter_id(const std::string & pattern, sack::QueryCmp cmp) {
     filter(F::id, pattern, cmp);
     return *this;
 }
+
 
 inline RepoQuery & RepoQuery::filter_id(const std::vector<std::string> & patterns, sack::QueryCmp cmp) {
     filter(F::id, patterns, cmp);
     return *this;
 }
 
+
 inline RepoQuery & RepoQuery::filter_local(bool local) {
     filter(F::local, local, sack::QueryCmp::EQ);
     return *this;
 }
+
 
 inline RepoQuery & RepoQuery::filter_name(const std::string & pattern, sack::QueryCmp cmp) {
     filter(F::name, pattern, cmp);
     return *this;
 }
 
+
 inline RepoQuery & RepoQuery::filter_name(const std::vector<std::string> & patterns, sack::QueryCmp cmp) {
     filter(F::name, patterns, cmp);
     return *this;
 }
 
+
 }  // namespace libdnf::repo
 
-#endif
+
+#endif  // LIBDNF_REPO_REPO_QUERY_HPP
