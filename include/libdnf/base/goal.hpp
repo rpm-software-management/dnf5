c9115652e78f858116273186e5edc4740b017107
Jaroslav Mracek <jmracek@redhat.com>
2021-08-10 21:30:09 +0200

    Enhance documentation strings of Goal class

--- include/libdnf/base/goal.hpp
+++ include/libdnf/base/goal.hpp
@@ -58,155 +58,155 @@ public:
     // TODO(jmracek) Not yet implemented
     void add_module_enable(const std::string & spec);
 
-    /// Add install job to the goal for future resolvement. The operation will not result in package reinstall when a
-    /// requested package is already installed. In default it uses `clean_requirements_on_remove` with `false` value,
-    /// but it can be overridden in `setting`.
+    /// Add install request to the goal. The `spec` will be resolved to packages in the resolve() call. The operation will not
+    /// result in a reinstall if the requested package is already installed. By default uses
+    /// `clean_requirements_on_remove` set to `false`, which can be overridden in `settings`.
     ///
-    /// @param spec      A string describing the Goal install job.
+    /// @param spec      A string describing the Goal install request.
     /// @param settings  A structure to override default goal settings. The value `from_repo_ids` is not used.
     // @replaces dnf:dnf/base.py:method:Base().install(self, pkg_spec, reponame=None, strict=True, forms=None)
     void add_rpm_install(const std::string & spec, const libdnf::GoalJobSettings & settings = libdnf::GoalJobSettings());
 
-    /// Add install job to the goal. The operation will not result in package reinstall when requested package
-    /// with the same NEVRA is already installed. In default it uses `clean_requirements_on_remove` with `false` value.
+    /// Add install request to the goal. The operation will not result in a reinstall when requested package
+    /// with the same NEVRA is already installed. By default uses `clean_requirements_on_remove` set to `false`.
     ///
-    /// @param rpm_package      A package to be added to the install job.
+    /// @param rpm_package      A package to be installed.
     /// @param settings         A structure to override default goal settings. Only `strict`, `best`, and `clean_requirements_on_remove` are used.
     // @replaces dnf:dnf/base.py:method:Base().package_install(self, pkg, strict=True)
     void add_rpm_install(const libdnf::rpm::Package & rpm_package, const libdnf::GoalJobSettings & settings = libdnf::GoalJobSettings());
 
-    /// Add install job to the goal. The operation will not result in package reinstall when requested package
-    /// with the same NEVRA is already installed. In default it uses `clean_requirements_on_remove` with `false` value.
+    /// Add install request to the goal. The operation will not result in a reinstall when requested package
+    /// with the same NEVRA is already installed. By default uses `clean_requirements_on_remove` set to `false`.
     ///
-    /// @param package_set      A package_set to be added to the install job.
+    /// @param package_set      A package_set containig candidates for the install action.
     /// @param settings         A structure to override default goal settings. Only `strict`, `best`, and `clean_requirements_on_remove` are used.
     void add_rpm_install(const libdnf::rpm::PackageSet & package_set, const libdnf::GoalJobSettings & settings = libdnf::GoalJobSettings());
 
-    /// Add install_or_reinstall job to the goal. In default it uses `clean_requirements_on_remove` with `false` value.
+    /// Add install or reinstall request to the goal. By default uses `clean_requirements_on_remove` set to `false`.
     ///
-    /// @param rpm_package      A package to be added to the install_or_reinstall job.
+    /// @param rpm_package      A package to be installed or reinstalled.
     /// @param settings         A structure to override default goal settings. Only `strict`, `best`, and `clean_requirements_on_remove` are used.
     // @replaces libdnf/hy-goal.h:function:hy_goal_install(HyGoal goal, DnfPackage *new_pkg)
     // @replaces libdnf/hy-goal.h:function:hy_goal_install_optional(HyGoal goal, DnfPackage *new_pkg)
     void add_rpm_install_or_reinstall(
         const libdnf::rpm::Package & rpm_package, const libdnf::GoalJobSettings & settings = libdnf::GoalJobSettings());
 
-    /// Add install_or_reinstall job to the goal. In default it uses `clean_requirements_on_remove` with `false` value.
+    /// Add install or reinstall request to the goal. By default uses `clean_requirements_on_remove` set to `false`.
     ///
-    /// @param package_set      A package_set to be added to the install_or_reinstall job.
+    /// @param package_set      A package_set containig candidates for the install or reinstall action.
     /// @param settings         A structure to override default goal settings. Only `strict`, `best`, and `clean_requirements_on_remove` are used.
     // @replaces libdnf/hy-goal.h:function:hy_goal_install_selector(HyGoal goal, HySelector sltr, GError **error)
     // @replaces libdnf/hy-goal.h:function:hy_goal_install_selector_optional(HyGoal goal, HySelector sltr, GError **error)
     void add_rpm_install_or_reinstall(
         const libdnf::rpm::PackageSet & package_set, const libdnf::GoalJobSettings & settings = libdnf::GoalJobSettings());
 
-    /// Add reinstall job to the goal for future resolvement. In default it uses `clean_requirements_on_remove` with `false` value,
-    /// but it can be overridden in `setting`.
+    /// Add reinstall request to the goal. The `spec` will be resolved to packages in the resolve() call. By default uses
+    /// `clean_requirements_on_remove` set to `false`, which can be overridden in `settings`.
     ///
-    /// @param spec      A string describing the Goal reinstall job.
+    /// @param spec      A string describing the Goal reinstall request.
     /// @param settings  A structure to override default goal settings. The value `from_repo_ids` is not used (not implemented yet).
     // @replaces dnf:dnf/base.py:method:Base().reinstall(self, pkg_spec, old_reponame=None, new_reponame=None, new_reponame_neq=None, remove_na=False)
     void add_rpm_reinstall(const std::string & spec, const libdnf::GoalJobSettings & settings = libdnf::GoalJobSettings());
 
     // TODO(jmracek) Do we want to add reinstall or remove?
 
-    /// Add remove job to the goal for future resolvement. In default it uses `clean_requirements_on_remove` according to Config,
-    /// but it can be overridden in `setting`.
+    /// Add remove request to the goal. The `spec` will be resolved to packages in the resolve() call. By default uses
+    /// `clean_requirements_on_remove` according to ConfigMain, which can be overridden in `settings`.
     ///
-    /// @param spec      A string describing the Goal remove job.
+    /// @param spec      A string describing the Goal remove request.
     /// @param settings  A structure to override default goal settings. Values `strict` and `best` are not used.
     // @replaces dnf:dnf/base.py:method:Base().remove(self, pkg_spec, reponame=None, forms=None)
     void add_rpm_remove(const std::string & spec, const libdnf::GoalJobSettings & settings = libdnf::GoalJobSettings());
 
-    /// Add remove job to the goal. In default it uses `clean_requirements_on_remove` according to Config,
-    /// but it can be overridden in `setting`.
+    /// Add remove request to the goal. By default uses `clean_requirements_on_remove` value from ConfigMain,
+    /// which can be overridden in `settings`.
     ///
-    /// @param rpm_package      A package to be added to the remove job.
+    /// @param rpm_package      A package to be removed.
     /// @param settings         A structure to override default goal settings. Only clean_requirements_on_remove is used.
     // @replaces libdnf/hy-goal.h:function:hy_goal_erase(HyGoal goal, DnfPackage *pkg)
     // @replaces libdnf/hy-goal.h:function:hy_goal_erase_flags(HyGoal goal, DnfPackage *pkg, int flags)
     // @replaces dnf:dnf/base.py:method:Base().package_remove(self, pkg)
     void add_rpm_remove(const libdnf::rpm::Package & rpm_package, const libdnf::GoalJobSettings & settings = libdnf::GoalJobSettings());
 
-    /// Add remove job to the goal. In default it uses `clean_requirements_on_remove` according to Config,
-    /// but it can be overridden in `setting`.
+    /// Add remove request to the goal. By default uses `clean_requirements_on_remove` value from ConfigMain,
+    /// which can be overridden in `settings`.
     ///
-    /// @param package_set      A package_set to be added to the remove job.
+    /// @param package_set      A package_set containing packages that will be removed
     /// @param settings         A structure to override default goal settings. Only `clean_requirements_on_remove` is used.
     // @replaces libdnf/hy-goal.h:function:hy_goal_erase_flags(HyGoal goal, DnfPackage *pkg, int flags)
     void add_rpm_remove(const libdnf::rpm::PackageSet & package_set, const libdnf::GoalJobSettings & settings = libdnf::GoalJobSettings());
 
-    /// Add upgrade job to the goal for future resolvement. In default it uses `clean_requirements_on_remove` with `false` value,
-    /// but it can be overridden in `setting`.
+    /// Add upgrade request to the goal. The `spec` will be resolved to packages in the resolve() call. By default uses
+    /// `clean_requirements_on_remove` set to `false`, which can be overridden in `settings`.
     ///
-    /// @param spec      A string describing the Goal upgrade job.
+    /// @param spec      A string describing the Goal upgrade request.
     /// @param settings  A structure to override default goal settings. Values  `from_repo_ids` and `strict` are not used.
     // @replaces dnf:dnf/base.py:method:Base().upgrade(self, pkg_spec, reponame=None)
     void add_rpm_upgrade(const std::string & spec, const libdnf::GoalJobSettings & settings = libdnf::GoalJobSettings());
 
     // TODO(jmracek) Add suport `to_repo_ids`
-    /// Add upgrade job of all installed packages to the goal. In default it uses `clean_requirements_on_remove` with `false` value,
-    /// but it can be overridden in `setting`.
+    /// Add upgrade job of all installed packages to the goal if not limited in `settings`. By default uses
+    /// `clean_requirements_on_remove` set to `false`, which can be overridden in `settings`.
     ///
     /// @param settings  A structure to override default goal settings. Values `from_repo_ids` and `strict` are not used.
     // @replaces libdnf/hy-goal.h:function:hy_goal_upgrade_all(HyGoal goal)
     // @replaces dnf:dnf/base.py:method:Base().upgrade_all(self, reponame=None)
     void add_rpm_upgrade(const libdnf::GoalJobSettings & settings = libdnf::GoalJobSettings());
 
-    /// Add upgrade job to the goal. In default it uses `clean_requirements_on_remove` with `false` value,
-    /// but it can be overridden in `setting`.
+    /// Add upgrade request to the goal. By default uses `clean_requirements_on_remove` set to `false`,
+    /// which can be overridden in `settings`.
     ///
-    /// @param rpm_package      A package to be added to the upgrade job.
+    /// @param rpm_package      A package that will be used as candidate for the upgrade action.
     /// @param settings         A structure to override default goal settings. Only `clean_requirements_on_remove` and `best` values are used.
     // @replaces libdnf/hy-goal.h:function:hy_goal_upgrade_to(HyGoal goal, DnfPackage *new_pkg)
     // @replaces dnf:dnf/base.py:method:Base().package_upgrade(self, pkg)
     void add_rpm_upgrade(const libdnf::rpm::Package & rpm_package, const libdnf::GoalJobSettings & settings = libdnf::GoalJobSettings());
 
-    /// Add upgrade job to the goal. In default it uses `clean_requirements_on_remove` with `false` value,
-    /// but it can be overridden in `setting`.
+    /// Add upgrade request to the goal. By default uses `clean_requirements_on_remove` set to `false`,
+    /// which can be overridden in `settings`.
     ///
-    /// @param package_set      A package_set to be added to the upgrade job.
+    /// @param package_set      A package_set containing candidates for the upgrade action.
     /// @param settings         A structure to override default goal settings. Only `clean_requirements_on_remove` and `best` values are used.
     // @replaces libdnf/hy-goal.h:function:hy_goal_upgrade_selector(HyGoal goal, HySelector sltr)
     void add_rpm_upgrade(const libdnf::rpm::PackageSet & package_set, const libdnf::GoalJobSettings & settings = libdnf::GoalJobSettings());
 
-    /// Add downgrade job to the goal for future resolvement. In default it uses `clean_requirements_on_remove` with `false` value,
-    /// but it can be overridden in `setting`.
+    /// Add downgrade request to the goal. The `spec` will be resolved to packages in the resolve() call. By default uses
+    /// `clean_requirements_on_remove` set to `false`, which can be overridden in `settings`.
     ///
-    /// @param spec      A string describing the Goal downgrade job.
+    /// @param spec      A string describing the Goal downgrade request.
     /// @param settings  A structure to override default goal settings. The value `from_repo_ids` is not used.
     // @replaces dnf:dnf/base.py:method:Base().downgrade(self, pkg_spec)
     void add_rpm_downgrade(const std::string & spec, const libdnf::GoalJobSettings & settings = libdnf::GoalJobSettings());
 
-    /// Add distrosync job to the goal for future resolvement. In default it uses `clean_requirements_on_remove` with `false` value,
-    /// but it can be overridden in `setting`.
+    /// Add distrosync request to the goal. The `spec` will be resolved to packages in the resolve() call. By default uses
+    /// `clean_requirements_on_remove` set to `false`, which can be overridden in `settings`.
     ///
-    /// @param spec      A string describing the Goal distrosync job.
+    /// @param spec      A string describing the Goal distrosync request.
     /// @param settings  A structure to override default goal settings. The value `from_repo_ids` is not used.
     // @replaces dnf:dnf/base.py:method:Base().distro_sync(self, pkg_spec=None)
     void add_rpm_distro_sync(const std::string & spec, const libdnf::GoalJobSettings & settings = libdnf::GoalJobSettings());
 
     // TODO(jmracek) Add suport `to_repo_ids`
-    /// Add distrosync job of all installed packages to the goal. In default it uses `clean_requirements_on_remove` with `false` value,
-    /// but it can be overridden in `setting`.
+    /// Add distrosync job of all installed packages to the goal if not limited in `settings`. By default uses
+    /// `clean_requirements_on_remove` set to `false`, which can be overridden in `settings`.
     ///
     /// @param settings  A structure to override default goal settings. Values `from_repo_ids` is not used.
     // @replaces libdnf/hy-goal.h:function:hy_goal_distupgrade_all(HyGoal goal)
     // @replaces dnf:dnf/base.py:method:Base().distro_sync(self, pkg_spec=None)
     void add_rpm_distro_sync(const libdnf::GoalJobSettings & settings = libdnf::GoalJobSettings());
 
-    /// Add distrosync job to the goal. In default it uses `clean_requirements_on_remove` with `false` value,
-    /// but it can be overridden in `setting`.
+    /// Add distrosync request to the goal. By default uses `clean_requirements_on_remove` set to `false`,
+    /// which can be overridden in `settings`.
     ///
-    /// @param rpm_package      A package to be added to the distrosync job.
+    /// @param rpm_package      A package hat will be used as candidate for the distrosync action.
     /// @param settings         A structure to override default goal settings. Only `strict`, `best`, and `clean_requirements_on_remove` values are used.
     // @replaces libdnf/hy-goal.h:function:hy_goal_distupgrade(HyGoal goal, DnfPackage *new_pkg)
     void add_rpm_distro_sync(const libdnf::rpm::Package & rpm_package, const libdnf::GoalJobSettings & settings = libdnf::GoalJobSettings());
 
-    /// Add distrosync job to the goal. In default it uses `clean_requirements_on_remove` with `false` value,
-    /// but it can be overridden in `setting`.
+    /// Add distrosync request to the goal. By default uses `clean_requirements_on_remove` set to `false`,
+    /// which can be overridden in `settings`.
     ///
-    /// @param package_set      A package_set to be added to the distrosync job.
+    /// @param package_set      A package_set containing candidates for the distrosync action.
     /// @param settings         A structure to override default goal settings. Only `strict`, `best`, and `clean_requirements_on_remove` values are used.
     // @replaces libdnf/hy-goal.h:function:hy_goal_distupgrade_selector(HyGoal goal, HySelector)
     void add_rpm_distro_sync(const libdnf::rpm::PackageSet & package_set, const libdnf::GoalJobSettings & settings = libdnf::GoalJobSettings());
@@ -214,7 +214,7 @@ public:
     // TODO(jmracek) Move transaction reports to Transaction class
     /// Resolve all jobs and return a transaction object
     ///
-    /// @param allow_erasing    A bool value When `true` it allows to remove installed package to resolve dependency problems
+    /// @param allow_erasing    When `true`, allows to remove installed packages to resolve dependency problems
     /// @return transaction object
     // @replaces libdnf/hy-goal.h:function:hy_goal_run_flags(HyGoal goal, DnfGoalActions flags)
     // @replaces dnf:dnf/base.py:method:Base().resolve(self, allow_erasing=False)
