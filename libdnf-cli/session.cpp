aee5ace59e29df4abce932f0b38165e62e4f94e5
Daniel Mach <dmach@redhat.com>
2021-08-23 08:57:19 +0200

    cli: Automatically set parsed command as selected

--- libdnf-cli/session.cpp
+++ libdnf-cli/session.cpp
@@ -38,11 +38,26 @@ Command::Command(Session & session, const std::string & program_name) : session{
 
 
 Command::Command(Command & parent, const std::string & name) : session{parent.session}, parent_command{&parent} {
-//    session = parent_command->session;
-
     // create a new command (owned by the arg_parser)
     argument_parser_command = session.get_argument_parser().add_new_command(name);
+
+    // set the command as selected when parsed
+    argument_parser_command->set_parse_hook_func([this](
+                                [[maybe_unused]] ArgumentParser::Argument * arg,
+                                [[maybe_unused]] const char * option,
+                                [[maybe_unused]] int argc,
+                                [[maybe_unused]] const char * const argv[]) {
+
+        // set the selected command only if a subcommand hasn't set it already
+        auto & session = this->get_session();
+        auto * selected_command = session.get_selected_command();
+        if (!selected_command || selected_command == session.get_root_command()) {
+            session.set_selected_command(this);
+        }
+        return true;
+    });
 }
+
 
 void Command::register_subcommand(std::unique_ptr<Command> subcommand, libdnf::cli::ArgumentParser::Group * group) {
     get_argument_parser_command()->register_command(subcommand->get_argument_parser_command());
@@ -52,4 +67,5 @@ void Command::register_subcommand(std::unique_ptr<Command> subcommand, libdnf::c
     subcommands.push_back(std::move(subcommand));
 }
 
-}  // libdnf::cli::session
+
+}  // namespace libdnf::cli::session
