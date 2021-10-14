2b929950eec67cfad8ae1064713ee93d44e6197e
Jaroslav Rohel <jrohel@redhat.com>
2021-10-14 15:29:09 +0200

    Create `sformat`: to format text with non-constant expression
    In C++20, the `format` function template requires that the format argument
    be convertible to std::string_view or std::wstring_view, and the conversion
    results in a constant expression and a valid format string for the arguments.
    Sometimes we do not know the format string at compile time because we use text
    translation at runtime. The commit adds `sformat` function template,
    which accepts the runtime computed format string.

---
+++ include/libdnf/utils/format.hpp
@@ -0,0 +1,38 @@
+/*
+Copyright (C) 2021 Red Hat, Inc.
+
+This file is part of libdnf: https://github.com/rpm-software-management/libdnf/
+
+Libdnf is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+Libdnf is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with libdnf.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#ifndef LIBDNF_UTILS_FORMAT_HPP
+#define LIBDNF_UTILS_FORMAT_HPP
+
+#include <fmt/format.h>
+
+
+namespace libdnf::utils {
+
+/// Format `args` according to the `runtime_format_string`, and return the result as a string.
+/// Unlike C++20 `std::format`, the format string of the `libdnf::sformat` function template
+/// does not have to be a constant expression.
+template <typename... Args>
+inline std::string sformat(std::string_view runtime_format_string, Args&&... args) {
+    return fmt::vformat(runtime_format_string, fmt::make_format_args(args...));
+}
+
+}
+
+#endif  // LIBDNF_UTILS_FORMAT_HPP
