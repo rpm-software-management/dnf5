98da82778b4a56467643f712e157f66c9a4f8164
Daniel Mach <daniel.mach@gmail.com>
2021-12-30 19:22:23 +0100

    output: Aways print install sizes in the transaction table
    Printing download sizes of upgraded packages
    and install sizes of removed packages next to each other
    didn't make much sense.
    Especially displaying install size of removed packages
    without additional context is confusing and pointless.

--- include/libdnf-cli/output/transaction_table.hpp
+++ include/libdnf-cli/output/transaction_table.hpp
@@ -300,10 +300,8 @@ bool print_transaction_table(Transaction & transaction) {
         scols_line_set_data(ln, COL_ARCH, pkg.get_arch().c_str());
         scols_line_set_data(ln, COL_EVR, pkg.get_evr().c_str());
         scols_line_set_data(ln, COL_REPO, pkg.get_repo_id().c_str());
-        uint64_t size = tspkg.get_action() == libdnf::transaction::TransactionItemAction::REMOVE
-                            ? pkg.get_install_size()
-                            : pkg.get_package_size();
-        scols_line_set_data(ln, COL_SIZE, libdnf::cli::utils::units::format_size(static_cast<int64_t>(size)).c_str());
+        auto tspkg_size = static_cast<int64_t>(pkg.get_install_size());
+        scols_line_set_data(ln, COL_SIZE, libdnf::cli::utils::units::format_size(tspkg_size).c_str());
         auto ce = scols_line_get_cell(ln, COL_NAME);
         scols_cell_set_color(ce, action_color(tspkg.get_action()));
 
@@ -317,10 +315,9 @@ bool print_transaction_table(Transaction & transaction) {
             scols_line_set_data(ln_replaced, COL_ARCH, replaced.get_arch().c_str());
             scols_line_set_data(ln_replaced, COL_EVR, replaced.get_evr().c_str());
             scols_line_set_data(ln_replaced, COL_REPO, replaced.get_repo_id().c_str());
-            uint64_t size = replaced.get_install_size();
-            scols_line_set_data(
-                ln_replaced, COL_SIZE, libdnf::cli::utils::units::format_size(static_cast<int64_t>(size)).c_str());
 
+            auto replaced_size = static_cast<int64_t>(replaced.get_install_size());
+            scols_line_set_data(ln_replaced, COL_SIZE, libdnf::cli::utils::units::format_size(replaced_size).c_str());
             auto replaced_color = action_color(libdnf::transaction::TransactionItemAction::REPLACED);
             auto obsoleted_color = action_color(libdnf::transaction::TransactionItemAction::OBSOLETED);
             scols_cell_set_color(scols_line_get_cell(ln_replaced, COL_EVR), replaced_color);
