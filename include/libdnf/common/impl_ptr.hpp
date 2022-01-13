dc640c8db9e9b2ff6e39f99979f5758f74041c4d
Jaroslav Rohel <jrohel@redhat.com>
2022-01-13 12:09:17 +0100

    ImplPtr: smart pointer, specialized in class implementation ownership
    ImplPtr owns the value it points to.
    This is an inline version implemented using only one header file.
    
    Differences from `std::unique_ptr`:
    - implements copy constructor - Constructs a ImplPtr that owns newly
      created instance value initialized from `src` or owns nothing if `src`
      owns nothing.
    
    - implements copy assignment operator - Copies the value pointed to by `src`.
      Not a pointer itself. If the destination owns nothing and `src` points to
      a value, a new instance of the value initialized from `src` is created.
      It does not do anything if both of them own nothing.
    
    - implements `T * operator->() noexcept` - `std::unique_ptr` implements
      only `const` version that returns pointer to non-constant value
    
    - implements `const T * operator->() const noexcept` - `std::unique_ptr` returns
      pointer to non-constant value
    
    - implements `T & operator*() noexcept` - `std::unique_ptr` implements
      only `const` version that returns reference to non-constant value
    
    - implements `const T & operator*() const noexcept` - `std::unique_ptr` returns
      reference to non-constant value
    
    - implements `T * get() noexcept` - `std::unique_ptr` implements
      only `const` version that returns pointer to non-constant value
    
    - implements `const T * get() const noexcept` - `std::unique_ptr` returns
      pointer to non-constant value
    
    - implements only the methods needed for the given purpose

---
+++ include/libdnf/common/impl_ptr.hpp
@@ -0,0 +1,86 @@
+/*
+Copyright Contributors to the libdnf project.
+
+This file is part of libdnf: https://github.com/rpm-software-management/libdnf/
+
+Libdnf is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 2.1 of the License, or
+(at your option) any later version.
+
+Libdnf is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with libdnf.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#ifndef LIBDNF_COMMON_IMPL_PTR_HPP
+#define LIBDNF_COMMON_IMPL_PTR_HPP
+
+
+namespace libdnf {
+
+template <class T>
+class ImplPtr {
+public:
+    /// Constructs an ImplPtr that owns nothing.
+    constexpr ImplPtr() noexcept = default;
+
+    /// Constructs an ImplPtr that takes ownership of `ptr`.
+    explicit constexpr ImplPtr(T * ptr) noexcept { this->ptr = ptr; }
+
+    /// Constructs an ImplPtr that owns newly created instance value initialized from `src` or
+    /// owns nothing if `src` owns nothing.
+    ImplPtr(const ImplPtr & src) : ptr(src.ptr ? new T(*src.ptr) : nullptr) {}
+
+    /// Constructs an ImplPtr by transferring ownership from `src` to *this and stores the null pointer in `src`.
+    constexpr ImplPtr(ImplPtr && src) noexcept : ptr(src.ptr) { src.ptr = nullptr; }
+
+    /// Copies the value pointed to by `src`, not the pointer itself.
+    /// If the destination owns nothing and `src` points to a value, a new instance of the value initialized from
+    /// `src` is created. Does not do anything if both of them own nothing.
+    ImplPtr & operator=(const ImplPtr & src) {
+        if (ptr != src.ptr) {
+            if (ptr) {
+                *ptr = *src.ptr;
+            } else {
+                ptr = new T(*src.ptr);
+            }
+        }
+        return *this;
+    }
+
+    /// Transfers ownership from `src` to *this and stores the null pointer in `src`.
+    ImplPtr & operator=(ImplPtr && src) noexcept {
+        if (ptr != src.ptr) {
+            delete ptr;
+            ptr = src.ptr;
+            src.ptr = nullptr;
+        }
+        return *this;
+    }
+
+    ~ImplPtr() { delete ptr; }
+
+    constexpr T * operator->() noexcept { return ptr; }
+
+    constexpr const T * operator->() const noexcept { return ptr; }
+
+    constexpr T & operator*() noexcept { return *ptr; }
+
+    constexpr const T & operator*() const noexcept { return *ptr; }
+
+    constexpr T * get() noexcept { return ptr; }
+
+    constexpr const T * get() const noexcept { return ptr; }
+
+private:
+    T * ptr{nullptr};  // object is owner of the pointer
+};
+
+}  // namespace libdnf
+
+#endif
