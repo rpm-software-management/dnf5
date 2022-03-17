acc9db12c5ab46b127f1746f70c77b07ca7a05cf
Lukáš Hrázký <lhrazky@redhat.com>
2022-03-17 14:14:18 +0100

    test/libdnf/run_tests: Support specifying test path to run as argument
    The test paths are those that are printed when running the binary, the
    form is "ClassName::test_name". Just the class name can be specified to
    run all tests from a class.

--- test/libdnf/run_tests.cpp
+++ test/libdnf/run_tests.cpp
@@ -89,7 +89,7 @@ public:
 };
 
 
-int main() {
+int main(int argc, char * argv[]) {
     // Create the event manager and test controller
     CPPUNIT_NS::TestResult controller;
 
@@ -110,10 +110,16 @@ int main() {
     CPPUNIT_NS::BriefTestProgressListener progress;
     controller.addListener(&progress);
 
+    std::string test_path;
+
+    if (argc > 1) {
+        test_path = argv[1];
+    }
+
     // Add the top suite to the test runner
     CPPUNIT_NS::TestRunner runner;
     runner.addTest(CPPUNIT_NS::TestFactoryRegistry::getRegistry().makeTest());
-    runner.run(controller);
+    runner.run(controller, test_path);
 
     // Print test in a compiler compatible format.
     CPPUNIT_NS::CompilerOutputter outputter(&result, CPPUNIT_NS::stdCOut());
