keszybz/libdnf

Unit tests for excludes/includes API

test/libdnf/rpm/test_package_sack.cpp
@@ -1,0 +1,121 @@
+/*
+Copyright Contributors to the libdnf project.
+
+This file is part of libdnf: https://github.com/rpm-software-management/libdnf/
+
+Libdnf is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+Libdnf is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with libdnf.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+
+#include "test_package_sack.hpp"
+
+#include "utils.hpp"
+
+#include "libdnf/rpm/package_sack.hpp"
+#include "libdnf/rpm/package_set.hpp"
+
+#include <filesystem>
+#include <set>
+#include <vector>
+
+
+CPPUNIT_TEST_SUITE_REGISTRATION(RpmPackageSackTest);
+
+using namespace libdnf::rpm;
+
+namespace {
+
+// make constructor public so we can create Package instances in the tests
+class TestPackage : public Package {
+public:
+    TestPackage(libdnf::Base & base, PackageId id) : libdnf::rpm::Package(base.get_weak_ptr(), id) {}
+};
+
+}  // namespace
+
+
+void RpmPackageSackTest::setUp() {
+    BaseTestCase::setUp();
+    add_repo_solv("solv-24pkgs");
+
+    pkgset = std::make_unique<PackageSet>(base);
+    pkg0 = std::make_unique<TestPackage>(base, libdnf::rpm::PackageId(0));
+    pkgset->add(*pkg0);
+}
+
+
+void RpmPackageSackTest::test_set_excludes() {
+    CPPUNIT_ASSERT(sack->get_excludes().size() == 0);
+    sack->set_excludes(*pkgset);
+    CPPUNIT_ASSERT(sack->get_excludes().contains(*pkg0) == true);
+    CPPUNIT_ASSERT(sack->get_excludes().size() == 1);
+}
+
+
+void RpmPackageSackTest::test_add_excludes() {
+    CPPUNIT_ASSERT(sack->get_excludes().contains(*pkg0) == false);
+    sack->add_excludes(*pkgset);
+    CPPUNIT_ASSERT(sack->get_excludes().contains(*pkg0) == true);
+    CPPUNIT_ASSERT(sack->get_excludes().size() == 1);
+
+    // add the same package again
+    sack->add_excludes(*pkgset);
+    CPPUNIT_ASSERT(sack->get_excludes().size() == 1);
+}
+
+
+void RpmPackageSackTest::test_remove_excludes() {
+    // remove package from empty excludes list does not fail
+    CPPUNIT_ASSERT(sack->get_excludes().contains(*pkg0) == false);
+    sack->remove_excludes(*pkgset);
+
+    // remove existing package
+    sack->set_excludes(*pkgset);
+    CPPUNIT_ASSERT(sack->get_excludes().contains(*pkg0) == true);
+    sack->remove_excludes(*pkgset);
+    CPPUNIT_ASSERT(sack->get_excludes().contains(*pkg0) == false);
+}
+
+
+void RpmPackageSackTest::test_set_includes() {
+    CPPUNIT_ASSERT(sack->get_includes().size() == 0);
+    sack->set_includes(*pkgset);
+    CPPUNIT_ASSERT(sack->get_includes().contains(*pkg0) == true);
+    CPPUNIT_ASSERT(sack->get_includes().size() == 1);
+}
+
+
+void RpmPackageSackTest::test_add_includes() {
+    CPPUNIT_ASSERT(sack->get_includes().contains(*pkg0) == false);
+    sack->add_includes(*pkgset);
+    CPPUNIT_ASSERT(sack->get_includes().contains(*pkg0) == true);
+    CPPUNIT_ASSERT(sack->get_includes().size() == 1);
+
+    // add the same package again
+    sack->add_includes(*pkgset);
+    CPPUNIT_ASSERT(sack->get_includes().size() == 1);
+}
+
+
+void RpmPackageSackTest::test_remove_includes() {
+    // remove package from empty includes list does not fail
+    CPPUNIT_ASSERT(sack->get_includes().contains(*pkg0) == false);
+    sack->remove_includes(*pkgset);
+
+    // remove existing package
+    sack->set_includes(*pkgset);
+    CPPUNIT_ASSERT(sack->get_includes().contains(*pkg0) == true);
+    sack->remove_includes(*pkgset);
+    CPPUNIT_ASSERT(sack->get_includes().contains(*pkg0) == false);
+}

rpm-software-management/dnf5

test/libdnf/rpm/test_package_sack.cpp
@@ -1,0 +1,125 @@
+/*
+Copyright Contributors to the libdnf project.
+
+This file is part of libdnf: https://github.com/rpm-software-management/libdnf/
+
+Libdnf is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+Libdnf is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with libdnf.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+
+#include "test_package_sack.hpp"
+
+#include "utils.hpp"
+
+#include "libdnf/rpm/package_sack.hpp"
+#include "libdnf/rpm/package_set.hpp"
+
+#include <filesystem>
+#include <set>
+#include <vector>
+
+
+CPPUNIT_TEST_SUITE_REGISTRATION(RpmPackageSackTest);
+
+using namespace libdnf::rpm;
+
+namespace {
+
+// make constructor public so we can create Package instances in the tests
+class TestPackage : public Package {
+public:
+    TestPackage(libdnf::Base & base, PackageId id) : libdnf::rpm::Package(base.get_weak_ptr(), id) {}
+};
+
+}  // namespace
+
+
+void RpmPackageSackTest::setUp() {
+    BaseTestCase::setUp();
+    add_repo_solv("solv-24pkgs");
+
+    pkgset = std::make_unique<PackageSet>(base);
+    pkg0 = std::make_unique<TestPackage>(base, libdnf::rpm::PackageId(0));
+    pkgset->add(*pkg0);
+}
+
+
+void RpmPackageSackTest::test_set_user_excludes() {
+    CPPUNIT_ASSERT(sack->get_user_excludes().size() == 0);
+    sack->set_user_excludes(*pkgset);
+    CPPUNIT_ASSERT(sack->get_user_excludes().contains(*pkg0) == true);
+    CPPUNIT_ASSERT(sack->get_user_excludes().size() == 1);
+    sack->clear_user_excludes();
+    CPPUNIT_ASSERT(sack->get_user_excludes().size() == 0);
+}
+
+
+void RpmPackageSackTest::test_add_user_excludes() {
+    CPPUNIT_ASSERT(sack->get_user_excludes().contains(*pkg0) == false);
+    sack->add_user_excludes(*pkgset);
+    CPPUNIT_ASSERT(sack->get_user_excludes().contains(*pkg0) == true);
+    CPPUNIT_ASSERT(sack->get_user_excludes().size() == 1);
+
+    // add the same package again
+    sack->add_user_excludes(*pkgset);
+    CPPUNIT_ASSERT(sack->get_user_excludes().size() == 1);
+}
+
+
+void RpmPackageSackTest::test_remove_user_excludes() {
+    // remove package from empty excludes list does not fail
+    CPPUNIT_ASSERT(sack->get_user_excludes().contains(*pkg0) == false);
+    sack->remove_user_excludes(*pkgset);
+
+    // remove existing package
+    sack->set_user_excludes(*pkgset);
+    CPPUNIT_ASSERT(sack->get_user_excludes().contains(*pkg0) == true);
+    sack->remove_user_excludes(*pkgset);
+    CPPUNIT_ASSERT(sack->get_user_excludes().contains(*pkg0) == false);
+}
+
+
+void RpmPackageSackTest::test_set_user_includes() {
+    CPPUNIT_ASSERT(sack->get_user_includes().size() == 0);
+    sack->set_user_includes(*pkgset);
+    CPPUNIT_ASSERT(sack->get_user_includes().contains(*pkg0) == true);
+    CPPUNIT_ASSERT(sack->get_user_includes().size() == 1);
+    sack->clear_user_includes();
+    CPPUNIT_ASSERT(sack->get_user_includes().size() == 0);
+}
+
+
+void RpmPackageSackTest::test_add_user_includes() {
+    CPPUNIT_ASSERT(sack->get_user_includes().contains(*pkg0) == false);
+    sack->add_user_includes(*pkgset);
+    CPPUNIT_ASSERT(sack->get_user_includes().contains(*pkg0) == true);
+    CPPUNIT_ASSERT(sack->get_user_includes().size() == 1);
+
+    // add the same package again
+    sack->add_user_includes(*pkgset);
+    CPPUNIT_ASSERT(sack->get_user_includes().size() == 1);
+}
+
+
+void RpmPackageSackTest::test_remove_user_includes() {
+    // remove package from empty includes list does not fail
+    CPPUNIT_ASSERT(sack->get_user_includes().contains(*pkg0) == false);
+    sack->remove_user_includes(*pkgset);
+
+    // remove existing package
+    sack->set_user_includes(*pkgset);
+    CPPUNIT_ASSERT(sack->get_user_includes().contains(*pkg0) == true);
+    sack->remove_user_includes(*pkgset);
+    CPPUNIT_ASSERT(sack->get_user_includes().contains(*pkg0) == false);
+}