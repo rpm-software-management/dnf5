572b6670aebf69d5b384a3084de7d35bc99c9d37
Jaroslav Rohel <jrohel@redhat.com>
2022-09-12 10:20:47 +0200

    HistoryDB: Create table with archs, use them as foreign keys in rpm table

---
+++ libdnf/transaction/db/archs.hpp
@@ -0,0 +1,37 @@
+/*
+Copyright Contributors to the libdnf project.
+
+This file is part of libdnf: https://github.com/rpm-software-management/libdnf/
+
+Libdnf is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 2.1 of the License, or
+(at your option) any later version.
+
+Libdnf is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with libdnf.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#ifndef LIBDNF_TRANSACTION_DB_ARCHS_HPP
+#define LIBDNF_TRANSACTION_DB_ARCHS_HPP
+
+#include "utils/sqlite3/sqlite3.hpp"
+
+#include <memory>
+
+namespace libdnf::transaction {
+
+/// Create a query (statement) that inserts new records to the 'archs' table
+std::unique_ptr<utils::SQLite3::Statement> archs_insert_if_not_exists_new_query(utils::SQLite3 & conn);
+
+/// Use a query to insert a new record to the 'archs' table
+int64_t archs_insert_if_not_exists(utils::SQLite3::Statement & query, const std::string & name);
+
+}  // namespace libdnf::transaction
+
+#endif  // LIBDNF_TRANSACTION_DB_ARCHS_HPP
