d0cf718792857e8f2f52a1396eb81161d85e1d67
Marek Blaha <mblaha@redhat.com>
2022-10-20 15:41:32 +0200

    dnf4convert: Method to generate system state from history
    Method reads dnf4 history database and resets system state of packages,
    groups, and environments to match the database.

--- libdnf/utils/dnf4convert/dnf4convert.cpp
+++ libdnf/utils/dnf4convert/dnf4convert.cpp
@@ -20,6 +20,7 @@ along with libdnf.  If not, see <https://www.gnu.org/licenses/>.
 #include "dnf4convert.hpp"
 
 #include "config_module.hpp"
+#include "utils/sqlite3/sqlite3.hpp"
 
 #include "libdnf/base/base.hpp"
 #include "libdnf/conf/config_parser.hpp"
@@ -27,6 +28,118 @@ along with libdnf.  If not, see <https://www.gnu.org/licenses/>.
 #include <filesystem>
 
 namespace libdnf::dnf4convert {
+
+// Query to get list of installed packages with versions and repoid.
+// The inner query selects id of the latest successful transaction item that touched
+// each NEVRA. Only REINSTALLED (10) action is skipped because it can be ordered
+// after respective REINSTALL action overwriting potential repoid change.
+// Packages with the latest action 3 (DOWNGRADED), 5 (OBSOLETED), 7 (UPGRADED), and
+// 8 (REMOVE) are eventually skiped as removed from the system.
+static constexpr const char * SQL_CURRENTLY_INSTALLED_PACKAGES = R"**(
+    SELECT
+        "ti"."item_id",
+        "ti"."action",
+        "ti"."reason",
+        "rpm"."name",
+        "rpm"."epoch",
+        "rpm"."version",
+        "rpm"."release",
+        "rpm"."arch",
+        "repo"."repoid"
+    FROM
+        "trans_item" "ti"
+    JOIN
+        "rpm" USING ("item_id")
+    JOIN
+        "repo" ON ("repo"."id" = "ti"."repo_id")
+    WHERE
+        "ti"."id" IN (
+            SELECT MAX("trans_item"."id")
+            FROM "trans_item"
+            JOIN "rpm" USING ("item_id")
+            JOIN "trans" ON ("trans"."id" = "trans_item"."trans_id")
+            WHERE "trans_item"."action" != 10
+                  AND "trans_item"."state" = 1
+                  AND "trans"."state" = 1
+            GROUP BY "rpm"."name", "rpm"."epoch", "rpm"."version", "rpm"."release", "rpm"."arch")
+    AND "ti"."action" NOT IN (3, 5, 7, 8)
+    ORDER BY "ti"."item_id" DESC
+)**";
+
+
+// Query to get list of currently installed comps groups.
+// The inner query select the latest successful transaction item touching each group.
+// Only actions 1 (INSTALL), 2 (UPGRADE), and 8 (REMOVE) are taken into account.
+static constexpr const char * SQL_CURRENTLY_INSTALLED_GROUPS = R"**(
+    SELECT
+        "ti"."item_id",
+        "ti"."reason",
+        "cg"."groupid"
+    FROM
+        "trans_item" "ti"
+    JOIN
+        "comps_group" "cg" USING ("item_id")
+    WHERE
+        "ti"."id" IN (
+            SELECT MAX("trans_item"."id")
+            FROM "trans_item"
+            JOIN "comps_group" USING ("item_id")
+            JOIN "trans" ON ("trans"."id" = "trans_item"."trans_id")
+            WHERE "trans_item"."action" IN (1, 6, 8)
+                  AND "trans_item"."state" = 1
+                  AND "trans"."state" = 1
+            GROUP BY "comps_group"."groupid")
+        AND "ti"."action" <> 8
+)**";
+
+
+// Query to get list of packages that were installed with given group.
+static constexpr const char * SQL_GROUP_PACKAGES = R"**(
+SELECT
+    DISTINCT "name"
+FROM
+    "comps_group_package"
+WHERE
+    "installed" = 1
+    AND "group_id" = ?
+)**";
+
+
+// Query to get list of currently installed comps environments.
+// The inner query select the latest successful transaction item touching each group.
+// Only actions 1 (INSTALL), 2 (UPGRADE), and 8 (REMOVE) are taken into account.
+static constexpr const char * SQL_CURRENTLY_INSTALLED_ENVIRONMENTS = R"**(
+    SELECT
+        "ti"."item_id",
+        "ce"."environmentid"
+    FROM
+        "trans_item" "ti"
+    JOIN
+        "comps_environment" "ce" USING ("item_id")
+    WHERE
+        "ti"."id" IN (
+            SELECT MAX("trans_item"."id")
+            FROM "trans_item"
+            JOIN "comps_environment" USING ("item_id")
+            JOIN "trans" ON ("trans"."id" = "trans_item"."trans_id")
+            WHERE "trans_item"."action" IN (1, 6, 8)
+                  AND "trans_item"."state" = 1
+                  AND "trans"."state" = 1
+            GROUP BY "comps_environment"."environmentid")
+        AND "ti"."action" <> 8
+)**";
+
+
+// Query to get list of packages that were installed with given group.
+static constexpr const char * SQL_ENVIRONMENT_GROUPS = R"**(
+SELECT
+    DISTINCT "groupid"
+FROM
+    "comps_environment_group"
+WHERE
+    "installed" = 1
+    AND "environment_id" = ?
+)**";
 
 
 std::map<std::string, libdnf::system::ModuleState> Dnf4Convert::read_module_states() {
@@ -62,6 +175,163 @@ std::map<std::string, libdnf::system::ModuleState> Dnf4Convert::read_module_stat
         }
     }
     return module_states;
+}
+
+
+bool Dnf4Convert::read_package_states_from_history(
+    std::map<std::string, libdnf::system::PackageState> & package_states,
+    std::map<std::string, libdnf::system::NevraState> & nevra_states,
+    std::map<std::string, libdnf::system::GroupState> & group_states,
+    std::map<std::string, libdnf::system::EnvironmentState> & environment_states) {
+    auto & logger = *base->get_logger();
+    // get dnf4 history database path (installroot/persistdir/history.sqlite)
+    auto & config = base->get_config();
+    std::filesystem::path path{config.installroot().get_value()};
+    path /= std::filesystem::path(config.persistdir().get_value()).relative_path();
+    path /= "history.sqlite";
+
+    logger.debug("Loading system state from dnf4 history database \"{}\"", path.string());
+
+    // These arrays temporarily hold data read from sqlite database. They are converted
+    // to libdnf::system::* objects once everything is read from db.
+    std::vector<std::tuple<rpm::Nevra, transaction::TransactionItemReason, std::string>> installed_packages;
+    std::vector<std::tuple<std::string, transaction::TransactionItemReason, std::set<std::string>>> installed_groups;
+    std::vector<std::tuple<std::string, std::set<std::string>>> installed_environments;
+
+    // Auxiliary map to optimalize looking up groups for package
+    // map package_name -> {set of group_id}
+    std::map<std::string, std::set<std::string>> package_groups;
+    // Auxiliary set of actually installed package names. Used to filter out group
+    // packages that are not currently installed.
+    std::set<std::string> installed_packages_names{};
+
+    // First mine all the data from sqlite database.
+    try {
+        auto conn = std::make_unique<libdnf::utils::SQLite3>(path.native());
+
+        auto query_environments =
+            std::make_unique<libdnf::utils::SQLite3::Query>(*conn, SQL_CURRENTLY_INSTALLED_ENVIRONMENTS);
+        std::set<std::string> groups_in_environments;
+        while (query_environments->step() == libdnf::utils::SQLite3::Statement::StepResult::ROW) {
+            auto environment_id = query_environments->get<std::string>("environmentid");
+            auto item_id = query_environments->get<int64_t>("item_id");
+            auto grps_query = std::make_unique<libdnf::utils::SQLite3::Query>(*conn, SQL_ENVIRONMENT_GROUPS);
+            grps_query->bindv(item_id);
+            std::set<std::string> groups;
+            while (grps_query->step() == libdnf::utils::SQLite3::Statement::StepResult::ROW) {
+                std::string groupid = grps_query->get<std::string>("groupid");
+                groups.emplace(groupid);
+                groups_in_environments.insert(std::move(groupid));
+            }
+            installed_environments.emplace_back(environment_id, std::move(groups));
+        }
+
+        auto query_groups = std::make_unique<libdnf::utils::SQLite3::Query>(*conn, SQL_CURRENTLY_INSTALLED_GROUPS);
+        while (query_groups->step() == libdnf::utils::SQLite3::Statement::StepResult::ROW) {
+            auto group_id = query_groups->get<std::string>("groupid");
+            auto item_id = query_groups->get<int64_t>("item_id");
+            auto pkgs_query = std::make_unique<libdnf::utils::SQLite3::Query>(*conn, SQL_GROUP_PACKAGES);
+            pkgs_query->bindv(item_id);
+            std::set<std::string> packages;
+            while (pkgs_query->step() == libdnf::utils::SQLite3::Statement::StepResult::ROW) {
+                auto pkg_name = pkgs_query->get<std::string>("name");
+                packages.emplace(pkg_name);
+                // keep track of every group the package is part of
+                package_groups[pkg_name].insert(group_id);
+            }
+            // reason stored in dnf4 history db is always USER
+            // if the group is part of installed environment, mark it as DEPENDENCY
+            installed_groups.emplace_back(
+                group_id,
+                groups_in_environments.contains(group_id) ? transaction::TransactionItemReason::DEPENDENCY
+                                                          : transaction::TransactionItemReason::USER,
+                std::move(packages));
+        }
+
+        auto query_pkgs = std::make_unique<libdnf::utils::SQLite3::Query>(*conn, SQL_CURRENTLY_INSTALLED_PACKAGES);
+        while (query_pkgs->step() == libdnf::utils::SQLite3::Statement::StepResult::ROW) {
+            auto reason = static_cast<transaction::TransactionItemReason>(query_pkgs->get<int>("reason"));
+            auto repo_id = query_pkgs->get<std::string>("repoid");
+            rpm::Nevra pkg_nevra;
+            pkg_nevra.set_name(query_pkgs->get<std::string>("name"));
+            pkg_nevra.set_epoch(query_pkgs->get<std::string>("epoch"));
+            pkg_nevra.set_version(query_pkgs->get<std::string>("version"));
+            pkg_nevra.set_release(query_pkgs->get<std::string>("release"));
+            pkg_nevra.set_arch(query_pkgs->get<std::string>("arch"));
+
+            installed_packages_names.insert(pkg_nevra.get_name());
+            installed_packages.emplace_back(pkg_nevra, reason, repo_id);
+        }
+
+    } catch (const libdnf::utils::SQLite3Error & e) {
+        logger.debug("Reading from history database failed: {}", e.what());
+        return false;
+    }
+
+    // Convert all database data to system::state::* objects
+
+    // Clear the current packages state
+    package_states.clear();
+    nevra_states.clear();
+    group_states.clear();
+    environment_states.clear();
+
+    for (const auto & [pkg_nevra, original_reason, repo_id] : installed_packages) {
+        // set from_repo attribute in nevras.toml
+        libdnf::system::NevraState nevra_state;
+        nevra_state.from_repo = repo_id;
+        nevra_states.emplace(rpm::to_nevra_string(pkg_nevra), std::move(nevra_state));
+
+        transaction::TransactionItemReason reason = original_reason;
+        if (reason == transaction::TransactionItemReason::GROUP) {
+            // group packages need a group they belong to
+            if (package_groups.find(pkg_nevra.get_name()) == package_groups.end()) {
+                // in case there is no such group, change the reason to USER
+                reason = transaction::TransactionItemReason::USER;
+            } else {
+                continue;
+            }
+        } else if (reason == transaction::TransactionItemReason::NONE) {
+            // Packages installed by another tool (e.g. rpm) can have (once they get
+            // upgraded) reason NONE. Change it to EXTERNAL_USER.
+            reason = transaction::TransactionItemReason::EXTERNAL_USER;
+        }
+        // for non-group packages save the reason in packages.toml
+        libdnf::system::PackageState package_state;
+        package_state.reason = transaction::transaction_item_reason_to_string(reason);
+        package_states.emplace(pkg_nevra.get_name() + "." + pkg_nevra.get_arch(), std::move(package_state));
+    }
+
+    // Store the groups state in groups.toml.
+    for (const auto & [group_id, reason, candidate_packages] : installed_groups) {
+        libdnf::system::GroupState group_state;
+        if (reason == transaction::TransactionItemReason::USER) {
+            group_state.userinstalled = true;
+        } else {
+            group_state.userinstalled = false;
+        }
+        for (const auto & candidate_pkg : candidate_packages) {
+            // store only actually installed group packages
+            if (installed_packages_names.contains(candidate_pkg)) {
+                group_state.packages.emplace_back(std::move(candidate_pkg));
+            }
+        }
+        group_states.emplace(group_id, std::move(group_state));
+    }
+
+    // Finally store the environmental groups state in environments.toml.
+    for (const auto & [environment_id, candidate_groups] : installed_environments) {
+        libdnf::system::EnvironmentState environment_state;
+        for (const auto & candidate_grp : candidate_groups) {
+            // store only actually installed groups
+            if (group_states.contains(candidate_grp)) {
+                environment_state.groups.emplace_back(std::move(candidate_grp));
+            }
+        }
+        environment_states.emplace(environment_id, std::move(environment_state));
+    }
+
+    return true;
 }
 
 
--- libdnf/utils/dnf4convert/dnf4convert.hpp
+++ libdnf/utils/dnf4convert/dnf4convert.hpp
@@ -42,6 +42,19 @@ public:
     /// @return The map {module_name -> ModuleState object}
     std::map<std::string, libdnf::system::ModuleState> read_module_states();
 
+    /// Reads installed packages, groups and environments from dnf4 history database.
+    /// The state is then stored in parameters.
+    /// @param[out] package_states Map {pkg.NA -> PackageState} for installed packages
+    /// @param[out] nevra_states Map {pkg.NEVRA -> NevraState} for installed packages
+    /// @param[out] group_states Map {group.group_id -> GroupState} for installed groups
+    /// @param[out] environment_states Map {environment.environment_id -> EnvironmentState} for installed environmental groups
+    /// @return True if packages were read successfully, False in case of database reading error.
+    bool read_package_states_from_history(
+        std::map<std::string, libdnf::system::PackageState> & package_states,
+        std::map<std::string, libdnf::system::NevraState> & nevra_states,
+        std::map<std::string, libdnf::system::GroupState> & group_states,
+        std::map<std::string, libdnf::system::EnvironmentState> & environment_states);
+
 private:
     BaseWeakPtr base;
 };
