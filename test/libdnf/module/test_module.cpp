75b9bc4e4c965384948abde7ab480f91055c066d
Pavla Kratochvilova <pkratoch@redhat.com>
2022-11-25 14:23:59 +0100

    modules: Change test_load() to query the tested module

--- test/libdnf/module/test_module.cpp
+++ test/libdnf/module/test_module.cpp
@@ -46,32 +46,23 @@ void ModuleTest::test_load() {
     auto module_sack = base.get_module_sack();
     CPPUNIT_ASSERT_EQUAL((size_t)10, module_sack->get_modules().size());
 
-    // TODO(pkratoch): Change this once individual modules can be queried
-    bool meson_checked = false;
-    for (auto & module_item : module_sack->get_modules()) {
-        if (module_item->get_name() != "meson") {
-            continue;
-        }
-        CPPUNIT_ASSERT_EQUAL(std::string("meson"), module_item->get_name());
-        CPPUNIT_ASSERT_EQUAL(std::string("master"), module_item->get_stream());
-        CPPUNIT_ASSERT_EQUAL(20180816151613ll, module_item->get_version());
-        CPPUNIT_ASSERT_EQUAL(std::string("06d0a27d"), module_item->get_context());
-        CPPUNIT_ASSERT_EQUAL(std::string("x86_64"), module_item->get_arch());
-        CPPUNIT_ASSERT_EQUAL(
-            std::string("meson:master:20180816151613:06d0a27d:x86_64"), module_item->get_full_identifier());
-        CPPUNIT_ASSERT_EQUAL(std::string("The Meson Build system"), module_item->get_summary());
-        CPPUNIT_ASSERT_EQUAL(
-            std::string(
-                "Meson is an open source build system meant to be both extremely fast, and, even more importantly, "
-                "as user friendly as possible.\nThe main design point of Meson is that every moment a developer "
-                "spends writing or debugging build definitions is a second wasted. So is every second spent "
-                "waiting for the build system to actually start compiling code."),
-            module_item->get_description());
-        CPPUNIT_ASSERT_EQUAL(
-            std::string("ninja;platform:[f29,f30,f31]"), module_item->get_module_dependencies_string());
-        meson_checked = true;
-    }
-    CPPUNIT_ASSERT_EQUAL(true, meson_checked);
+    ModuleQuery query = *new ModuleQuery(base, false);
+    query.filter_name("meson");
+    auto meson = query.get();
+    CPPUNIT_ASSERT_EQUAL(std::string("meson"), meson.get_name());
+    CPPUNIT_ASSERT_EQUAL(std::string("master"), meson.get_stream());
+    CPPUNIT_ASSERT_EQUAL(20180816151613ll, meson.get_version());
+    CPPUNIT_ASSERT_EQUAL(std::string("06d0a27d"), meson.get_context());
+    CPPUNIT_ASSERT_EQUAL(std::string("x86_64"), meson.get_arch());
+    CPPUNIT_ASSERT_EQUAL(std::string("meson:master:20180816151613:06d0a27d:x86_64"), meson.get_full_identifier());
+    CPPUNIT_ASSERT_EQUAL(std::string("The Meson Build system"), meson.get_summary());
+    CPPUNIT_ASSERT_EQUAL(
+        std::string("Meson is an open source build system meant to be both extremely fast, and, even more importantly, "
+                    "as user friendly as possible.\nThe main design point of Meson is that every moment a developer "
+                    "spends writing or debugging build definitions is a second wasted. So is every second spent "
+                    "waiting for the build system to actually start compiling code."),
+        meson.get_description());
+    CPPUNIT_ASSERT_EQUAL(std::string("ninja;platform:[f29,f30,f31]"), meson.get_module_dependencies_string());
 
     CPPUNIT_ASSERT_EQUAL(std::string(""), module_sack->get_default_stream("meson"));
     CPPUNIT_ASSERT_EQUAL(std::string("main"), module_sack->get_default_stream("berries"));
