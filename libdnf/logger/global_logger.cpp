e0e372ab6a9c5b2d57fb22d07d556a8764fbc2f6
Jaroslav Rohel <jrohel@redhat.com>
2022-12-18 16:36:13 +0100

    GlobalLogger: Class for setting up context-free logging from libraries
    Librepo and libmodulmd use the GLib logging system. This class sets up
    the routing of messages from these libraries to the desired logger.
    The messages do not contain information about the libdnf context (which
    Base object they are related to).

---
+++ libdnf/logger/global_logger.cpp
@@ -0,0 +1,55 @@
+/*
+Copyright Contributors to the libdnf project.
+
+This file is part of libdnf: https://github.com/rpm-software-management/libdnf/
+
+Libdnf is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 2.1 of the License, or
+(at your option) any later version.
+
+Libdnf is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with libdnf.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#include "libdnf/logger/global_logger.hpp"
+
+#include "logger/glib_log_handler.hpp"
+#include "repo/librepo.hpp"
+
+#include "libdnf/common/exception.hpp"
+
+namespace libdnf {
+
+static GlibLogHandler * librepo_logger{nullptr};
+static GlibLogHandler * libmodulemd_logger{nullptr};
+
+GlobalLogger::GlobalLogger() {
+    libdnf_assert(librepo_logger == nullptr, "Only one GlobalLogger can exist at a time");
+}
+
+GlobalLogger::~GlobalLogger() {
+    unset();
+}
+
+void GlobalLogger::set(Logger & destination, Logger::Level verbosity) {
+    if (librepo_logger) {
+        unset();
+    }
+    librepo_logger = new GlibLogHandler(destination, "librepo", verbosity);
+    libmodulemd_logger = new GlibLogHandler(destination, "libmodulemd", verbosity);
+}
+
+void GlobalLogger::unset() noexcept {
+    delete libmodulemd_logger;
+    libmodulemd_logger = nullptr;
+    delete librepo_logger;
+    librepo_logger = nullptr;
+}
+
+}  // namespace libdnf
