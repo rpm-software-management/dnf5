b44e09637d10bc7572c9f34e8587ebaa00c502ec
Aleš Matěj <amatej@redhat.com>
2023-01-23 13:48:45 +0100

    Run swig manually in the readthedocs workflow
    We need to generate the bindings files so that they can be used to
    generate python docs.
    Readthedocs doesn't have to do a full build in order to generate
    documentation so it doesn't use the cmake specified workflow. It has a
    custom workflow that needs to run swig manually.
    
    The approach here tries to use all interface files except `shared.i`, this may
    break in the future if there is for example another shared file. The
    actual list is hardcoded in cmake: https://github.com/rpm-software-management/dnf5/blob/main/bindings/CMakeLists.txt#L51-L56

--- doc/setup.py
+++ doc/setup.py
@@ -25,6 +25,18 @@ def configure(src_path, dst_path, substitutions):
     open(dst_path, "w").write(data)
 
 
+def generate_bindings_from_dir(in_dir, out_dir):
+    swig_options = ["-python", "-DSWIGWORDSIZE64", "-doxygen", "-relativeimport", "-outdir", out_dir, "-c++"]
+    swig_includes = ["-I" + os.path.join(DIR, "../include"), "-I" + os.path.join(DIR, "../common"), "-I/usr/include/python3.11"]
+
+    for in_file in os.listdir(in_dir):
+        # exclude shared.i which is not a standalone interface file
+        # it is included in other files.
+        if in_file.endswith(".i") and in_file != "shared.i":
+            print("Generating bindings for: " + in_file)
+            subprocess.run(["/usr/bin/swig"] + swig_options + ["-interface", "_" + in_file[:-2]] + swig_includes + [os.path.join(in_dir, in_file)], cwd=DIR, check=True)
+
+
 # configure the .in files
 configure("Doxyfile.in", "Doxyfile", {"@CMAKE_SOURCE_DIR@": os.path.join(DIR, "..")})
 configure("conf.py.in", "conf.py", {"@CMAKE_CURRENT_BINARY_DIR@": DIR})
@@ -34,6 +46,16 @@ configure("conf.py.in", "conf.py", {"@CMAKE_CURRENT_BINARY_DIR@": DIR})
 print("Running doxygen...")
 subprocess.run(["doxygen"], cwd=DIR, check=True)
 
+
+# run swig manually to generate Python bindings which are then used to generate Python API docs
+print("Running SWIG...")
+
+# libdnf5
+# Generate bindings outside of doc dir, into their python bindings dir. This has to match with path provided to autoapi_dirs in conf.py.in
+generate_bindings_from_dir(os.path.join(DIR + "/../bindings/libdnf5"), os.path.join(DIR + "/../bindings/python3/libdnf5"))
+
+# libdnf5-cli
+generate_bindings_from_dir(os.path.join(DIR + "/../bindings/libdnf5_cli"), os.path.join(DIR + "/../bindings/python3/libdnf5_cli"))
 
 # no setup() is called
 # this file only configures files for building docs in Read the Docs
