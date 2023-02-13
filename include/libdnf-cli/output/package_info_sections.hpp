d1d0766de1582e3c1cf3847435cdd56a3511212e
Marek Blaha <mblaha@redhat.com>
2023-02-13 14:06:33 +0100

    libdnf-cli: Packages info by sections
    Class to handle output of the `dnf info` command.

---
+++ include/libdnf-cli/output/package_info_sections.hpp
@@ -0,0 +1,51 @@
+/*
+Copyright Contributors to the libdnf project.
+
+This file is part of libdnf: https://github.com/rpm-software-management/libdnf/
+
+Libdnf is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 2.1 of the License, or
+(at your option) any later version.
+
+Libdnf is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with libdnf.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+
+#ifndef LIBDNF_CLI_OUTPUT_PACKAGE_INFO_SECTIONS_HPP
+#define LIBDNF_CLI_OUTPUT_PACKAGE_INFO_SECTIONS_HPP
+
+#include "package_list_sections.hpp"
+
+#include "libdnf/rpm/package_set.hpp"
+
+#include <libsmartcols/libsmartcols.h>
+
+#include <string>
+
+namespace libdnf::cli::output {
+
+class PackageInfoSections : public PackageListSections {
+public:
+    PackageInfoSections() : PackageListSections() {}
+    bool virtual add_section(
+        const std::string & heading,
+        const libdnf::rpm::PackageSet & pkg_set,
+        const std::unique_ptr<PkgColorizer> & colorizer = nullptr,
+        const std::map<libdnf::rpm::PackageId, std::vector<libdnf::rpm::Package>> & obsoletes = {}) override;
+
+    void setup_cols() override;
+
+private:
+    struct libscols_line * add_line(const std::string & key, const std::string & value);
+};
+
+}  // namespace libdnf::cli::output
+
+#endif  // LIBDNF_CLI_OUTPUT_PACKAGE_INFO_SECTIONS_HPP
