e56de7486454baf65e08c1cb4329161b0a5d9598
Jan Kolarik <jkolarik@redhat.com>
2023-02-20 08:19:20 +0000

    vars: Add API test for accessing undefined vars

---
+++ test/python3/libdnf5/conf/test_vars.py
@@ -0,0 +1,24 @@
+# Copyright Contributors to the libdnf project.
+#
+# This file is part of libdnf: https://github.com/rpm-software-management/libdnf/
+#
+# Libdnf is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# Libdnf is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with libdnf.  If not, see <https://www.gnu.org/licenses/>.
+
+import base_test_case
+
+
+class TestVars(base_test_case.BaseTestCase):
+    def test_getting_undefined_variable(self):
+        vars = self.base.get_vars()
+        self.assertRaises(IndexError, vars.get_value, "undefined")
