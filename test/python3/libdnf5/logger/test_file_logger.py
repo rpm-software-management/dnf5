e8bcf77bde468c0e95087564fc53718e3ab17f55
Jan Kolarik <jkolarik@redhat.com>
2023-03-06 12:54:46 +0000

    Add file logger factory Python API tests

---
+++ test/python3/libdnf5/logger/test_file_logger.py
@@ -0,0 +1,47 @@
+# Copyright Contributors to the libdnf project.
+#
+# This file is part of libdnf: https://github.com/rpm-software-management/libdnf/
+#
+# Libdnf is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# Libdnf is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with libdnf.  If not, see <https://www.gnu.org/licenses/>.
+
+import libdnf5
+
+import base_test_case
+
+import os
+import shutil
+
+
+class TestFileLogger(base_test_case.BaseTestCase):
+    def setUp(self):
+        super().setUp()
+        config = self.base.get_config()
+        config.logdir = "FileLoggerTestLogDir"
+        self.full_log_path = os.path.join(config.installroot, config.logdir, libdnf5.logger.FILE_LOGGER_FILENAME)
+
+    def tearDown(self):
+        super().tearDown()
+        shutil.rmtree(os.path.dirname(self.full_log_path), ignore_errors=True)
+
+    def test_file_logger_create(self):
+        self.assertFalse(os.path.exists(self.full_log_path))
+        _ = libdnf5.logger.create_file_logger(self.base)
+        self.assertTrue(os.path.exists(self.full_log_path))
+
+    def test_file_logger_add(self):
+        log_router = self.base.get_logger()
+        loggers_count_before = log_router.get_loggers_count()
+        logger = libdnf5.logger.create_file_logger(self.base)
+        log_router.add_logger(logger)
+        self.assertEqual(loggers_count_before + 1, log_router.get_loggers_count())
