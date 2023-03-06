98ad025df5ab0cbfc124b3b23fd4a079ea70cb50
Jan Kolarik <jkolarik@redhat.com>
2023-03-06 12:52:08 +0000

    logger: Add factory method for creating file logger
    Create helper method for easily creating a file logger based on configured file path.
    
    This can be used to easily setup file logging for API users.

---
+++ include/libdnf/logger/factory.hpp
@@ -0,0 +1,40 @@
+/*
+Copyright Contributors to the libdnf project.
+
+This file is part of libdnf: https://github.com/rpm-software-management/libdnf/
+
+Libdnf is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 2.1 of the License, or
+(at your option) any later version.
+
+Libdnf is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with libdnf.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#ifndef LIBDNF_LOGGER_FACTORY_HPP
+#define LIBDNF_LOGGER_FACTORY_HPP
+
+#include "logger.hpp"
+
+#include "libdnf/base/base.hpp"
+
+
+namespace libdnf {
+
+// File logger destination filename.
+constexpr const char * FILE_LOGGER_FILENAME = "dnf5.log";
+
+/// @brief Helper method for creating a file logger.
+/// @param base Reference to Base for loading the configured logger path.
+/// @return Instance of a new file logger.
+std::unique_ptr<libdnf::Logger> create_file_logger(libdnf::Base & base);
+
+}  // namespace libdnf
+
+#endif
