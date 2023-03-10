70f691b1ea9f3cb7d9b3576ebbbd896602ecb589
Jaroslav Mracek <jmracek@redhat.com>
2023-03-10 12:03:54 +0100

    Add capability to find binaries to resolve_spec
    It will enhance UX when usel want to install package according to binary
    name without additional search steps.
    
    It will allow to install open-ssh using `dnf5 install ssh`.
    
    Resolves: https://github.com/rpm-software-management/dnf5/issues/338

--- include/libdnf/base/goal_elements.hpp
+++ include/libdnf/base/goal_elements.hpp
@@ -125,6 +125,9 @@ public:
     /// Important for queries that resolve SPEC
     bool with_filenames{true};
     /// Important for queries that resolve SPEC
+    /// It will check whether SPEC is a binary -> /usr/(s)bin/<SPEC>
+    bool with_binaries{true};
+    /// Important for queries that resolve SPEC
     std::vector<libdnf::rpm::Nevra::Form> nevra_forms{};
 
     /// these flags are used only for group resolving
