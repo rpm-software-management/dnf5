2f18089602c770db72efa53634edd84f22d47a37
Jan Kolarik <jkolarik@redhat.com>
2023-03-17 13:10:11 +0000

    Add Python API tests for unsupported arguments in goal

--- test/python3/libdnf5/base/test_goal.py
+++ test/python3/libdnf5/base/test_goal.py
@@ -50,3 +50,21 @@ class TestGoal(unittest.TestCase):
         goal.add_install('https://i-dont-exist.com')
 
         self.assertRaises(RuntimeError, goal.resolve)
+
+    def test_unsupported_argument_add_remove(self):
+        # Try passing unsupported argument to add_remove() method
+        base = libdnf5.base.Base()
+        base.setup()
+
+        goal = libdnf5.base.Goal(base)
+
+        self.assertRaises(RuntimeError, goal.add_remove, 'pkg.rpm')
+
+    def test_group_rpm_reason_change_without_id(self):
+        # Try changing group reason without group_id
+        base = libdnf5.base.Base()
+        base.setup()
+
+        goal = libdnf5.base.Goal(base)
+
+        self.assertRaises(RuntimeError, goal.add_rpm_reason_change, '@fake-group-spec', libdnf5.base.transaction.TransactionItemReason_GROUP, '')
