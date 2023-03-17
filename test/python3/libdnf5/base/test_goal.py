53f24dc6734c607732057e012ab4b43de3eb6612
Jan Kolarik <jkolarik@redhat.com>
2023-03-17 10:53:41 +0000

    Add Python API tests for goal resolving use cases
    Tests for throwing goal resolving problems and move them to a separate test file.

---
+++ test/python3/libdnf5/base/test_goal.py
@@ -0,0 +1,52 @@
+# Copyright Contributors to the libdnf project.
+#
+# This file is part of libdnf: https://github.com/rpm-software-management/libdnf/
+#
+# Libdnf is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# Libdnf is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with libdnf.  If not, see <https://www.gnu.org/licenses/>.
+
+import unittest
+
+import libdnf5.base
+
+
+class TestGoal(unittest.TestCase):
+    def test_missing_setup_goal_resolve(self):
+        # Create a new Base object
+        base = libdnf5.base.Base()
+
+        # Create a new empty Goal
+        goal = libdnf5.base.Goal(base)
+
+        # Try to resolve the goal without running base.setup()
+        self.assertRaises(RuntimeError, goal.resolve)
+
+    def test_missing_rpm_goal_resolve(self):
+        # Try to resolve the goal using unknown RPM package
+        base = libdnf5.base.Base()
+        base.setup()
+
+        goal = libdnf5.base.Goal(base)
+        goal.add_install('unknown_pkg.rpm')
+
+        self.assertRaises(RuntimeError, goal.resolve)
+
+    def test_invalid_url_goal_resolve(self):
+        # Try to resolve the goal using invalid URL
+        base = libdnf5.base.Base()
+        base.setup()
+
+        goal = libdnf5.base.Goal(base)
+        goal.add_install('https://i-dont-exist.com')
+
+        self.assertRaises(RuntimeError, goal.resolve)
