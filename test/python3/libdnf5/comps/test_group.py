2c13de4b93091f2cabfca4b93a2c80a2843c5c11
Jan Kolarik <jkolarik@redhat.com>
2023-03-27 05:43:08 +0000

    Add Python API test for resolving group transaction
    This is intended to cover the use case from #392.

--- test/python3/libdnf5/comps/test_group.py
+++ test/python3/libdnf5/comps/test_group.py
@@ -16,8 +16,6 @@
 # along with libdnf.  If not, see <https://www.gnu.org/licenses/>.
 
 
-import unittest
-
 import libdnf5
 
 import base_test_case
@@ -27,7 +25,7 @@ class TestGroup(base_test_case.BaseTestCase):
     def test_group(self):
         self.add_repo_repomd("repomd-comps-core")
         q_core = libdnf5.comps.GroupQuery(self.base)
-        core = q_core.get()
+        _ = q_core.get()
 
     def test_group_query_without_setup(self):
         # Create a new Base object
@@ -35,3 +33,28 @@ class TestGroup(base_test_case.BaseTestCase):
 
         # Try to create a group query without running base.setup()
         self.assertRaises(RuntimeError, libdnf5.comps.GroupQuery, base)
+
+    def test_group_install_remove(self):
+        # Check group is correctly registered after install
+        # and so it could be removed after that
+        self.add_repo_repomd("repomd-comps-core")
+
+        goal_install = libdnf5.base.Goal(self.base)
+        goal_install.add_install("@core")
+
+        transaction_install = goal_install.resolve()
+        self.assertEqual(transaction_install.get_problems(),
+                         libdnf5.base.GoalProblem_NO_PROBLEM)
+        transaction_install.run()
+        self.assertFalse(transaction_install.get_transaction_problems())
+
+        self.repo_sack.update_and_load_enabled_repos(True)
+
+        goal_remove = libdnf5.base.Goal(self.base)
+        goal_remove.add_remove("@core")
+
+        transaction_remove = goal_remove.resolve()
+        self.assertEqual(transaction_remove.get_problems(),
+                         libdnf5.base.GoalProblem_NO_PROBLEM)
+        transaction_remove.run()
+        self.assertFalse(transaction_remove.get_transaction_problems())
