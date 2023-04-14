d97462773a03174fb2a457d7f8bd0da186f59948
Marek Blaha <mblaha@redhat.com>
2023-04-14 11:53:44 +0200

    dnf5: Add autoremove command

---
+++ dnf5/commands/autoremove/autoremove.cpp
@@ -0,0 +1,56 @@
+/*
+Copyright Contributors to the libdnf project.
+
+This file is part of libdnf: https://github.com/rpm-software-management/libdnf/
+
+Libdnf is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+Libdnf is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with libdnf.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#include "autoremove.hpp"
+
+#include <libdnf/rpm/package_query.hpp>
+
+namespace dnf5 {
+
+using namespace libdnf::cli;
+
+void AutoremoveCommand::set_parent_command() {
+    auto * arg_parser_parent_cmd = get_session().get_argument_parser().get_root_command();
+    auto * arg_parser_this_cmd = get_argument_parser_command();
+    arg_parser_parent_cmd->register_command(arg_parser_this_cmd);
+    arg_parser_parent_cmd->get_group("software_management_commands").register_argument(arg_parser_this_cmd);
+}
+
+void AutoremoveCommand::set_argument_parser() {
+    get_argument_parser_command()->set_description(
+        "Remove all unneeded packages originally installed as dependencies.");
+}
+
+void AutoremoveCommand::configure() {
+    auto & context = get_context();
+    context.set_load_system_repo(true);
+    context.set_load_available_repos(Context::LoadAvailableRepos::NONE);
+}
+
+void AutoremoveCommand::run() {
+    auto & ctx = get_context();
+    libdnf::rpm::PackageQuery unneeded(ctx.base);
+    unneeded.filter_unneeded();
+    auto goal = get_context().get_goal();
+    for (const auto & pkg : unneeded) {
+        goal->add_rpm_remove(pkg);
+    }
+}
+
+}  // namespace dnf5
---
+++ dnf5/commands/autoremove/autoremove.hpp
@@ -0,0 +1,43 @@
+/*
+Copyright Contributors to the libdnf project.
+
+This file is part of libdnf: https://github.com/rpm-software-management/libdnf/
+
+Libdnf is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+Libdnf is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with libdnf.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#ifndef DNF5_COMMANDS_AUTOREMOVE_AUTOREMOVE_HPP
+#define DNF5_COMMANDS_AUTOREMOVE_AUTOREMOVE_HPP
+
+#include <dnf5/context.hpp>
+#include <libdnf/conf/option_bool.hpp>
+
+#include <memory>
+#include <vector>
+
+namespace dnf5 {
+
+class AutoremoveCommand : public Command {
+public:
+    explicit AutoremoveCommand(Context & context) : Command(context, "autoremove") {}
+    void set_parent_command() override;
+    void set_argument_parser() override;
+    void configure() override;
+    void run() override;
+};
+
+}  // namespace dnf5
+
+
+#endif  // DNF5_COMMANDS_AUTOREMOVE_AUTOREMOVE_HPP
