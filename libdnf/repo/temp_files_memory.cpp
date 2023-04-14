e0895a57bff5ab9a48b6ab69aa2b317272d8f130
Jan Kolarik <jkolarik@redhat.com>
2023-04-14 06:37:37 +0000

    Implement memory file for storing paths of temporary files
    This storage is implemented in order to keep track of downloaded packages that should not be cached.

---
+++ libdnf/repo/temp_files_memory.cpp
@@ -0,0 +1,68 @@
+/*
+Copyright Contributors to the libdnf project.
+
+This file is part of libdnf: https://github.com/rpm-software-management/libdnf/
+
+Libdnf is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 2.1 of the License, or
+(at your option) any later version.
+
+Libdnf is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with libdnf.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#include "temp_files_memory.hpp"
+
+#include "utils/bgettext/bgettext-mark-domain.h"
+#include "utils/fs/file.hpp"
+
+#include "libdnf/common/exception.hpp"
+
+#include <toml.hpp>
+
+
+namespace libdnf::repo {
+
+TempFilesMemory::TempFilesMemory(const std::string & parent_dir)
+    : full_memory_path(std::filesystem::path(parent_dir) / MEMORY_FILENAME) {}
+
+TempFilesMemory::~TempFilesMemory() = default;
+
+std::vector<std::string> TempFilesMemory::get_files() const {
+    if (!std::filesystem::exists(full_memory_path)) {
+        return {};
+    }
+
+    try {
+        auto toml_data = toml::parse(full_memory_path);
+        return toml::get<std::vector<std::string>>(toml_data[FILE_PATHS_TOML_KEY]);
+    } catch (const toml::exception & e) {
+        throw libdnf::Error(
+            M_("An error occurred when parsing the temporary files memory file at '{}': {}"),
+            full_memory_path.string(),
+            std::string(e.what()));
+    }
+}
+
+void TempFilesMemory::add_files(const std::vector<std::string> & paths) {
+    auto files = get_files();
+    files.insert(files.end(), paths.begin(), paths.end());
+
+    // sort and deduplicate resulting vector
+    std::sort(files.begin(), files.end());
+    files.erase(std::unique(files.begin(), files.end()), files.end());
+
+    utils::fs::File(full_memory_path, "w").write(toml::format(toml::value({{FILE_PATHS_TOML_KEY, files}})));
+}
+
+void TempFilesMemory::clear() {
+    std::filesystem::remove(full_memory_path);
+}
+
+}  //namespace libdnf::repo
---
+++ libdnf/repo/temp_files_memory.hpp
@@ -0,0 +1,71 @@
+/*
+Copyright Contributors to the libdnf project.
+
+This file is part of libdnf: https://github.com/rpm-software-management/libdnf/
+
+Libdnf is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 2.1 of the License, or
+(at your option) any later version.
+
+Libdnf is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with libdnf.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#ifndef LIBDNF_REPO_TEMP_FILES_MEMORY_HPP
+#define LIBDNF_REPO_TEMP_FILES_MEMORY_HPP
+
+#include <filesystem>
+#include <vector>
+
+
+namespace libdnf::repo {
+
+/// @brief A class for storing paths of temporary files in a TOML file.
+/// It behaves in a stateless way, meaning no data are cached within the
+/// class.
+class TempFilesMemory {
+public:
+    /// @brief Filename in which the paths are stored.
+    static constexpr const char * MEMORY_FILENAME = "temporary_files.toml";
+
+    /// @brief TOML key used for the array of stored paths.
+    static constexpr const char * FILE_PATHS_TOML_KEY = "files";
+
+    /// @brief Create the object for managing temporary files' paths.
+    /// @param parent_dir Path to a directory where the memory file is or will be stored.
+    TempFilesMemory(const std::string & parent_dir);
+    ~TempFilesMemory();
+
+    /// @brief Retrieve stored paths of temporary files.
+    /// @return A vector of parsed paths of temporary files.
+    /// @exception libdnf::Error When an error occurs during parsing of the file with temporary files.
+    /// @exception std::filesystem::filesystem_error When an error occurs during accessing the memory file.
+    std::vector<std::string> get_files() const;
+
+    /// @brief Add a list of file paths into the memory file.
+    /// The memory file is created if it didn't exist before.
+    /// Existing paths are loaded first using the `get_files()` method.
+    /// The resulting list contains both existing and new paths and it's sorted and deduplicated before storing in the file.
+    /// @param paths A list of file paths to be added into the memory file.
+    /// @exception libdnf::Error When an error occurs during parsing of the file with temporary files.
+    /// @exception std::filesystem::filesystem_error When an error occurs during accessing or writing the memory file.
+    void add_files(const std::vector<std::string> & paths);
+
+    /// @brief Deletes the memory file.
+    /// @exception std::filesystem::filesystem_error When an error occurs during deleting the memory file.
+    void clear();
+
+private:
+    std::filesystem::path full_memory_path;
+};
+
+
+}  // namespace libdnf::repo
+
+#endif  // LIBDNF_REPO_TEMP_FILES_MEMORY_HPP
