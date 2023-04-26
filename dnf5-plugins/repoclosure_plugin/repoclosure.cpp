1bca2898af9b1d3b229209bbbb659ab98e243155
Marek Blaha <mblaha@redhat.com>
2023-04-26 14:06:24 +0200

    Implementation of repoclosure plugin

---
+++ dnf5-plugins/repoclosure_plugin/repoclosure.cpp
@@ -0,0 +1,216 @@
+/*
+Copyright Contributors to the libdnf project.
+
+This file is part of libdnf: https://github.com/rpm-software-management/libdnf/
+
+Libdnf is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+Libdnf is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with libdnf.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#include "repoclosure.hpp"
+
+#include "utils/bgettext/bgettext-mark-domain.h"
+
+#include "libdnf-cli/argument_parser.hpp"
+
+#include "libdnf/conf/const.hpp"
+#include "libdnf/rpm/package.hpp"
+#include "libdnf/rpm/package_query.hpp"
+#include "libdnf/utils/format.hpp"
+
+#include <iostream>
+
+
+namespace dnf5 {
+
+void RepoclosureCommand::set_parent_command() {
+    auto * arg_parser_parent_cmd = get_session().get_argument_parser().get_root_command();
+    auto * arg_parser_this_cmd = get_argument_parser_command();
+    arg_parser_parent_cmd->register_command(arg_parser_this_cmd);
+}
+
+void RepoclosureCommand::set_argument_parser() {
+    auto & ctx = get_context();
+    auto & parser = ctx.get_argument_parser();
+
+    auto & cmd = *get_argument_parser_command();
+    cmd.set_description(_("Print list of unresolved dependencies for repositories"));
+
+    auto specs =
+        parser.add_new_positional_arg("specs", libdnf::cli::ArgumentParser::PositionalArg::UNLIMITED, nullptr, nullptr);
+    specs->set_description("List of package specs to check closure for");
+    specs->set_parse_hook_func(
+        [this]([[maybe_unused]] libdnf::cli::ArgumentParser::PositionalArg * arg, int argc, const char * const argv[]) {
+            for (int i = 0; i < argc; ++i) {
+                pkg_specs.emplace_back(argv[i]);
+            }
+            return true;
+        });
+    cmd.register_positional_arg(specs);
+
+    auto * check_repos_arg = parser.add_new_named_arg("check");
+    check_repos_arg->set_long_name("check");
+    check_repos_arg->set_description("Specify repo ids to check");
+    check_repos_arg->set_has_value(true);
+    check_repos_arg->set_arg_value_help("<REPO ID>,...");
+    check_repos_arg->set_parse_hook_func([this](
+                                             [[maybe_unused]] libdnf::cli::ArgumentParser::NamedArg * arg,
+                                             [[maybe_unused]] const char * option,
+                                             const char * value) {
+        libdnf::OptionStringList list_value(value);
+        for (const auto & repoid : list_value.get_value()) {
+            check_repos.emplace_back(repoid);
+        }
+        return true;
+    });
+    cmd.register_named_arg(check_repos_arg);
+
+    auto * arches_arg = parser.add_new_named_arg("arch");
+    arches_arg->set_long_name("arch");
+    arches_arg->set_description("Only check packages of specified architectures.");
+    arches_arg->set_has_value(true);
+    arches_arg->set_arg_value_help("<ARCH>,...");
+    arches_arg->set_parse_hook_func([this](
+                                        [[maybe_unused]] libdnf::cli::ArgumentParser::NamedArg * arg,
+                                        [[maybe_unused]] const char * option,
+                                        const char * value) {
+        libdnf::OptionStringList list_value(value);
+        for (const auto & arch : list_value.get_value()) {
+            arches.emplace_back(arch);
+        }
+        return true;
+    });
+    cmd.register_named_arg(arches_arg);
+
+    newest = std::make_unique<libdnf::cli::session::BoolOption>(
+        *this, "newest", '\0', "Only consider the latest version of a package from each repo.", false);
+}
+
+void RepoclosureCommand::configure() {
+    auto & context = get_context();
+    context.set_load_system_repo(false);
+    // filelists needed because there are packages in repos with file requirements
+    context.base.get_config().get_optional_metadata_types_option().add_item(libdnf::METADATA_TYPE_FILELISTS);
+    context.set_load_available_repos(Context::LoadAvailableRepos::ENABLED);
+}
+
+void RepoclosureCommand::run() {
+    auto & ctx = get_context();
+
+    // available_query is the set of packages used to satisfy dependencies
+    // to_check_query is the set of packages we are checking the dependencies of
+    // in case that `--newest` was used, start with an empty query
+    bool newest_used = newest->get_value();
+    libdnf::rpm::PackageQuery available_query(ctx.base, libdnf::sack::ExcludeFlags::APPLY_EXCLUDES, newest_used);
+    libdnf::rpm::PackageQuery to_check_query(ctx.base, libdnf::sack::ExcludeFlags::APPLY_EXCLUDES, newest_used);
+    if (newest_used) {
+        libdnf::repo::RepoQuery repos(ctx.base);
+        repos.filter_enabled(true);
+        for (const auto & repo : repos) {
+            libdnf::rpm::PackageQuery repo_pkgs(ctx.base);
+            repo_pkgs.filter_repo_id({repo->get_id()});
+            repo_pkgs.filter_latest_evr();
+            available_query |= repo_pkgs;
+            to_check_query |= repo_pkgs;
+        }
+    }
+
+    if (!check_repos.empty()) {
+        to_check_query.filter_repo_id(check_repos);
+    }
+
+    if (!arches.empty()) {
+        to_check_query.filter_arch(arches);
+    }
+
+    if (ctx.base.get_config().get_best_option().get_value()) {
+        available_query.filter_latest_evr();
+    }
+
+    if (!pkg_specs.empty()) {
+        libdnf::rpm::PackageQuery to_check_pkgs(ctx.base, libdnf::sack::ExcludeFlags::APPLY_EXCLUDES, true);
+        libdnf::ResolveSpecSettings settings{.with_nevra = true, .with_provides = false, .with_filenames = false};
+        bool specs_resolved = true;
+        for (const auto & spec : pkg_specs) {
+            libdnf::rpm::PackageQuery package_query(to_check_query);
+            auto nevra_pair = package_query.resolve_pkg_spec(spec, settings, true);
+            if (!nevra_pair.first) {
+                specs_resolved = false;
+                std::cerr << libdnf::utils::sformat(_("No match for argument \"{}\"."), spec) << std::endl;
+                continue;
+            }
+            to_check_pkgs |= package_query;
+        }
+        if (!specs_resolved) {
+            throw libdnf::cli::CommandExitError(1, M_("Failed to resolve package specifications."));
+        }
+        to_check_query = to_check_pkgs;
+    }
+
+
+    // cache query results: reldepid -> whether the reldep is satisfied
+    std::unordered_map<int, bool> resolved;
+    std::vector<std::pair<libdnf::rpm::Package, std::vector<std::string>>> unresolved_packages;
+    for (const auto & pkg : to_check_query) {
+        std::vector<std::string> unsatisfied;
+        for (const auto & reldep : pkg.get_requires()) {
+            int reldep_id = reldep.get_id().id;
+            auto resolved_it = resolved.find(reldep_id);
+            bool satisfied;
+            if (resolved_it == resolved.end()) {
+                libdnf::rpm::PackageQuery reldep_q(available_query);
+                reldep_q.filter_provides(reldep);
+                satisfied = !reldep_q.empty();
+                resolved.emplace(reldep_id, satisfied);
+            } else {
+                satisfied = resolved_it->second;
+            }
+            if (!satisfied) {
+                unsatisfied.emplace_back(reldep.to_string());
+            }
+        }
+        if (!unsatisfied.empty()) {
+            std::sort(unsatisfied.begin(), unsatisfied.end());
+            unresolved_packages.emplace_back(pkg, std::move(unsatisfied));
+        }
+    }
+
+    if (!unresolved_packages.empty()) {
+        // sort unresolved packages
+        std::sort(
+            unresolved_packages.begin(),
+            unresolved_packages.end(),
+            [](const std::pair<libdnf::rpm::Package, std::vector<std::string>> & l,
+               const std::pair<libdnf::rpm::Package, std::vector<std::string>> & r) {
+                return libdnf::rpm::cmp_nevra<libdnf::rpm::Package>(l.first, r.first);
+            });
+        int unresolved_deps_count = 0;
+
+        for (auto const & [pkg, unsatisfied] : unresolved_packages) {
+            std::cout << "package: " << pkg.get_nevra() << " from " << pkg.get_repo_id() << std::endl;
+            std::cout << "  unresolved deps (" << unsatisfied.size() << "):" << std::endl;
+            for (const auto & dep : unsatisfied) {
+                unresolved_deps_count += 1;
+                std::cout << "    " << dep << std::endl;
+            }
+        }
+
+        throw libdnf::cli::CommandExitError(
+            1,
+            M_("Error: Repoclosure ended with unresolved dependencies ({}) across {} packages."),
+            unresolved_deps_count,
+            unresolved_packages.size());
+    }
+}
+
+}  // namespace dnf5
---
+++ dnf5-plugins/repoclosure_plugin/repoclosure.hpp
@@ -0,0 +1,55 @@
+/*
+Copyright Contributors to the libdnf project.
+
+This file is part of libdnf: https://github.com/rpm-software-management/libdnf/
+
+Libdnf is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+Libdnf is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with libdnf.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+
+#ifndef DNF5_PLUGINS_REPOCLOSURE_PLUGIN_REPOCLOSURE_HPP
+#define DNF5_PLUGINS_REPOCLOSURE_PLUGIN_REPOCLOSURE_HPP
+
+
+#include <dnf5/context.hpp>
+#include <dnf5/shared_options.hpp>
+#include <libdnf-cli/session.hpp>
+#include <libdnf/conf/option_string_list.hpp>
+
+#include <vector>
+
+
+namespace dnf5 {
+
+
+class RepoclosureCommand : public Command {
+public:
+    explicit RepoclosureCommand(Context & context) : Command(context, "repoclosure") {}
+    void set_parent_command() override;
+    void set_argument_parser() override;
+    void configure() override;
+    void run() override;
+
+private:
+    std::vector<std::string> pkg_specs{};
+    std::vector<std::string> check_repos{};
+    std::vector<std::string> arches{};
+    std::unique_ptr<libdnf::cli::session::BoolOption> newest{nullptr};
+};
+
+
+}  // namespace dnf5
+
+
+#endif  // DNF5_PLUGINS_REPOCLOSURE_PLUGIN_REPOCLOSURE_HPP
