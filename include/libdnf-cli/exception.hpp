a60f56e51357e0833d181b896876e295359b5fee
Jan Kolarik <jkolarik@redhat.com>
2023-06-01 11:33:30 +0000

    exception: Modify silent command error to take no message
    As the `SilentCommandExitError` is supposed to be used when no additional message to be displayed for the user, change the constructor to not accept any.

--- include/libdnf-cli/exception.hpp
+++ include/libdnf-cli/exception.hpp
@@ -88,19 +88,13 @@ private:
     int exit_code;
 };
 
-/// Exception used when non-standard exit code should be returned
+/// Exception used when non-standard exit code should be returned without displaying any message to the user
 class SilentCommandExitError : public Error {
 public:
-    /// Constructs the error with given exit code and with custom formatted error
-    /// message.
+    /// Constructs the error with given exit code.
     ///
     /// @param exit_code The exit code
-    /// @param format The format string for the message
-    /// @param args The format arguments
-    template <typename... Ss>
-    explicit SilentCommandExitError(int exit_code, BgettextMessage format, Ss &&... args)
-        : Error(format, std::forward<Ss>(args)...),
-          exit_code(exit_code) {}
+    explicit SilentCommandExitError(int exit_code);
 
     const char * get_name() const noexcept override { return "SilentCommandExitError"; }
 
