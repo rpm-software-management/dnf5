c6e337a77efb8a2f0fa89790cc8e0b64506fc4ae
Charlie Sale <softwaresale01@gmail.com>
2023-09-28 23:05:31 -0400

    Formatting fixes

--- libdnf5/plugin/plugins.hpp
+++ libdnf5/plugin/plugins.hpp
@@ -25,10 +25,10 @@ along with libdnf.  If not, see <https://www.gnu.org/licenses/>.
 #include "libdnf5/plugin/iplugin.hpp"
 
 #include <memory>
-#include <string>
-#include <vector>
-#include <utility>
 #include <optional>
+#include <string>
+#include <utility>
+#include <vector>
 
 
 namespace libdnf5::plugin {
@@ -72,10 +72,10 @@ protected:
 };
 
 enum class PluginEnabled {
-    NO, // = 0
-    YES, // = 1
-    HOST_ONLY, // = 2
-    INSTALLROOT_ONLY // = 3
+    NO,               // = 0
+    YES,              // = 1
+    HOST_ONLY,        // = 2
+    INSTALLROOT_ONLY  // = 3
 };
 
 /**
@@ -90,7 +90,7 @@ public:
      * @param installroot The install root. May be empty
      * @return True if the plugin status corresponds to enabled, false otherwise
      */
-    static bool plugin_status_is_enabled(PluginEnabled status, const std::string &installroot) const noexcept;
+    static bool plugin_status_is_enabled(PluginEnabled status, const std::string & installroot) const noexcept;
 
     EnabledPlugins(std::initializer_list<std::pair<std::string, PluginEnabled>> elements);
     EnabledPlugins(std::initializer_list<std::pair<std::string, bool>> elements);
@@ -106,14 +106,14 @@ public:
      * @param plugin_pattern The pattern of plugins to match
      * @param status Enablement status
      */
-    void push_back(std::string &&plugin_pattern, PluginEnabled status) noexcept;
+    void push_back(std::string && plugin_pattern, PluginEnabled status) noexcept;
 
     /**
      * Adds a new enable/disable request.
      * @param plugin_pattern Plugin pattern to match
      * @param enabled True if enabled, false if disabled.
      */
-    void push_back(std::string &&plugin_pattern, bool enabled) noexcept;
+    void push_back(std::string && plugin_pattern, bool enabled) noexcept;
 
     /**
      * Determines if a plugin specified here is enabled or not. If the plugin is specified here, then its enablement
@@ -122,14 +122,15 @@ public:
      * @param install_root The install root of the system to optionally check against
      * @return True if enabled and defined, false if disabled and defined, empty if not defined
      */
-    std::optional<bool> plugin_enabled(const std::string &plugin_name, const std::string &install_root = "") const noexcept;
+    std::optional<bool> plugin_enabled(
+        const std::string & plugin_name, const std::string & install_root = "") const noexcept;
 
     /**
      * Gets the enablement status of the given plugin name, or empty if it isn't defined here
      * @param plugin_name The name of the plugin to check. Globbing is enabled.
      * @return Status if plugin is defined here, empty otherwise
      */
-    std::optional<PluginEnabled> plugin_enabled_status(const std::string &plugin_name) const noexcept;
+    std::optional<PluginEnabled> plugin_enabled_status(const std::string & plugin_name) const noexcept;
 
 private:
     std::vector<std::pair<std::string, PluginEnabled>> enabled_plugins;
@@ -145,10 +146,10 @@ public:
     void register_plugin(std::unique_ptr<Plugin> && plugin);
 
     /// Loads the plugin from the library defined by the configuration file config_file_path.
-    void load_plugin(const std::string & config_file_path, const EnabledPlugins &plugin_enablement);
+    void load_plugin(const std::string & config_file_path, const EnabledPlugins & plugin_enablement);
 
     /// Loads plugins defined by configuration files in the directory.
-    void load_plugins(const std::string & config_dir_path, const EnabledPlugins &plugin_enablement);
+    void load_plugins(const std::string & config_dir_path, const EnabledPlugins & plugin_enablement);
 
     /// Returns the number of registered plugins.
     size_t count() const noexcept;
