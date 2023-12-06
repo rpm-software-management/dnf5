229fc78d0c7f9f6ee1b763f5ecaf0a38427c06c3
Aleš Matěj <amatej@redhat.com>
2023-12-06 09:41:09 +0100

    Separate repos_from_paths id and path vars substitutions
    When later setting repos_from_path as enabled we want to use substituted ids.
    This fixes a bug when --repofrompath with variable is diabled by --disablerepo=*
    
    Eg:
    `dnf5 repo list --repofrompath=NEW_REPO,THE_PATH --disablerepo=*`
    -> NEW_REPO is enabled
    
    but:
    `dnf5 repo list --releasever=RLS --repofrompath=NEW_REPO_\${{releasever}},THE_PATH --disablerepo=* `
    -> wrongly results in NEW_REPO_RLS being disabled

--- libdnf5/repo/repo_sack.cpp
+++ libdnf5/repo/repo_sack.cpp
@@ -601,13 +601,11 @@ void RepoSack::create_repos_from_reposdir() {
 
 void RepoSack::create_repos_from_paths(
     const std::vector<std::pair<std::string, std::string>> & repos_paths, libdnf5::Option::Priority priority) {
-    auto vars = base->get_vars();
     for (const auto & [id, path] : repos_paths) {
-        auto repo_id = vars->substitute(id);
-        auto new_repo = create_repo(repo_id);
+        auto new_repo = create_repo(id);
         auto & new_repo_config = new_repo->get_config();
-        new_repo_config.get_name_option().set(priority, repo_id);
-        new_repo_config.get_baseurl_option().set(priority, {vars->substitute(path)});
+        new_repo_config.get_name_option().set(priority, id);
+        new_repo_config.get_baseurl_option().set(priority, path);
     }
 }
 
