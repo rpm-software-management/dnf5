b87578ed385fc64d83948a04d3cc00df08bb91e8
Jaroslav Mracek <jmracek@redhat.com>
2023-12-15 15:03:58 +0100

    Add a method accepting std::string for filter_requires()
    This is required for Python binding, because when a string argument is
    provided, the binding converts it to a vector of characters. The change
    might be problematic because filter_requires({"<something>"}) and
    filter_requires({"<something>", "<something>"}) stop to work.

--- include/libdnf5/rpm/package_query.hpp
+++ include/libdnf5/rpm/package_query.hpp
@@ -441,12 +441,23 @@ public:
 
     /// Filter packages by their `requires`.
     ///
+    /// @param pattern          A string the filter is matched against.
+    /// @param cmp_type         A comparison (match) operator, defaults to `QueryCmp::EQ`.
+    ///                         Supported values: `EQ`, `NEQ`, `GLOB`, `NOT_GLOB`.
+    /// @since 5.2
+    //
+    // @replaces libdnf/sack/query.hpp:method:addFilter(int keyname, int cmp_type, const char *match) - cmp_type = HY_PKG_REQUIRES
+    void filter_requires(const std::string & pattern, libdnf5::sack::QueryCmp cmp_type = libdnf5::sack::QueryCmp::EQ) {
+        filter_requires(std::vector<std::string>{pattern}, cmp_type);
+    };
+
+    /// Filter packages by their `requires`.
+    ///
     /// @param patterns         A vector of strings the filter is matched against.
     /// @param cmp_type         A comparison (match) operator, defaults to `QueryCmp::EQ`.
     ///                         Supported values: `EQ`, `NEQ`, `GLOB`, `NOT_GLOB`.
     /// @since 5.0
     //
-    // @replaces libdnf/sack/query.hpp:method:addFilter(int keyname, int cmp_type, const char *match) - cmp_type = HY_PKG_REQUIRES
     // @replaces libdnf/sack/query.hpp:method:addFilter(int keyname, int cmp_type, const char **matches) - cmp_type = HY_PKG_REQUIRES
     void filter_requires(
         const std::vector<std::string> & patterns, libdnf5::sack::QueryCmp cmp_type = libdnf5::sack::QueryCmp::EQ);
