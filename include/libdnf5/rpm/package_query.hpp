263c8244823a8070811121b3daf70f5ad0a45640
Jaroslav Mracek <jmracek@redhat.com>
2023-12-15 15:08:12 +0100

    Add a method accepting std::string for filter_conflicts()
    This is required for Python binding, because when a string argument is
    provided, the binding converts it to a vector of characters. The change
    might be problematic because filter_conflicts({"<something>"}) and
    filter_conflicts({"<something>", "<something>"}) stop to work.

--- include/libdnf5/rpm/package_query.hpp
+++ include/libdnf5/rpm/package_query.hpp
@@ -487,12 +487,23 @@ public:
 
     /// Filter packages by their `conflicts`.
     ///
+    /// @param pattern          A string the filter is matched against.
+    /// @param cmp_type         A comparison (match) operator, defaults to `QueryCmp::EQ`.
+    ///                         Supported values: `EQ`, `NEQ`, `GLOB`, `NOT_GLOB`.
+    /// @since 5.2
+    //
+    // @replaces libdnf/sack/query.hpp:method:addFilter(int keyname, int cmp_type, const char *match) - cmp_type = HY_PKG_CONFLICTS
+    void filter_conflicts(const std::string & pattern, libdnf5::sack::QueryCmp cmp_type = libdnf5::sack::QueryCmp::EQ) {
+        filter_conflicts(std::vector<std::string>{pattern}, cmp_type);
+    };
+
+    /// Filter packages by their `conflicts`.
+    ///
     /// @param patterns         A vector of strings the filter is matched against.
     /// @param cmp_type         A comparison (match) operator, defaults to `QueryCmp::EQ`.
     ///                         Supported values: `EQ`, `NEQ`, `GLOB`, `NOT_GLOB`.
     /// @since 5.0
     //
-    // @replaces libdnf/sack/query.hpp:method:addFilter(int keyname, int cmp_type, const char *match) - cmp_type = HY_PKG_CONFLICTS
     // @replaces libdnf/sack/query.hpp:method:addFilter(int keyname, int cmp_type, const char **matches) - cmp_type = HY_PKG_CONFLICTS
     void filter_conflicts(
         const std::vector<std::string> & patterns, libdnf5::sack::QueryCmp cmp_type = libdnf5::sack::QueryCmp::EQ);
