4859807b738c5630a045566e359999ec8c1a79f7
Aleš Matěj <amatej@redhat.com>
2024-01-10 13:33:40 +0100

    Add p_impl to libdnf5::GoalJobSettings and add getters and setters

--- dnf5daemon-server/services/rpm/rpm.cpp
+++ dnf5daemon-server/services/rpm/rpm.cpp
@@ -311,7 +311,7 @@ sdbus::MethodReply Rpm::downgrade(sdbus::MethodCall & call) {
     // fill the goal
     auto & goal = session.get_goal();
     libdnf5::GoalJobSettings setting;
-    setting.to_repo_ids = repo_ids;
+    setting.set_to_repo_ids(repo_ids);
     for (const auto & spec : specs) {
         goal.add_downgrade(spec, setting);
     }
@@ -347,9 +347,9 @@ sdbus::MethodReply Rpm::install(sdbus::MethodCall & call) {
     // fill the goal
     auto & goal = session.get_goal();
     libdnf5::GoalJobSettings setting;
-    setting.skip_broken = skip_broken;
-    setting.skip_unavailable = skip_unavailable;
-    setting.to_repo_ids = repo_ids;
+    setting.set_skip_broken(skip_broken);
+    setting.set_skip_unavailable(skip_unavailable);
+    setting.set_to_repo_ids(repo_ids);
 
     for (const auto & spec : specs) {
         goal.add_install(spec, setting);
@@ -371,7 +371,7 @@ sdbus::MethodReply Rpm::upgrade(sdbus::MethodCall & call) {
     // fill the goal
     auto & goal = session.get_goal();
     libdnf5::GoalJobSettings setting;
-    setting.to_repo_ids = repo_ids;
+    setting.set_to_repo_ids(repo_ids);
     if (specs.empty()) {
         goal.add_rpm_upgrade(setting);
     } else {
@@ -396,7 +396,7 @@ sdbus::MethodReply Rpm::reinstall(sdbus::MethodCall & call) {
     // fill the goal
     auto & goal = session.get_goal();
     libdnf5::GoalJobSettings setting;
-    setting.to_repo_ids = repo_ids;
+    setting.set_to_repo_ids(repo_ids);
     for (const auto & spec : specs) {
         goal.add_reinstall(spec, setting);
     }
