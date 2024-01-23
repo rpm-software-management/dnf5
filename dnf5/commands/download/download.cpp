00aa861b9efaa42c549f059edf730ce48f07e089
Derick Diaz <derickdiaz123@outlook.com>
2024-01-22 21:09:57 -0600

    Converted to string

--- dnf5/commands/download/download.cpp
+++ dnf5/commands/download/download.cpp
@@ -97,7 +97,8 @@ void DownloadCommand::set_argument_parser() {
         [&ctx, this](
             [[maybe_unused]] ArgumentParser::NamedArg * arg, [[maybe_unused]] const char * option, const char * value) {
             if (urlprotocol_valid_options.find(value) == urlprotocol_valid_options.end()) {
-                throw libdnf5::cli::ArgumentParserInvalidValueError(M_("Invalid urlprotocol option: {}"), value);
+                throw libdnf5::cli::ArgumentParserInvalidValueError(
+                    M_("Invalid urlprotocol option: {}"), std::string(value));
             }
             urlprotocol_option.emplace(value);
         });
