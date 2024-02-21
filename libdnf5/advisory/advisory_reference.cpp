961b7f12b71da9b5b82afeb9d8111dd676295f52
Aleš Matěj <amatej@redhat.com>
2024-02-21 12:38:53 +0100

    Add pImpl to `libdnf5::advisory::AdvisoryReference`

--- libdnf5/advisory/advisory_reference.cpp
+++ libdnf5/advisory/advisory_reference.cpp
@@ -27,25 +27,65 @@ along with libdnf.  If not, see <https://www.gnu.org/licenses/>.
 
 namespace libdnf5::advisory {
 
+class AdvisoryReference::Impl {
+public:
+    explicit Impl(const libdnf5::BaseWeakPtr & base, AdvisoryId advisory, int index)
+        : base(base),
+          advisory(advisory),
+          index(index) {}
+
+private:
+    friend AdvisoryReference;
+
+    BaseWeakPtr base;
+
+    AdvisoryId advisory;
+
+    /// We cannot store IDs of reference data (id, type, title, url) because they
+    /// don't have ids set in libsolv (they are only strings), therefore we store
+    /// index of the reference.
+    int index;
+};
+
 AdvisoryReference::AdvisoryReference(const libdnf5::BaseWeakPtr & base, AdvisoryId advisory, int index)
-    : base(base),
-      advisory(advisory),
-      index(index) {}
+    : p_impl(std::make_unique<Impl>(base, advisory, index)) {}
+
+AdvisoryReference::~AdvisoryReference() = default;
+
+AdvisoryReference::AdvisoryReference(const AdvisoryReference & src) : p_impl(new Impl(*src.p_impl)) {}
+AdvisoryReference & AdvisoryReference::operator=(const AdvisoryReference & src) {
+    if (this != &src) {
+        if (p_impl) {
+            *p_impl = *src.p_impl;
+        } else {
+            p_impl = std::make_unique<Impl>(*src.p_impl);
+        }
+    }
+
+    return *this;
+}
+
+AdvisoryReference::AdvisoryReference(AdvisoryReference && src) noexcept = default;
+AdvisoryReference & AdvisoryReference::operator=(AdvisoryReference && src) noexcept = default;
 
 std::string AdvisoryReference::get_id() const {
-    return std::string(get_rpm_pool(base).get_str_from_pool(UPDATE_REFERENCE_ID, advisory.id, index));
+    return std::string(
+        get_rpm_pool(p_impl->base).get_str_from_pool(UPDATE_REFERENCE_ID, p_impl->advisory.id, p_impl->index));
 }
 std::string AdvisoryReference::get_type() const {
-    return std::string(get_rpm_pool(base).get_str_from_pool(UPDATE_REFERENCE_TYPE, advisory.id, index));
+    return std::string(
+        get_rpm_pool(p_impl->base).get_str_from_pool(UPDATE_REFERENCE_TYPE, p_impl->advisory.id, p_impl->index));
 }
 const char * AdvisoryReference::get_type_cstring() const {
-    return get_rpm_pool(base).get_str_from_pool(UPDATE_REFERENCE_TYPE, advisory.id, index);
+    return get_rpm_pool(p_impl->base).get_str_from_pool(UPDATE_REFERENCE_TYPE, p_impl->advisory.id, p_impl->index);
 }
 std::string AdvisoryReference::get_title() const {
-    return std::string(get_rpm_pool(base).get_str_from_pool(UPDATE_REFERENCE_TITLE, advisory.id, index));
+    return std::string(
+        get_rpm_pool(p_impl->base).get_str_from_pool(UPDATE_REFERENCE_TITLE, p_impl->advisory.id, p_impl->index));
 }
 std::string AdvisoryReference::get_url() const {
-    return std::string(get_rpm_pool(base).get_str_from_pool(UPDATE_REFERENCE_HREF, advisory.id, index));
+    return std::string(
+        get_rpm_pool(p_impl->base).get_str_from_pool(UPDATE_REFERENCE_HREF, p_impl->advisory.id, p_impl->index));
 }
 
 }  // namespace libdnf5::advisory
