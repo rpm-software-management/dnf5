c8295a643c59c44aeba66ebd2907d39b9cdac80b
Aleš Matěj <amatej@redhat.com>
2024-02-21 12:30:40 +0100

    Add pImpl to `libdnf5::advisory::AdvisoryCollection`

--- libdnf5/advisory/advisory_collection.cpp
+++ libdnf5/advisory/advisory_collection.cpp
@@ -26,10 +26,50 @@ along with libdnf.  If not, see <https://www.gnu.org/licenses/>.
 
 namespace libdnf5::advisory {
 
+class AdvisoryCollection::Impl {
+public:
+    explicit Impl(const libdnf5::BaseWeakPtr & base, AdvisoryId advisory, int index)
+        : advisory(advisory),
+          index(index),
+          base(base) {}
+
+    /// Get all AdvisoryModules stored in this AdvisoryCollection
+    /// @param output           std::vector of AdvisorModules used as output.
+    ///                         This is much faster than returning new std::vector and later joining
+    ///                         them when collecting AdvisoryModules from multiple collections.
+    void get_modules(std::vector<AdvisoryModule> & output);
+
+private:
+    friend AdvisoryCollection;
+
+    AdvisoryId advisory;
+
+    /// AdvisoryCollections don't have their own Id, therefore store it's index in its Advisory (just like AdvisoryReference)
+    int index;
+
+    BaseWeakPtr base;
+};
+
 AdvisoryCollection::AdvisoryCollection(const libdnf5::BaseWeakPtr & base, AdvisoryId advisory, int index)
-    : base(base),
-      advisory(advisory),
-      index(index) {}
+    : p_impl(std::make_unique<Impl>(base, advisory, index)) {}
+
+AdvisoryCollection::~AdvisoryCollection() = default;
+
+AdvisoryCollection::AdvisoryCollection(const AdvisoryCollection & src) : p_impl(new Impl(*src.p_impl)) {}
+AdvisoryCollection & AdvisoryCollection::operator=(const AdvisoryCollection & src) {
+    if (this != &src) {
+        if (p_impl) {
+            *p_impl = *src.p_impl;
+        } else {
+            p_impl = std::make_unique<Impl>(*src.p_impl);
+        }
+    }
+
+    return *this;
+}
+
+AdvisoryCollection::AdvisoryCollection(AdvisoryCollection && src) noexcept = default;
+AdvisoryCollection & AdvisoryCollection::operator=(AdvisoryCollection && src) noexcept = default;
 
 bool AdvisoryCollection::is_applicable() const {
     //TODO(amatej): check if collection is applicable
@@ -44,20 +84,20 @@ std::vector<AdvisoryPackage> AdvisoryCollection::get_packages() {
 
 std::vector<AdvisoryModule> AdvisoryCollection::get_modules() {
     std::vector<AdvisoryModule> output;
-    get_modules(output);
+    p_impl->get_modules(output);
     return output;
 }
 
 void AdvisoryCollection::get_packages(std::vector<AdvisoryPackage> & output, bool with_filemanes) {
     Dataiterator di;
     const char * filename = nullptr;
-    auto & pool = get_rpm_pool(base);
+    auto & pool = get_rpm_pool(p_impl->base);
     int count = 0;
 
-    dataiterator_init(&di, *pool, 0, advisory.id, UPDATE_COLLECTIONLIST, 0, 0);
+    dataiterator_init(&di, *pool, 0, p_impl->advisory.id, UPDATE_COLLECTIONLIST, 0, 0);
     while (dataiterator_step(&di)) {
         dataiterator_setpos(&di);
-        if (count == index) {
+        if (count == p_impl->index) {
             Dataiterator di_inner;
             dataiterator_init(&di_inner, *pool, 0, SOLVID_POS, UPDATE_COLLECTION, 0, 0);
             while (dataiterator_step(&di_inner)) {
@@ -72,9 +112,9 @@ void AdvisoryCollection::get_packages(std::vector<AdvisoryPackage> & output, boo
                     filename = pool.lookup_str(SOLVID_POS, UPDATE_COLLECTION_FILENAME);
                 }
                 output.emplace_back(AdvisoryPackage(new AdvisoryPackage::Impl(
-                    base,
-                    advisory,
-                    index,
+                    p_impl->base,
+                    p_impl->advisory,
+                    p_impl->index,
                     name,
                     evr,
                     arch,
@@ -91,7 +131,7 @@ void AdvisoryCollection::get_packages(std::vector<AdvisoryPackage> & output, boo
     dataiterator_free(&di);
 }
 
-void AdvisoryCollection::get_modules(std::vector<AdvisoryModule> & output) {
+void AdvisoryCollection::Impl::get_modules(std::vector<AdvisoryModule> & output) {
     Dataiterator di;
     auto & pool = get_rpm_pool(base);
     int count = 0;
@@ -121,11 +161,11 @@ void AdvisoryCollection::get_modules(std::vector<AdvisoryModule> & output) {
 }
 
 AdvisoryId AdvisoryCollection::get_advisory_id() const {
-    return advisory;
+    return p_impl->advisory;
 }
 
 Advisory AdvisoryCollection::get_advisory() const {
-    return Advisory(base, advisory);
+    return Advisory(p_impl->base, p_impl->advisory);
 }
 
 }  // namespace libdnf5::advisory
