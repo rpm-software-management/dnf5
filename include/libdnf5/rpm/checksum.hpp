982a359c1046d6f64ebe7b7a807f787546dc597b
Aleš Matěj <amatej@redhat.com>
2024-02-26 11:56:56 +0100

    Add pImpl to `libdnf5::rpm::Checksum`

--- include/libdnf5/rpm/checksum.hpp
+++ include/libdnf5/rpm/checksum.hpp
@@ -20,6 +20,9 @@ along with libdnf.  If not, see <https://www.gnu.org/licenses/>.
 #ifndef LIBDNF5_RPM_CHECKSUM_HPP
 #define LIBDNF5_RPM_CHECKSUM_HPP
 
+#include "libdnf5/common/impl_ptr.hpp"
+
+#include <memory>
 #include <string>
 
 
@@ -28,12 +31,19 @@ namespace libdnf5::rpm {
 /// Class contains checksum and checksum type
 class Checksum {
 public:
+    ~Checksum();
+    Checksum(const Checksum & src);
+    Checksum & operator=(const Checksum & src);
+
+    Checksum(Checksum && src) noexcept;
+    Checksum & operator=(Checksum && src) noexcept;
+
     enum class Type { UNKNOWN, MD5, SHA1, SHA224, SHA256, SHA384, SHA512 };
     Type get_type() const noexcept;
     std::string get_type_str() const;
 
     /// Return checksum in hex format
-    const std::string & get_checksum() const noexcept { return checksum; };
+    const std::string & get_checksum() const noexcept;
 
 
 private:
@@ -42,17 +52,10 @@ private:
     /// Require checksum in hex and libsolv checksum type
     Checksum(const char * checksum, int libsolv_type);
 
-    std::string checksum;
-    int libsolv_type;
+    class Impl;
+    ImplPtr<Impl> p_impl;
 };
 
-
-/// Constructor requires checksum in hex and libsolv checksum type
-inline Checksum::Checksum(const char * checksum, int libsolv_type) : libsolv_type(libsolv_type) {
-    if (checksum != nullptr) {
-        this->checksum = checksum;
-    }
-}
 
 }  // namespace libdnf5::rpm
 
