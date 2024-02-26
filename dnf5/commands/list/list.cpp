c98243d97af8b4ea8b8bcbf47dee5410fdc1041f
Jaroslav Mracek <jmracek@redhat.com>
2024-02-26 10:45:53 +0100

    Make info and list commane case insesitive
    It unifies behavior with repoquery command and DNF4
    
    Closes: https://github.com/rpm-software-management/dnf5/issues/1259

--- dnf5/commands/list/list.cpp
+++ dnf5/commands/list/list.cpp
@@ -152,7 +152,11 @@ void ListCommand::run() {
     if (!pkg_specs.empty()) {
         base_query = libdnf5::rpm::PackageQuery(ctx.base, libdnf5::sack::ExcludeFlags::APPLY_EXCLUDES, true);
         libdnf5::ResolveSpecSettings settings{
-            .with_nevra = true, .with_provides = false, .with_filenames = false, .with_binaries = false};
+            .ignore_case = true,
+            .with_nevra = true,
+            .with_provides = false,
+            .with_filenames = false,
+            .with_binaries = false};
         for (const auto & spec : pkg_specs) {
             libdnf5::rpm::PackageQuery pkg_query(full_package_query);
             pkg_query.resolve_pkg_spec(spec, settings, true);
