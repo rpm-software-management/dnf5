d2a2abf80ef5ef99da0e8a863cec330caeca82bf
Pavla Kratochvilova <pkratoch@redhat.com>
2024-02-27 13:46:01 +0100

    modules: Report module solver problems

--- include/libdnf5/module/module_sack.hpp
+++ include/libdnf5/module/module_sack.hpp
@@ -64,22 +64,6 @@ enum class ModuleStatus { AVAILABLE, ENABLED, DISABLED };
 /// Container with data and methods related to modules
 class ModuleSack {
 public:
-    enum class ModuleErrorType {
-        NO_ERROR = 0,
-        INFO,
-        /// Error in module defaults detected during resolvement of module dependencies
-        ERROR_IN_DEFAULTS,
-        /// Error detected during resolvement of module dependencies
-        ERROR,
-        /// Error detected during resolvement of module dependencies - Unexpected error!!!
-        CANNOT_RESOLVE_MODULES,
-        CANNOT_RESOLVE_MODULE_SPEC,
-        CANNOT_ENABLE_MULTIPLE_STREAMS,
-        CANNOT_MODIFY_MULTIPLE_TIMES_MODULE_STATUS,
-        /// Problem with latest modules during resolvement of module dependencies
-        ERROR_IN_LATEST
-    };
-
     ~ModuleSack();
 
     ModuleSackWeakPtr get_weak_ptr();
@@ -102,9 +86,9 @@ public:
     /// Resolve which module items are active. This means requesting all enabled streams or default streams (default
     /// streams only from not-enabled non-disabled modules), while excluding all disabled module streams.
     ///
-    /// @return A pair of problems in resolving to report and ModuleErrorType.
+    /// @return A pair of problems in resolving to report and GoalProblem.
     /// @since 5.0
-    std::pair<base::SolverProblems, ModuleErrorType> resolve_active_module_items();
+    std::pair<base::SolverProblems, GoalProblem> resolve_active_module_items();
 
 private:
     friend class libdnf5::Base;
