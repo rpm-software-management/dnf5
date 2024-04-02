6e5f9a2f7af7b9d8d4898204a56dcdb39761aebf
Aleš Matěj <amatej@redhat.com>
2024-04-02 10:00:16 +0200

    Add pImpl to `libdnf5::OptionNumber`

--- libdnf5/conf/option_number.cpp
+++ libdnf5/conf/option_number.cpp
@@ -27,6 +27,28 @@ along with libdnf.  If not, see <https://www.gnu.org/licenses/>.
 namespace libdnf5 {
 
 template <typename T>
+class OptionNumber<T>::Impl {
+public:
+    Impl(T default_value, T min, T max) : default_value(default_value), min(min), max(max), value(default_value) {}
+
+    Impl(T default_value, T min, T max, FromStringFunc && from_string_func)
+        : from_string_user(std::move(from_string_func)),
+          default_value(default_value),
+          min(min),
+          max(max),
+          value(default_value) {}
+
+private:
+    friend OptionNumber;
+
+    FromStringFunc from_string_user;
+    ValueType default_value;
+    ValueType min;
+    ValueType max;
+    ValueType value;
+};
+
+template <typename T>
 bool from_string(T & out, const std::string & in, std::ios_base & (*manipulator)(std::ios_base &)) {
     std::istringstream iss(in);
     return !(iss >> manipulator >> out).fail();
@@ -35,10 +57,7 @@ bool from_string(T & out, const std::string & in, std::ios_base & (*manipulator)
 template <typename T>
 OptionNumber<T>::OptionNumber(T default_value, T min, T max)
     : Option(Priority::DEFAULT),
-      default_value(default_value),
-      min(min),
-      max(max),
-      value(default_value) {
+      p_impl(new Impl(default_value, min, max)) {
     test(default_value);
 }
 
@@ -53,12 +72,8 @@ OptionNumber<T>::OptionNumber(T default_value)
 template <typename T>
 OptionNumber<T>::OptionNumber(T default_value, T min, T max, FromStringFunc && from_string_func)
     : Option(Priority::DEFAULT),
-      from_string_user(std::move(from_string_func)),
-      default_value(default_value),
-      min(min),
-      max(max),
-      value(default_value) {
-    test(default_value);
+      p_impl(new Impl(default_value, min, max, std::move(from_string_func))) {
+    test(p_impl->default_value);
 }
 
 template <typename T>
@@ -71,16 +86,24 @@ OptionNumber<T>::OptionNumber(T default_value, FromStringFunc && from_string_fun
           default_value, std::numeric_limits<T>::min(), std::numeric_limits<T>::max(), std::move(from_string_func)) {}
 
 template <typename T>
+OptionNumber<T>::~OptionNumber() = default;
+
+template <typename T>
+OptionNumber<T>::OptionNumber(const OptionNumber & src) : Option(src),
+                                                          p_impl(new Impl(*src.p_impl)) {}
+
+template <typename T>
 void OptionNumber<T>::test(ValueType value) const {
-    if (value < min || value > max) {
-        throw OptionValueNotAllowedError(M_("Input value {} is outside the allowed range {} ... {}"), value, min, max);
+    if (value < p_impl->min || value > p_impl->max) {
+        throw OptionValueNotAllowedError(
+            M_("Input value {} is outside the allowed range {} ... {}"), p_impl->value, p_impl->min, p_impl->max);
     }
 }
 
 template <typename T>
 T OptionNumber<T>::from_string(const std::string & value) const {
-    if (from_string_user) {
-        return from_string_user(value);
+    if (p_impl->from_string_user) {
+        return p_impl->from_string_user(value);
     }
     ValueType val;
     if (libdnf5::from_string<ValueType>(val, value, std::dec)) {
@@ -95,7 +118,7 @@ void OptionNumber<T>::set(Priority priority, ValueType value) {
 
     if (priority >= get_priority()) {
         test(value);
-        this->value = value;
+        p_impl->value = value;
         set_priority(priority);
     }
 }
@@ -121,6 +144,27 @@ std::string OptionNumber<T>::to_string(ValueType value) const {
     oss << value;
     return oss.str();
 }
+
+template <typename T>
+OptionNumber<T> * OptionNumber<T>::clone() const {
+    return new OptionNumber<T>(*this);
+}
+
+template <typename T>
+T OptionNumber<T>::get_value() const {
+    return p_impl->value;
+}
+
+template <typename T>
+T OptionNumber<T>::get_default_value() const {
+    return p_impl->default_value;
+}
+
+template <typename T>
+std::string OptionNumber<T>::get_value_string() const {
+    return to_string(p_impl->value);
+}
+
 
 template class OptionNumber<std::int32_t>;
 template class OptionNumber<std::uint32_t>;
