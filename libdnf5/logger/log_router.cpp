9f94dda8f83bd7bc830951ceadef3d502ec55e7a
Aleš Matěj <amatej@redhat.com>
2024-04-04 12:05:23 +0200

    Add pImpl to `libdnf5::LogRouter`

--- libdnf5/logger/log_router.cpp
+++ libdnf5/logger/log_router.cpp
@@ -21,16 +21,48 @@ along with libdnf.  If not, see <https://www.gnu.org/licenses/>.
 
 namespace libdnf5 {
 
+class LogRouter::Impl {
+public:
+    Impl(){};
+    Impl(std::vector<std::unique_ptr<Logger>> && loggers) : loggers(std::move(loggers)) {}
+
+private:
+    friend LogRouter;
+    std::vector<std::unique_ptr<Logger>> loggers;
+};
+
+LogRouter::LogRouter() : p_impl(new Impl()){};
+
+LogRouter::~LogRouter() = default;
+
+LogRouter::LogRouter(std::vector<std::unique_ptr<Logger>> && loggers) : p_impl(new Impl(std::move(loggers))) {}
+
+void LogRouter::add_logger(std::unique_ptr<Logger> && logger) {
+    p_impl->loggers.push_back(std::move(logger));
+}
+
+Logger * LogRouter::get_logger(size_t index) {
+    return p_impl->loggers.at(index).get();
+}
+
+void LogRouter::swap_logger(std::unique_ptr<Logger> & logger, size_t index) {
+    p_impl->loggers.at(index).swap(logger);
+}
+
+size_t LogRouter::get_loggers_count() const noexcept {
+    return p_impl->loggers.size();
+}
+
 std::unique_ptr<Logger> LogRouter::release_logger(size_t index) {
-    auto ret = std::move(loggers.at(index));
-    loggers.erase(loggers.begin() + static_cast<int>(index));
+    auto ret = std::move(p_impl->loggers.at(index));
+    p_impl->loggers.erase(p_impl->loggers.begin() + static_cast<int>(index));
     return ret;
 }
 
 void LogRouter::log_line(Level level, const std::string & message) noexcept {
     auto now = std::chrono::system_clock::now();
     auto pid = getpid();
-    for (auto & logger : loggers) {
+    for (auto & logger : p_impl->loggers) {
         logger->write(now, pid, level, message);
     }
 }
@@ -41,7 +73,7 @@ void LogRouter::write(
     pid_t pid,
     Level level,
     const std::string & message) noexcept {
-    for (auto & logger : loggers) {
+    for (auto & logger : p_impl->loggers) {
         logger->write(time, pid, level, message);
     }
 }
