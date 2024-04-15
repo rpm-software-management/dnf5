6aa1ea12e6b5211d580f75a38b3c49e0415e30a1
Jaroslav Rohel <jrohel@redhat.com>
2024-04-15 09:39:21 +0200

    dnf5 IPlugin: Do not use inline methods on API
    And increases the dnf5 plugin API version to 2.0.

---
+++ dnf5/iplugin.cpp
@@ -0,0 +1,35 @@
+/*
+Copyright Contributors to the libdnf project.
+
+This file is part of libdnf: https://github.com/rpm-software-management/libdnf/
+
+Libdnf is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 2.1 of the License, or
+(at your option) any later version.
+
+Libdnf is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with libdnf.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+
+#include "dnf5/iplugin.hpp"
+
+namespace dnf5 {
+
+IPlugin::IPlugin(Context & context) : context(&context) {}
+
+IPlugin::~IPlugin() = default;
+
+void IPlugin::init() {}
+
+Context & IPlugin::get_context() const noexcept {
+    return *context;
+}
+
+}  // namespace dnf5
