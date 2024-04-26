rpm-software-management/dnf5

commit 5f8b76ad7e0faa5c5b151e4301417f004bc4470e

Add `--store` option for storing arbitrary transaction
It is not in the `module` command beacuse we cannot serialize module
operations yet.

dnf5/context.cpp
@@ -25,6 +25,8 @@ along with libdnf.  If not, see <https://www.gnu.org/licenses/>.
 #include "utils/string.hpp"
 #include "utils/url.hpp"
 
+#include "libdnf5/utils/fs/file.hpp"
+
 #include <fmt/format.h>
 #include <libdnf5-cli/progressbar/multi_progress_bar.hpp>
 #include <libdnf5-cli/tty.hpp>
@@ -404,7 +406,35 @@ void Context::download_and_run(libdnf5::base::Transaction & transaction) {
     if (should_store_offline) {
         base.get_config().get_tsflags_option().set(libdnf5::Option::Priority::RUNTIME, "test");
     }
-    transaction.download();
+
+    if (!transaction_store_path.empty()) {
+        auto transaction_location = transaction_store_path / "transaction.json";
+        constexpr const char * packages_in_trans_dir{"./packages"};
+        auto packages_location = transaction_store_path / packages_in_trans_dir;
+        constexpr const char * comps_in_trans_dir{"./comps"};
+        auto comps_location = transaction_store_path / comps_in_trans_dir;
+        if (std::filesystem::exists(transaction_location)) {
+            std::cout << libdnf5::utils::sformat(
+                _("Location \"{}\" already contains a stored transaction, it will be overwritten.\n"),
+                transaction_store_path.string());
+            if (libdnf5::cli::utils::userconfirm::userconfirm(base.get_config())) {
+                std::filesystem::remove_all(packages_location);
+                std::filesystem::remove_all(comps_location);
+            } else {
+                throw libdnf5::cli::AbortedByUserError();
+            }
+        }
+        auto & destdir_opt = base.get_config().get_destdir_option();
+        destdir_opt.set(packages_location);
+        std::filesystem::create_directories(transaction_store_path);
+        transaction.download();
+        transaction.store_comps(comps_location);
+        libdnf5::utils::fs::File transfile(transaction_location, "w");
+        transfile.write(transaction.serialize(packages_in_trans_dir, comps_in_trans_dir));
+        return;
+    } else {
+        transaction.download();
+    }
 
     if (base.get_config().get_downloadonly_option().get_value()) {
         return;

dnf5/shared_options.cpp
@@ -96,5 +96,23 @@ void create_offline_option(dnf5::Command & command) {
     command.get_argument_parser_command()->register_named_arg(offline);
 }
 
+void create_store_option(dnf5::Command & command) {
+    auto & ctx = command.get_context();
+    auto & parser = ctx.get_argument_parser();
+    auto * store = parser.add_new_named_arg("store");
+    store->set_long_name("store");
+    store->set_has_value(true);
+    store->set_description("Store the current transaction in a directory at the specified path instead of running it.");
+    store->set_arg_value_help("STORED_TRANSACTION_PATH");
+    command.get_argument_parser_command()->register_named_arg(store);
+    store->set_parse_hook_func([&ctx](
+                                   [[maybe_unused]] libdnf5::cli::ArgumentParser::NamedArg * arg,
+                                   [[maybe_unused]] const char * option,
+                                   const char * value) {
+        ctx.transaction_store_path = value;
+        return true;
+    });
+}
+
 
 }  // namespace dnf5

dnf5/commands/autoremove/autoremove.cpp
@@ -37,6 +37,7 @@ void AutoremoveCommand::set_argument_parser() {
     get_argument_parser_command()->set_description(
         "Remove all unneeded packages originally installed as dependencies.");
     create_offline_option(*this);
+    create_store_option(*this);
 }
 
 void AutoremoveCommand::configure() {

dnf5/commands/clean/clean.cpp
@@ -20,6 +20,8 @@ along with libdnf.  If not, see <https://www.gnu.org/licenses/>.
 
 #include "clean.hpp"
 
+#include "dnf5/shared_options.hpp"
+
 #include <libdnf5-cli/argument_parser.hpp>
 #include <libdnf5/repo/repo_cache.hpp>
 #include <libdnf5/utils/bgettext/bgettext-lib.h>
@@ -125,6 +127,7 @@ void CleanCommand::set_argument_parser() {
     });
 
     cmd.register_positional_arg(cache_types);
+    create_store_option(*this);
 }
 
 void CleanCommand::run() {

dnf5/commands/distro-sync/distro-sync.cpp
@@ -53,6 +53,7 @@ void DistroSyncCommand::set_argument_parser() {
     auto skip_broken = std::make_unique<SkipBrokenOption>(*this);
     auto skip_unavailable = std::make_unique<SkipUnavailableOption>(*this);
     create_offline_option(*this);
+    create_store_option(*this);
 }
 
 void DistroSyncCommand::configure() {

dnf5/commands/downgrade/downgrade.cpp
@@ -57,6 +57,7 @@ void DowngradeCommand::set_argument_parser() {
     create_allow_downgrade_options(*this);
     create_downloadonly_option(*this);
     create_offline_option(*this);
+    create_store_option(*this);
 }
 
 void DowngradeCommand::configure() {

dnf5/commands/group/group_install.cpp
@@ -44,6 +44,7 @@ void GroupInstallCommand::set_argument_parser() {
     create_allow_downgrade_options(*this);
     create_downloadonly_option(*this);
     create_offline_option(*this);
+    create_store_option(*this);
 }
 
 void GroupInstallCommand::configure() {

dnf5/commands/group/group_remove.cpp
@@ -35,6 +35,7 @@ void GroupRemoveCommand::set_argument_parser() {
     no_packages = std::make_unique<GroupNoPackagesOption>(*this);
     group_specs = std::make_unique<GroupSpecArguments>(*this, ArgumentParser::PositionalArg::AT_LEAST_ONE);
     create_offline_option(*this);
+    create_store_option(*this);
 }
 
 void GroupRemoveCommand::configure() {

commit b459128b2fc7905831d5cdb333957e23a2d5030e

system-upgrade: adapt to new transaction serialization format
System-upgrade was broken by 5f8b76ad7e0faa5c5b151e4301417f004bc4470e,
since RPMs of serialized transactions are now expected to be stored in a
single flat directory (like that created by `dnf5 download --destdir
./packages`) rather than the directory structure used by the DNF cache
(in `/path/to/repository/cachedir/packages`)

We would like to set `destdir` to download the packages to a single flat
directory, but then we can't test the transaction as usual because of
the limitation described in
https://github.com/rpm-software-management/dnf5/pull/1301. So instead,
we serialize the transaction without testing it, then build a new "test
transaction" from the serialized transaction and test that. This
solution may seem like a hack, but it's actually quite nice, since we
can a test the offline transaction exactly how it will be performed
later by `dnf5 offline _execute`.

Due to the improvements from
https://github.com/rpm-software-management/dnf5/pull/1264, we can now
create the offline transaction without making a copy of the entire
repository cache, and we can execute it without loading the repositories
at all.

dnf5/commands/offline/offline.cpp
@@ -406,7 +406,8 @@ void OfflineExecuteCommand::configure() {
     check_state(*state);
 
     ctx.set_load_system_repo(true);
-    ctx.set_load_available_repos(Context::LoadAvailableRepos::ENABLED);
+    // Stored transactions can be replayed without loading available repos
+    ctx.set_load_available_repos(Context::LoadAvailableRepos::NONE);
 
     // Get the cache from the cachedir specified in the state file
     ctx.get_base().get_config().get_system_cachedir_option().set(state->get_data().cachedir);
@@ -444,10 +445,10 @@ void OfflineExecuteCommand::run() {
     state->get_data().status = dnf5::offline::STATUS_TRANSACTION_INCOMPLETE;
     state->write();
 
-    const auto & installroot = ctx.base.get_config().get_installroot_option().get_value();
+    const auto & installroot = ctx.get_base().get_config().get_installroot_option().get_value();
     const auto & datadir = installroot / dnf5::offline::DEFAULT_DATADIR.relative_path();
     std::filesystem::create_directories(datadir);
-    const auto & transaction_json_path = datadir / dnf5::offline::TRANSACTION_JSON_FILENAME;
+    const auto & transaction_json_path = datadir / "transaction.json";
 
     const auto & goal = std::make_unique<libdnf5::Goal>(ctx.get_base());
 

dnf5/context.cpp
@@ -312,6 +312,11 @@ void Context::Impl::store_offline(libdnf5::base::Transaction & transaction) {
     const auto & offline_datadir = installroot / dnf5::offline::DEFAULT_DATADIR.relative_path();
     std::filesystem::create_directories(offline_datadir);
 
+    constexpr const char * packages_in_trans_dir{"./packages"};
+    const auto & packages_location = offline_datadir / packages_in_trans_dir;
+    constexpr const char * comps_in_trans_dir{"./comps"};
+    const auto & comps_location = offline_datadir / comps_in_trans_dir;
+
     const std::filesystem::path state_path{offline_datadir / dnf5::offline::TRANSACTION_STATE_FILENAME};
     dnf5::offline::OfflineTransactionState state{state_path};
 
@@ -320,19 +325,50 @@ void Context::Impl::store_offline(libdnf5::base::Transaction & transaction) {
                   << "\t" << state.get_data().cmd_line << std::endl
                   << "Continuing will cancel the old offline transaction and replace it with this one." << std::endl;
         if (!libdnf5::cli::utils::userconfirm::userconfirm(base.get_config())) {
-            throw libdnf5::cli::SilentCommandExitError(0);
+            throw libdnf5::cli::AbortedByUserError();
         }
     }
 
-    const std::filesystem::path transaction_json_path{offline_datadir / dnf5::offline::TRANSACTION_JSON_FILENAME};
+    state.get_data().status = dnf5::offline::STATUS_DOWNLOAD_INCOMPLETE;
+    state.write();
 
-    const std::string json = transaction.serialize();
+    // First, serialize the transaction
+    transaction.store_comps(comps_location);
 
-    auto transaction_json_file = libdnf5::utils::fs::File(transaction_json_path, "w");
-
-    transaction_json_file.write(json);
+    const auto transaction_json_path = offline_datadir / "transaction.json";
+    libdnf5::utils::fs::File transaction_json_file{transaction_json_path, "w"};
+    transaction_json_file.write(transaction.serialize(packages_in_trans_dir, comps_in_trans_dir));
     transaction_json_file.close();
 
+    // Then, test the serialized transaction
+    const auto & goal = std::make_unique<libdnf5::Goal>(base);
+    goal->add_serialized_transaction(transaction_json_path);
+    auto test_transaction = goal->resolve();
+    if (test_transaction.get_problems() != libdnf5::GoalProblem::NO_PROBLEM) {
+        throw libdnf5::cli::GoalResolveError(transaction);
+    }
+    base.get_config().get_tsflags_option().set(libdnf5::Option::Priority::RUNTIME, "test");
+
+    print_info("\nTesting offline transaction");
+    auto result = test_transaction.run();
+    if (result != libdnf5::base::Transaction::TransactionRunResult::SUCCESS) {
+        std::cerr << "Transaction failed: " << libdnf5::base::Transaction::transaction_result_to_string(result)
+                  << std::endl;
+        for (auto const & entry : transaction.get_gpg_signature_problems()) {
+            std::cerr << entry << std::endl;
+        }
+        for (auto & problem : test_transaction.get_transaction_problems()) {
+            std::cerr << "  - " << problem << std::endl;
+        }
+        throw libdnf5::cli::SilentCommandExitError(1);
+    }
+
+    for (auto const & entry : test_transaction.get_gpg_signature_problems()) {
+        std::cerr << entry << std::endl;
+    }
+
+    // Download and transaction test complete. Fill out entries in offline
+    // transaction state file.
     state.get_data().status = dnf5::offline::STATUS_DOWNLOAD_COMPLETE;
     state.get_data().cachedir = base.get_config().get_cachedir_option().get_value();
 
@@ -359,10 +395,6 @@ void Context::Impl::store_offline(libdnf5::base::Transaction & transaction) {
 }
 
 void Context::Impl::download_and_run(libdnf5::base::Transaction & transaction) {
-    if (should_store_offline) {
-        base.get_config().get_tsflags_option().set(libdnf5::Option::Priority::RUNTIME, "test");
-    }
-
     if (!transaction_store_path.empty()) {
         auto transaction_location = transaction_store_path / "transaction.json";
         constexpr const char * packages_in_trans_dir{"./packages"};
@@ -388,11 +420,27 @@ void Context::Impl::download_and_run(libdnf5::base::Transaction & transaction) {
         libdnf5::utils::fs::File transfile(transaction_location, "w");
         transfile.write(transaction.serialize(packages_in_trans_dir, comps_in_trans_dir));
         return;
-    } else {
-        transaction.download();
     }
 
+    if (should_store_offline) {
+        const auto & installroot = base.get_config().get_installroot_option().get_value();
+        const auto & offline_datadir = installroot / dnf5::offline::DEFAULT_DATADIR.relative_path();
+        std::filesystem::create_directories(offline_datadir);
+
+        base.get_config().get_destdir_option().set(offline_datadir / "packages");
+    }
+
+    transaction.download();
     if (base.get_config().get_downloadonly_option().get_value()) {
+        return;
+    }
+
+    if (should_store_offline) {
+        store_offline(transaction);
+        std::cout << "Transaction stored to be performed offline. Run `dnf5 offline reboot` to reboot and run the "
+                     "transaction. To cancel the transaction and delete the downloaded files, use `dnf5 "
+                     "offline clean`."
+                  << std::endl;
         return;
     }
 
@@ -439,14 +487,6 @@ void Context::Impl::download_and_run(libdnf5::base::Transaction & transaction) {
         std::cerr << entry << std::endl;
     }
     // TODO(mblaha): print a summary of successful transaction
-
-    if (should_store_offline) {
-        store_offline(transaction);
-        std::cout << "Transaction stored to be performed offline. Run `dnf5 offline reboot` to reboot and run the "
-                     "transaction. To cancel the transaction and delete the downloaded files, use `dnf5 "
-                     "offline clean`."
-                  << std::endl;
-    }
 }
 
 libdnf5::Goal * Context::Impl::get_goal(bool new_if_not_exist) {

dnf5/include/dnf5/offline.hpp
@@ -43,12 +43,11 @@ const std::string STATUS_DOWNLOAD_COMPLETE{"download-complete"};
 const std::string STATUS_READY{"ready"};
 const std::string STATUS_TRANSACTION_INCOMPLETE{"transaction-incomplete"};
 
-const int STATE_VERSION = 0;
+const int STATE_VERSION = 1;
 const std::string STATE_HEADER{"offline-transaction-state"};
 
 const std::filesystem::path DEFAULT_DATADIR{std::filesystem::path(libdnf5::SYSTEM_STATE_DIR) / "offline"};
 const std::filesystem::path TRANSACTION_STATE_FILENAME{"offline-transaction-state.toml"};
-const std::filesystem::path TRANSACTION_JSON_FILENAME{"offline-transaction.json"};
 
 struct OfflineTransactionStateData {
     int state_version = STATE_VERSION;
@@ -59,8 +58,6 @@ struct OfflineTransactionStateData {
     std::string verb;
     std::string cmd_line;
     bool poweroff_after = false;
-    std::vector<std::string> enabled_repos;
-    std::vector<std::string> disabled_repos;
     std::string module_platform_id;
 };
 
@@ -98,8 +95,6 @@ TOML11_DEFINE_CONVERSION_NON_INTRUSIVE(
     verb,
     cmd_line,
     poweroff_after,
-    enabled_repos,
-    disabled_repos,
     module_platform_id)
 
 #endif  // DNF5_OFFLINE_HPP

dnf5/shared_options.cpp
@@ -115,11 +115,6 @@ void create_offline_option(dnf5::Command & command) {
                                      [[maybe_unused]] libdnf5::cli::ArgumentParser::NamedArg * arg,
                                      [[maybe_unused]] const char * option,
                                      [[maybe_unused]] const char * value) {
-        // The installroot will be prepended to the cachedir and system_cachedir later in Base.setup()
-        const auto & offline_datadir = dnf5::offline::DEFAULT_DATADIR;
-        ctx.get_base().get_config().get_system_cachedir_option().set(
-            libdnf5::Option::Priority::INSTALLROOT, offline_datadir);
-        ctx.get_base().get_config().get_cachedir_option().set(libdnf5::Option::Priority::INSTALLROOT, offline_datadir);
         ctx.set_should_store_offline(true);
         return true;
     });