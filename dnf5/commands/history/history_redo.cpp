96891cb7f4d773ef572ecb7b194ad2758517ea13
Aleš Matěj <amatej@redhat.com>
2024-07-25 08:40:08 +0200

    Add `history redo` command
    It adds a new goal API to redo a history transaction.

--- dnf5/commands/history/history_redo.cpp
+++ dnf5/commands/history/history_redo.cpp
@@ -19,14 +19,54 @@ along with libdnf.  If not, see <https://www.gnu.org/licenses/>.
 
 #include "history_redo.hpp"
 
+#include "commands/history/transaction_id.hpp"
+#include "dnf5/shared_options.hpp"
+
+#include <libdnf5/utils/bgettext/bgettext-mark-domain.h>
+
 namespace dnf5 {
 
 using namespace libdnf5::cli;
 
 void HistoryRedoCommand::set_argument_parser() {
-    get_argument_parser_command()->set_description("Repeat transactions");
+    get_argument_parser_command()->set_description("Repeat all actions from the specified transaction");
+    transaction_specs = std::make_unique<TransactionSpecArguments>(*this, 1);
+    auto & ctx = get_context();
+    transaction_specs->get_arg()->set_complete_hook_func(create_history_id_autocomplete(ctx));
+    auto skip_unavailable = std::make_unique<SkipUnavailableOption>(*this);
 }
 
-void HistoryRedoCommand::run() {}
+void HistoryRedoCommand::configure() {
+    auto & context = get_context();
+    context.set_load_system_repo(true);
+    context.set_load_available_repos(Context::LoadAvailableRepos::ENABLED);
+}
+
+void HistoryRedoCommand::run() {
+    auto ts_specs = transaction_specs->get_value();
+    libdnf5::transaction::TransactionHistory history(get_context().get_base());
+    std::vector<libdnf5::transaction::Transaction> target_trans;
+
+    target_trans = list_transactions_from_specs(history, ts_specs);
+
+    if (target_trans.size() < 1) {
+        throw libdnf5::cli::CommandExitError(1, M_("No matching transaction ID found, exactly one required."));
+    }
+
+    if (target_trans.size() > 1) {
+        throw libdnf5::cli::CommandExitError(1, M_("Matched more than one transaction ID, exactly one required."));
+    }
+
+    auto goal = get_context().get_goal();
+    // To enable removal of dependency packages not present in the selected transaction
+    // it requires allow_erasing. This will inform the user that additional removes
+    // are required and the transaction won't proceed without --ignore-extras.
+    goal->set_allow_erasing(true);
+
+    auto settings = libdnf5::GoalJobSettings();
+    settings.set_ignore_extras(true);
+    settings.set_ignore_installed(true);
+    goal->add_redo_transaction(target_trans[0], settings);
+}
 
 }  // namespace dnf5
