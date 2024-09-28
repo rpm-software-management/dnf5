e93df4a3e84783da08442a41dedc72525497dbee
Ali Mirjamali <ali@mirjamali.com>
2024-09-28 17:40:23 +0330

    Add --allmirros option for `dnf download --url`
    resolves: https://github.com/rpm-software-management/dnf5/issues/1735

--- dnf5/commands/download/download.hpp
+++ dnf5/commands/download/download.hpp
@@ -47,6 +47,7 @@ private:
     libdnf5::OptionBool * resolve_option{nullptr};
     libdnf5::OptionBool * alldeps_option{nullptr};
     libdnf5::OptionBool * url_option{nullptr};
+    libdnf5::OptionBool * allmirrors_option{nullptr};
     libdnf5::OptionBool * srpm_option{nullptr};
 
     std::vector<std::unique_ptr<libdnf5::Option>> * patterns_to_download_options{nullptr};
