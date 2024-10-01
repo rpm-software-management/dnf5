463742d981b4e03505c2f14a108d3523ff7367d4
Jaroslav Rohel <jrohel@redhat.com>
2024-10-01 10:27:20 +0200

    Unit tests for libdnf5::utils::[is_glob_pattern | is_file_pattern]
    Includes C++, Perl, Python and Ruby tests.

---
+++ test/python3/libdnf5/utils/test_patterns.py
@@ -0,0 +1,36 @@
+# Copyright Contributors to the libdnf project.
+#
+# This file is part of libdnf: https://github.com/rpm-software-management/libdnf/
+#
+# Libdnf is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# Libdnf is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with libdnf.  If not, see <https://www.gnu.org/licenses/>.
+
+import unittest
+
+import libdnf5.utils
+
+
+class TestIsXPattern(unittest.TestCase):
+    def test_is_file_pattern(self):
+        self.assertFalse(libdnf5.utils.is_file_pattern(''))
+        self.assertFalse(libdnf5.utils.is_file_pattern('no_file_pattern'))
+        self.assertFalse(libdnf5.utils.is_file_pattern('no_file/pattern'))
+        self.assertTrue(libdnf5.utils.is_file_pattern('/pattern'))
+        self.assertTrue(libdnf5.utils.is_file_pattern('*/pattern'))
+
+    def test_is_glob_pattern(self):
+        self.assertFalse(libdnf5.utils.is_glob_pattern(''))
+        self.assertFalse(libdnf5.utils.is_glob_pattern('no_glob_pattern'))
+        self.assertTrue(libdnf5.utils.is_glob_pattern('glob*_pattern'))
+        self.assertTrue(libdnf5.utils.is_glob_pattern('glob[sdf]_pattern'))
+        self.assertTrue(libdnf5.utils.is_glob_pattern('glob?_pattern'))
