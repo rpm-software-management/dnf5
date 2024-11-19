d745332008b5b39d139aafcf93895a2de57d2386
Marek Blaha <mblaha@redhat.com>
2024-11-19 17:28:49 +0100

    repo: Add option to download all repository metadata
    Needed for `dnf5 reposync --download-metadata` command.

--- include/libdnf5/conf/const.hpp
+++ include/libdnf5/conf/const.hpp
@@ -63,6 +63,7 @@ constexpr const char * METADATA_TYPE_FILELISTS = "filelists";
 constexpr const char * METADATA_TYPE_OTHER = "other";
 constexpr const char * METADATA_TYPE_PRESTO = "presto";
 constexpr const char * METADATA_TYPE_UPDATEINFO = "updateinfo";
+constexpr const char * METADATA_TYPE_ALL = "all";
 
 const std::set<std::string> OPTIONAL_METADATA_TYPES{
     METADATA_TYPE_COMPS, METADATA_TYPE_FILELISTS, METADATA_TYPE_OTHER, METADATA_TYPE_PRESTO, METADATA_TYPE_UPDATEINFO};
