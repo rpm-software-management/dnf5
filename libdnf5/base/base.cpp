a4e16c5356c69ff0fd8e95ac6f88ac9f0b385ce8
Aleš Matěj <amatej@redhat.com>
2025-01-09 14:09:53 +0100

    Load `libdnf5::system::State` only when required
    Commands that don't need system state are more efficient because they
    don't have to load it.
    
    They also don't run into problems caused be the dnf4 history conversion.

--- libdnf5/base/base.cpp
+++ libdnf5/base/base.cpp
@@ -232,39 +232,6 @@ void Base::setup() {
 
     vars->load(vars_installroot, config.get_varsdir_option().get_value());
 
-    // TODO(mblaha) - move system state load closer to the system repo loading
-    std::filesystem::path system_state_dir{config.get_system_state_dir_option().get_value()};
-    p_impl->system_state.emplace(installroot.get_value() / system_state_dir.relative_path());
-
-    auto & system_state = p_impl->get_system_state();
-
-    // TODO(mblaha) - this is temporary override of modules state by reading
-    // dnf4 persistor from /etc/dnf/modules.d/
-    // Remove once reading of dnf4 data is not needed
-    libdnf5::dnf4convert::Dnf4Convert convertor(get_weak_ptr());
-#ifdef WITH_MODULEMD
-    if ((!std::filesystem::exists(system_state.get_module_state_path()))) {
-        system_state.reset_module_states(convertor.read_module_states());
-    }
-#endif
-
-    if (system_state.packages_import_required()) {
-        // TODO(mblaha) - first try dnf5 history database, then fall back to dnf4
-        std::map<std::string, libdnf5::system::PackageState> package_states;
-        std::map<std::string, libdnf5::system::NevraState> nevra_states;
-        std::map<std::string, libdnf5::system::GroupState> group_states;
-        std::map<std::string, libdnf5::system::EnvironmentState> environment_states;
-
-        if (convertor.read_package_states_from_history(
-                package_states, nevra_states, group_states, environment_states)) {
-            system_state.reset_packages_states(
-                std::move(package_states),
-                std::move(nevra_states),
-                std::move(group_states),
-                std::move(environment_states));
-        }
-    }
-
     config.get_varsdir_option().lock("Locked by Base::setup()");
     pool_setdisttype(**pool, DISTTYPE_RPM);
     // TODO(jmracek) - architecture variable is changeable therefore architecture in vars must be synchronized with RpmPool
@@ -336,6 +303,44 @@ void Base::set_download_callbacks(std::unique_ptr<repo::DownloadCallbacks> && do
 }
 repo::DownloadCallbacks * Base::get_download_callbacks() {
     return p_impl->download_callbacks.get();
+}
+
+libdnf5::system::State & Base::Impl::get_system_state() {
+    if (system_state) {
+        return *system_state;
+    }
+
+    std::filesystem::path system_state_dir{config.get_system_state_dir_option().get_value()};
+    system_state.emplace(config.get_installroot_option().get_value() / system_state_dir.relative_path());
+
+    // TODO(mblaha) - this is temporary override of modules state by reading
+    // dnf4 persistor from /etc/dnf/modules.d/
+    // Remove once reading of dnf4 data is not needed
+    libdnf5::dnf4convert::Dnf4Convert convertor(repo_sack.get_base());
+#ifdef WITH_MODULEMD
+    if ((!std::filesystem::exists(system_state->get_module_state_path()))) {
+        system_state->reset_module_states(convertor.read_module_states());
+    }
+#endif
+
+    if (system_state->packages_import_required()) {
+        // TODO(mblaha) - first try dnf5 history database, then fall back to dnf4
+        std::map<std::string, libdnf5::system::PackageState> package_states;
+        std::map<std::string, libdnf5::system::NevraState> nevra_states;
+        std::map<std::string, libdnf5::system::GroupState> group_states;
+        std::map<std::string, libdnf5::system::EnvironmentState> environment_states;
+
+        if (convertor.read_package_states_from_history(
+                package_states, nevra_states, group_states, environment_states)) {
+            system_state->reset_packages_states(
+                std::move(package_states),
+                std::move(nevra_states),
+                std::move(group_states),
+                std::move(environment_states));
+        }
+    }
+
+    return *system_state;
 }
 
 }  // namespace libdnf5
