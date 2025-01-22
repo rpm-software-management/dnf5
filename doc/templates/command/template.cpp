b0f1caf82fb9f6fdc2c1ea2d3cdcc4661e267aef
FeRD (Frank Dana) <ferdnyc@gmail.com>
2025-01-22 12:04:12 -0500

    Docs: Explain template.cpp in more detail

--- doc/templates/command/template.cpp
+++ doc/templates/command/template.cpp
@@ -27,15 +27,15 @@ void TemplateCommand::set_argument_parser() {
     auto & ctx = get_context();
     auto & parser = ctx.get_argument_parser();
 
-    auto & cmd = *get_argument_parser_command();
+    auto & this_cmd = *get_argument_parser_command();
 
-    // Add template command to the Parser
+    // Configure 'template' command in the Parser
     //
-    // Set a description that would be displayed in the second column of the
-    // help message would also add the command to the parser
-    cmd.set_description("A command that prints its name and arguments' name");
+    // Set a description that would be displayed in the second column of
+    // the main help message.
+    this_cmd.set_description("A command that prints its name and arguments' name");
 
-    // Add foo and bar options
+    // Add '--foo' and '--bar' options
     //
     // Option 1: as said in the header file here you should handle the pointer
     // by giving up the ownership to dnf5's parser.
@@ -46,16 +46,19 @@ void TemplateCommand::set_argument_parser() {
                 new libdnf5::OptionBool(false))));
 
     // Create an option by giving it a name. It will be shown in the help message.
-    // Set long name, description and constant value.
-    // Link the option to the TemplateCommand's class member.
     auto foo = parser.add_new_named_arg("foo");
+    // Set the long name for the option.
     foo->set_long_name("foo");
+    // Set the option's description (for the help message).
     foo->set_description("print foo");
+    // Set the constant value. This is the value assigned when an option
+    // which takes no value arguments appears on the command line.
     foo->set_const_value("true");
+    // Link the option to the TemplateCommand's class member.
     foo->link_value(foo_option);
 
-    // Register the 'foo' argument to the command template.
-    cmd.register_named_arg(foo);
+    // Register the '--foo' argument to the 'template' command.
+    this_cmd.register_named_arg(foo);
 
     // Option 2: create a unique_ptr of type BarOption.
     // The long name, description, and other values were set in
