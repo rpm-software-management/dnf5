b08497b2efd0f3ef8b31d2ac993011fc7842e89b
Jaroslav Rohel <jrohel@redhat.com>
2025-02-02 22:02:13 +0100

    bgettext: Add function b_gettextmsg_get_plural_id
    Returns the plural message id from the encoded message.

--- include/libdnf5/utils/bgettext/bgettext-mark-common.h
+++ include/libdnf5/utils/bgettext/bgettext-mark-common.h
@@ -38,6 +38,11 @@ const char * b_gettextmsg_get_domain(struct BgettextMessage message);
 /// @return message id
 const char * b_gettextmsg_get_id(struct BgettextMessage message);
 
+/// Returns message plural id from encoded message.
+/// @param message message encoded for translation
+/// @return plural message id or NULL if it is not present in the encoded message
+const char * b_gettextmsg_get_plural_id(struct BgettextMessage message);
+
 /// Attempts to translate the 'message' into the user's language by searching for the translation in a message catalog.
 /// @param domain message domain used for translation, argument is used only if the domain is not present in encoded message
 /// @param message message encoded for translation
