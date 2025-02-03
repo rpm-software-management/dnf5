c6331e508d0185eaea59e798dcbee13d5ce42eee
Jaroslav Rohel <jrohel@redhat.com>
2025-02-03 13:06:48 +0100

    utils::format: Support for user defined locale
    A new class `utils::Locale` has been created. It contains C and CPP
    locale.
    
    A variant of `utils::format` has been created which adds
    the `utils::Locale` argument. This allows the user to specify a locale
    for formatting.

--- include/libdnf5/utils/format_locale.hpp
+++ include/libdnf5/utils/format_locale.hpp
@@ -21,6 +21,7 @@ along with libdnf.  If not, see <https://www.gnu.org/licenses/>.
 #define LIBDNF5_UTILS_FORMAT_LOCALE_HPP
 
 #include "bgettext/bgettext-mark-common.h"
+#include "locale.hpp"
 
 #include <fmt/format.h>
 
@@ -50,6 +51,44 @@ std::string format(bool translate, BgettextMessage format_string, unsigned long 
     }
 
     return fmt::vformat(final_format_string, fmt::make_format_args(args...));
+}
+
+
+/// Format `args` according to the `format_string`, and return the result as a string.
+///
+/// @param locale        requested locale for translation and argument formating, nullptr = use global/thread locale
+/// @param translate     If `true`, it will attempt to translate the message to the requested locale.
+/// @param format_string message contains formating string
+/// @param plural_form   number is used to select the appropriate plural form of the format string
+/// @param args          arguments to be formated
+/// @return A string object holding the formatted result.
+template <typename... Args>
+std::string format(
+    const Locale * locale, bool translate, BgettextMessage format_string, unsigned long plural_form, Args &&... args) {
+    const char * final_format_string{nullptr};
+
+    if (!locale) {
+        return utils::format(translate, format_string, plural_form, args...);
+    }
+
+    if (translate) {
+        // sets the requested C locale for this thread, needed for bgettext
+        ::locale_t previous_locale = uselocale(locale->get_c_locale());
+
+        final_format_string = b_dmgettext(NULL, format_string, plural_form);
+
+        // restores the previous C locale for this thread
+        uselocale(previous_locale);
+    } else {
+        if (plural_form > 1) {
+            final_format_string = b_gettextmsg_get_plural_id(format_string);
+        }
+        if (!final_format_string) {
+            final_format_string = b_gettextmsg_get_id(format_string);
+        }
+    }
+
+    return fmt::vformat(locale->get_cpp_locale(), final_format_string, fmt::make_format_args(args...));
 }
 
 }  // namespace libdnf5::utils
