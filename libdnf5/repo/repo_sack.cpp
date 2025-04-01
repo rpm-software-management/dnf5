530019ee290974677658c1910819c252fa019d4b
Marek Blaha <mblaha@redhat.com>
2025-04-01 14:03:04 +0200

    repo: Allow system_cachedir copy for the root user
    Even for the root user it might be useful to leverage copy of the
    system_cachedir to actual cachedir location. For example if the cachedir
    location is altered using --setopt, or for dnfdaemon which uses its own
    cachedir location.

--- libdnf5/repo/repo_sack.cpp
+++ libdnf5/repo/repo_sack.cpp
@@ -565,7 +565,7 @@ void RepoSack::Impl::update_and_load_repos(libdnf5::repo::RepoQuery & repos, boo
                     send_to_sack_loader(repo);
                 } else {
                     // Try reusing the root cache
-                    if (!root_cache_tried && !libdnf5::utils::am_i_root() && repo->clone_root_metadata()) {
+                    if (!root_cache_tried && repo->clone_root_metadata()) {
                         root_cache_tried = true;
                         continue;
                     }
