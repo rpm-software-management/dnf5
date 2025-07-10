f01657d0a6d2ffe0009dea4e48a331621a7835db
Marek Blaha <mblaha@redhat.com>
2025-07-10 13:03:53 +0200

    cli: Add pImpl to libdnf5::cli::progressbar::MultiProgressBar

--- include/libdnf5-cli/progressbar/multi_progress_bar.hpp
+++ include/libdnf5-cli/progressbar/multi_progress_bar.hpp
@@ -27,6 +27,8 @@
 
 #include "libdnf5-cli/defs.h"
 
+#include "libdnf5/common/impl_ptr.hpp"
+
 #include <iostream>
 #include <memory>
 #include <vector>
@@ -42,21 +44,28 @@ public:
     explicit MultiProgressBar();
     ~MultiProgressBar();
 
+    MultiProgressBar(const MultiProgressBar & src) = delete;
+    MultiProgressBar & operator=(const MultiProgressBar & src) = delete;
+
+    MultiProgressBar(MultiProgressBar && src) noexcept = delete;
+    MultiProgressBar & operator=(MultiProgressBar && src) noexcept = delete;
+
     void add_bar(std::unique_ptr<ProgressBar> && bar);
 
     // In interactive mode MultiProgressBar doesn't print a newline after unfinished progressbars.
     // Finished progressbars always end with a newline.
-    void print() {
-        std::cerr << *this;
-        std::cerr << std::flush;
-    }
+    void print();
+
     LIBDNF_CLI_API friend std::ostream & operator<<(std::ostream & stream, MultiProgressBar & mbar);
 
+    /// Returns the total bar.
+    DownloadProgressBar & get_total_bar() noexcept;
+
     /// Sets the minimum number of registered progress bars to show the total bar.
-    void set_total_bar_visible_limit(std::size_t value) noexcept { total_bar_visible_limit = value; }
+    void set_total_bar_visible_limit(std::size_t value) noexcept;
 
     /// Sets the visibility of number widget in the total bar.
-    void set_total_bar_number_widget_visible(bool value) noexcept { total.set_number_widget_visible(value); }
+    void set_total_bar_number_widget_visible(bool value) noexcept;
 
     /// Allows one to preset the value of the total number of progress bars.
     /// If the value is lower than the current number of registered progress bars, it is automatically increased.
@@ -67,14 +76,8 @@ public:
     std::size_t get_total_num_of_bars() const noexcept;
 
 private:
-    std::size_t total_bar_visible_limit{0};
-    std::vector<std::unique_ptr<ProgressBar>> bars_all;
-    std::vector<ProgressBar *> bars_todo;
-    std::vector<ProgressBar *> bars_done;
-    DownloadProgressBar total;
-    // Whether the last line was printed without a new line ending (such as an in progress bar)
-    bool line_printed{false};
-    std::size_t num_of_lines_to_clear{0};
+    class LIBDNF_CLI_LOCAL Impl;
+    ImplPtr<Impl> p_impl;
 };
 
 
