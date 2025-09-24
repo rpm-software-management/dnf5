fa9351f2c3de2aae6c21955c859a479c789c2375
Marek Blaha <mblaha@redhat.com>
2025-09-24 14:41:13 +0200

    Handle TOML parsing errors for ActiveTransactionInfo
    Errors are no longer silently ignored, a new
    ActiveTransactionInfoParseError is thrown instead. The error is caught
    and logged during the transaction run.

--- libdnf5/base/transaction.cpp
+++ libdnf5/base/transaction.cpp
@@ -985,10 +985,15 @@ Transaction::TransactionRunResult Transaction::Impl::_run(
     lock_file_path /= std::filesystem::path(libdnf5::TRANSACTION_LOCK_FILEPATH).relative_path();
     std::filesystem::create_directories(lock_file_path.parent_path());
 
+    auto logger = base->get_logger().get();
     TransactionLocker transaction_locker(lock_file_path, info);
     if (!transaction_locker.write_lock()) {
         auto content = transaction_locker.read_content();
-        concurrent_transaction = std::make_unique<ActiveTransactionInfo>(ActiveTransactionInfo::from_toml(content));
+        try {
+            concurrent_transaction = std::make_unique<ActiveTransactionInfo>(ActiveTransactionInfo::from_toml(content));
+        } catch (const ActiveTransactionInfoParseError & ex) {
+            logger->warning(ex.what());
+        }
         return TransactionRunResult::ERROR_LOCK;
     }
     transaction_locker.write_transaction_metadata();
@@ -1103,9 +1108,6 @@ Transaction::TransactionRunResult Transaction::Impl::_run(
     auto time = std::chrono::system_clock::now().time_since_epoch();
     db_transaction.set_dt_start(std::chrono::duration_cast<std::chrono::seconds>(time).count());
     db_transaction.start();
-
-
-    auto logger = base->get_logger().get();
 
     int pipe_out_from_scriptlets[2];
     if (pipe2(pipe_out_from_scriptlets, O_CLOEXEC) == -1) {
