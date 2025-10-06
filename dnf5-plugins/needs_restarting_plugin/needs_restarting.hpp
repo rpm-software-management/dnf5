e1cb521b8b02492aef70866ef2abaea449a8bbd4
Dave Cantrell <dcantrell@redhat.com>
2025-10-06 15:06:32 -0400

    needs-restarting: Implement the --processes and --exclude-services options
    Shaving the yak led me to dnf5 issues #1730 where the needs-restarting
    plugin lacked an option to check running processes and to optionally
    filter out processes handled by systemd services.  I have tried to
    implement that with this PR.  I used Claude to help me with C++ data
    types which is why I make use of vectors.  I also used Claude to clean
    up some compile errors, which is why you see some casting.
    
    Please check the logic here.  The idea was similar to how the services
    check works, but it's checking running processes and those
    executables.  Running processes that no longer exist on the system
    shouldn't be reported, but maybe we do want to report those.
    
    Fixes: #1730
    
    Signed-off-by: Dave Cantrell <dcantrell@redhat.com>

--- dnf5-plugins/needs_restarting_plugin/needs_restarting.hpp
+++ dnf5-plugins/needs_restarting_plugin/needs_restarting.hpp
@@ -38,10 +38,23 @@ public:
     void run() override;
 
 private:
+    struct SystemdService {
+        std::string name;
+        uint64_t start_timestamp_us;
+        sdbus::ObjectPath object_path;
+        std::string fragment_path;
+    };
+
     libdnf5::OptionBool * services_option{nullptr};
+    libdnf5::OptionBool * processes_option{nullptr};
+    libdnf5::OptionBool * exclude_services_option{nullptr};
+    static time_t get_proc_1_boot_time(Context &);
+    static time_t get_kernel_boot_time(Context &);
     static time_t get_boot_time(Context &);
     static void system_needs_restarting(Context &);
+    static std::vector<SystemdService> get_systemd_services(Context &);
     static void services_need_restarting(Context &);
+    static void processes_need_restarting(Context &, bool exclude_services);
 };
 
 }  // namespace dnf5
