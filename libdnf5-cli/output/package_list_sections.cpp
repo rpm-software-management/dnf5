b263d9f642df15bfe06c6ee77d98b507ac408612
Matej Focko <mfocko@redhat.com>
2025-11-09 14:59:19 +0100

    style: skip empty package sets instead of nesting
    When printing out the packages in `check-upgrade`, skip the empty sets
    instead of nesting the printing logic within an `if` for a non-empty
    package set.
    
    Signed-off-by: Matej Focko <mfocko@redhat.com>

--- libdnf5-cli/output/package_list_sections.cpp
+++ libdnf5-cli/output/package_list_sections.cpp
@@ -54,46 +54,49 @@ void PackageListSections::print(const std::unique_ptr<PkgColorizer> & colorizer)
     enum { COL_NA, COL_EVR, COL_REPO };
 
     for (const auto & [heading, pkg_set, obsoletes] : p_impl->sections) {
-        if (!pkg_set.empty()) {
-            // sort the packages in section according to NEVRA
-            std::vector<libdnf5::rpm::Package> packages;
-            for (const auto & pkg : pkg_set) {
-                packages.emplace_back(std::move(pkg));
-            }
-            std::sort(packages.begin(), packages.end(), libdnf5::rpm::cmp_nevra<libdnf5::rpm::Package>);
-
-            struct libscols_line * first_line = nullptr;
-            struct libscols_line * last_line = nullptr;
-            for (const auto & pkg : packages) {
-                struct libscols_line * ln = scols_table_new_line(table, NULL);
-                if (first_line == nullptr) {
-                    first_line = ln;
-                }
-                last_line = ln;
-                if (colorizer) {
-                    scols_line_set_color(ln, colorizer->get_pkg_color(PackageAdapter(pkg)).c_str());
-                }
-                scols_line_set_data(ln, COL_NA, pkg.get_na().c_str());
-                scols_line_set_data(ln, COL_EVR, pkg.get_evr().c_str());
-                if (pkg.is_installed()) {
-                    scols_line_set_data(ln, COL_REPO, pkg.get_from_repo_id().c_str());
-                } else {
-                    scols_line_set_data(ln, COL_REPO, pkg.get_repo_id().c_str());
-                }
-
-                auto obsoletes_it = obsoletes.find(pkg.get_id());
-                if (obsoletes_it != obsoletes.end() && !obsoletes_it->second.empty()) {
-                    for (const auto & pkg_ob : obsoletes_it->second) {
-                        struct libscols_line * ln = scols_table_new_line(table, NULL);
-                        last_line = ln;
-                        scols_line_set_data(ln, COL_NA, ("    " + pkg_ob.get_na()).c_str());
-                        scols_line_set_data(ln, COL_EVR, pkg_ob.get_evr().c_str());
-                        scols_line_set_data(ln, COL_REPO, pkg_ob.get_from_repo_id().c_str());
-                    }
-                }
-            }
-            table_sections.emplace_back(heading, first_line, last_line);
+        if (pkg_set.empty()) {
+            // skip empty sections
+            continue;
         }
+
+        // sort the packages in section according to NEVRA
+        std::vector<libdnf5::rpm::Package> packages;
+        for (const auto & pkg : pkg_set) {
+            packages.emplace_back(std::move(pkg));
+        }
+        std::sort(packages.begin(), packages.end(), libdnf5::rpm::cmp_nevra<libdnf5::rpm::Package>);
+
+        struct libscols_line * first_line = nullptr;
+        struct libscols_line * last_line = nullptr;
+        for (const auto & pkg : packages) {
+            struct libscols_line * ln = scols_table_new_line(table, NULL);
+            if (first_line == nullptr) {
+                first_line = ln;
+            }
+            last_line = ln;
+            if (colorizer) {
+                scols_line_set_color(ln, colorizer->get_pkg_color(PackageAdapter(pkg)).c_str());
+            }
+            scols_line_set_data(ln, COL_NA, pkg.get_na().c_str());
+            scols_line_set_data(ln, COL_EVR, pkg.get_evr().c_str());
+            if (pkg.is_installed()) {
+                scols_line_set_data(ln, COL_REPO, pkg.get_from_repo_id().c_str());
+            } else {
+                scols_line_set_data(ln, COL_REPO, pkg.get_repo_id().c_str());
+            }
+
+            auto obsoletes_it = obsoletes.find(pkg.get_id());
+            if (obsoletes_it != obsoletes.end() && !obsoletes_it->second.empty()) {
+                for (const auto & pkg_ob : obsoletes_it->second) {
+                    struct libscols_line * ln = scols_table_new_line(table, NULL);
+                    last_line = ln;
+                    scols_line_set_data(ln, COL_NA, ("    " + pkg_ob.get_na()).c_str());
+                    scols_line_set_data(ln, COL_EVR, pkg_ob.get_evr().c_str());
+                    scols_line_set_data(ln, COL_REPO, pkg_ob.get_from_repo_id().c_str());
+                }
+            }
+        }
+        table_sections.emplace_back(heading, first_line, last_line);
     }
 
 
