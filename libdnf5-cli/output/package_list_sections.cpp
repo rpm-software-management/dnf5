3ef3f55831d19a2c39aeb32cbd2edeac4598b9bd
Matej Focko <mfocko@redhat.com>
2025-11-10 11:21:47 +0100

    feat(cli): JSON output for `check-upgrade`
    Fixes #2481
    
    Signed-off-by: Matej Focko <mfocko@redhat.com>

--- libdnf5-cli/output/package_list_sections.cpp
+++ libdnf5-cli/output/package_list_sections.cpp
@@ -24,6 +24,7 @@
 #include "libdnf5-cli/output/adapters/package.hpp"
 #include "libdnf5-cli/tty.hpp"
 
+#include <json-c/json.h>
 #include <libdnf5/rpm/nevra.hpp>
 #include <libsmartcols/libsmartcols.h>
 
@@ -45,6 +46,18 @@ std::vector<libdnf5::rpm::Package> to_sorted_vector(const libdnf5::rpm::PackageS
 
 std::vector<libdnf5::rpm::Package> to_sorted_vector(const libdnf5::rpm::PackageSet & pkg_set) {
     return to_sorted_vector(pkg_set, libdnf5::rpm::cmp_nevra<libdnf5::rpm::Package>);
+}
+
+json_object * package_to_json(const libdnf5::rpm::Package & pkg) {
+    json_object * j_pkg = json_object_new_object();
+    json_object_object_add(j_pkg, "name", json_object_new_string(pkg.get_name().c_str()));
+    json_object_object_add(j_pkg, "arch", json_object_new_string(pkg.get_arch().c_str()));
+    json_object_object_add(j_pkg, "evr", json_object_new_string(pkg.get_evr().c_str()));
+    json_object_object_add(
+        j_pkg,
+        "repository",
+        json_object_new_string(pkg.is_installed() ? pkg.get_from_repo_id().c_str() : pkg.get_repo_id().c_str()));
+    return j_pkg;
 }
 
 }  // namespace
@@ -134,6 +147,43 @@ void PackageListSections::print(const std::unique_ptr<PkgColorizer> & colorizer)
     }
 
     scols_unref_table(table);
+}
+
+// [NOTE] When editing JSON output format, do not forget to update the docs at doc/commands/check-upgrade.8.rst
+void PackageListSections::print_json() {
+    json_object * j_output = json_object_new_object();
+    for (const auto & [heading, pkg_set, obsoletes] : p_impl->sections) {
+        if (pkg_set.empty()) {
+            // skip empty sections
+            continue;
+        }
+
+        json_object * j_packages = json_object_new_array();
+
+        // iterate through the packages in section ordered by NEVRA
+        for (const auto & pkg : to_sorted_vector(pkg_set)) {
+            json_object * j_pkg = package_to_json(pkg);
+
+            // add obsoleted packages
+            auto obsoletes_it = obsoletes.find(pkg.get_id());
+            if (obsoletes_it != obsoletes.end() && !obsoletes_it->second.empty()) {
+                json_object * j_obsoleted = json_object_new_array();
+                for (const auto & pkg_ob : obsoletes_it->second) {
+                    json_object_array_add(j_obsoleted, package_to_json(pkg_ob));
+                }
+                json_object_object_add(j_pkg, "obsoletes", j_obsoleted);
+            }
+
+            json_object_array_add(j_packages, j_pkg);
+        }
+
+        // [TODO](mfocko) Consider adding header to CLI “pretty” output too…
+        json_object_object_add(j_output, heading != "" ? heading.c_str() : "upgrades", j_packages);
+    }
+
+    // Print and deallocate
+    std::cout << json_object_to_json_string_ext(j_output, JSON_C_TO_STRING_PRETTY) << std::endl;
+    json_object_put(j_output);
 }
 
 
