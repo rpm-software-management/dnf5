c3623d791f1938669bc492bfb29aabd2d9eeb153
Jaroslav Rohel <jrohel@redhat.com>
2025-12-04 12:41:49 +0100

    plugin::get_config_dirs: Interpret LIBDNF_PLUGINS_CONFIG_DIR as a list of dirs
    The `plugin::get_config_dirs` function now interprets the environment variable
    `LIBDNF_PLUGINS_CONFIG_DIR` as a list of directories instead of a single
    directory path. This change allows users to specify multiple configuration
    paths via the environment.
    
    The OptionStringList parser is used to handle the list format.

--- libdnf5/plugin/utils.cpp
+++ libdnf5/plugin/utils.cpp
@@ -11,9 +11,11 @@ std::vector<std::filesystem::path> get_config_dirs(const Base & base) {
     std::vector<std::filesystem::path> dirs;
     const auto & config = base.get_config();
 
-    const char * const plugins_config_dir = std::getenv("LIBDNF_PLUGINS_CONFIG_DIR");
-    if (plugins_config_dir && config.get_pluginconfpath_option().get_priority() < Option::Priority::COMMANDLINE) {
-        dirs.emplace_back(plugins_config_dir);
+    const char * const env_plugins_config_dirs = std::getenv("LIBDNF_PLUGINS_CONFIG_DIR");
+    if (env_plugins_config_dirs && config.get_pluginconfpath_option().get_priority() < Option::Priority::COMMANDLINE) {
+        OptionStringList env_opt{std::string(env_plugins_config_dirs)};
+        const auto & env_value = env_opt.get_value();
+        dirs = {env_value.begin(), env_value.end()};
     } else {
         dirs.emplace_back(config.get_pluginconfpath_option().get_value());
     }
