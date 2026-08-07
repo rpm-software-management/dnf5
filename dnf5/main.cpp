ca9bbc9f89c28f3238968218da3c45395d94ffad
Jaroslav Rohel <jrohel@redhat.com>
2026-08-07 12:22:35 +0000

    dnf5: Add --add-vendor-policy and --clear-vendor-policies CLI options
    Add two new global command-line options for managing vendor change
    policies at runtime:
    
    --add-vendor-policy=POLICY  Add a vendor change policy using the compact
      format. Can be specified multiple times.
    
    --clear-vendor-policies  Remove all vendor change policies. Can be
      combined with --add-vendor-policy to replace existing policies with
      custom ones. Conflicts with --allow-vendor-change.

--- dnf5/main.cpp
+++ dnf5/main.cpp
@@ -355,6 +355,40 @@ void RootCommand::set_argument_parser() {
     no_allow_vendor_change->add_conflict_argument(*allow_vendor_change);
 
     {
+        auto add_vendor_policy = parser.add_new_named_arg("add-vendor-policy");
+        add_vendor_policy->set_long_name("add-vendor-policy");
+        add_vendor_policy->set_has_value(true);
+        add_vendor_policy->set_arg_value_help("POLICY");
+        add_vendor_policy->set_description(
+            _("Add a vendor change policy for this run. Can be specified multiple times."));
+        add_vendor_policy->set_parse_hook_func([&ctx](
+                                                   [[maybe_unused]] ArgumentParser::NamedArg * arg,
+                                                   [[maybe_unused]] const char * option,
+                                                   const char * value) {
+            ctx.get_cmdline_vendor_policies().emplace_back(value);
+            return true;
+        });
+        global_options_group->register_argument(add_vendor_policy);
+    }
+
+    {
+        auto clear_vendor_policies = parser.add_new_named_arg("clear-vendor-policies");
+        clear_vendor_policies->set_long_name("clear-vendor-policies");
+        clear_vendor_policies->set_description(libdnf5::utils::sformat(
+            _("Remove all vendor change policies. Can be combined with {} to replace them."),
+            "--add-vendor-policy=POLICY"));
+        clear_vendor_policies->set_parse_hook_func([&ctx](
+                                                       [[maybe_unused]] ArgumentParser::NamedArg * arg,
+                                                       [[maybe_unused]] const char * option,
+                                                       [[maybe_unused]] const char * value) {
+            ctx.set_clear_vendor_policies(true);
+            return true;
+        });
+        clear_vendor_policies->add_conflict_argument(*allow_vendor_change);
+        global_options_group->register_argument(clear_vendor_policies);
+    }
+
+    {
         auto no_docs = parser.add_new_named_arg("no-docs");
         no_docs->set_long_name("no-docs");
         no_docs->set_description(
@@ -1555,6 +1589,8 @@ int main(int argc, char * argv[]) try {
 
                 context.apply_repository_setopts();
             }
+
+            context.apply_cmdline_vendor_policies();
 
             // Run selected command
             command->configure();
